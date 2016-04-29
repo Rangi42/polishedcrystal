0fe5b5fbb0b0ef9406c549d4867f53c8fb3a263c
Remy Oukaour <remy.oukaour@gmail.com>
2016-04-28 23:19:35 -0400

    Rename FAIRY egg group to FAERY.

--- constants/pokemon_data_constants.asm
+++ constants/pokemon_data_constants.asm
@@ -14,7 +14,7 @@ const_value SET 1
 	const INSECT       ; 3
 	const AVIAN        ; 4
 	const FIELD        ; 5
-	const FAIRY        ; 6
+	const FAERY        ; 6
 	const PLANT        ; 7
 	const HUMANSHAPE   ; 8
 	const INVERTEBRATE ; 9
