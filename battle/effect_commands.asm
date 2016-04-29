0f7d96133c187d6c5c682082a9545baaa3404358
Remy Oukaour <remy.oukaour@gmail.com>
2016-04-28 23:45:31 -0400

    Fix Belly Drum bug.

--- battle/effect_commands.asm
+++ battle/effect_commands.asm
@@ -9459,17 +9459,14 @@ BattleCommand_StartSun: ; 37c07
 
 BattleCommand_BellyDrum: ; 37c1a
 ; bellydrum
-; This command is buggy because it raises the user's attack
-; before checking that it has enough HP to use the move.
-; Swap the order of these two blocks to fix.
+	callab GetHalfMaxHP
+	callab CheckUserHasEnoughHP
+	jr nc, .failed
+
 	call BattleCommand_AttackUp2
 	ld a, [AttackMissed]
 	and a
 	jr nz, .failed
-
-	callab GetHalfMaxHP
-	callab CheckUserHasEnoughHP
-	jr nc, .failed
 
 	push bc
 	call AnimateCurrentMove
