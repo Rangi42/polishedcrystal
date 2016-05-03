32a97fb938033cc17e42fe88b7bb092e8c8b42ea
Remy Oukaour <remy.oukaour@gmail.com>
2016-05-03 06:53:00 -0400

    Explicitly number first icon bank as 1

--- menu/mon_icons_1.asm
+++ menu/mon_icons_1.asm
@@ -1,81 +1,81 @@
 IconPointers1:
-	dw NullIcon
-	dw PoliwagIcon
-	dw JigglypuffIcon
-	dw DiglettIcon
-	dw PikachuIcon
-	dw StaryuIcon
-	dw FishIcon
-	dw BirdIcon
-	dw MonsterIcon
-	dw ClefairyIcon
-	dw OddishIcon
-	dw BugIcon
-	dw GhostIcon
-	dw LaprasIcon
-	dw HumanshapeIcon
-	dw FoxIcon
-	dw EquineIcon
-	dw ShellIcon
-	dw BlobIcon
-	dw SerpentIcon
-	dw VoltorbIcon
-	dw SquirtleIcon
-	dw BulbasaurIcon
-	dw CharmanderIcon
-	dw CaterpillarIcon
-	dw UnownIcon
-	dw GeodudeIcon
-	dw FighterIcon
-	dw EggIcon
-	dw JellyfishIcon
-	dw MothIcon
-	dw BatIcon
-	dw SnorlaxIcon
-	dw HoOhIcon
-	dw LugiaIcon
-	dw GyaradosIcon
-	dw SlowpokeIcon
-	dw SudowoodoIcon
-	dw BigmonIcon
+	dw NullIcon1
+	dw PoliwagIcon1
+	dw JigglypuffIcon1
+	dw DiglettIcon1
+	dw PikachuIcon1
+	dw StaryuIcon1
+	dw FishIcon1
+	dw BirdIcon1
+	dw MonsterIcon1
+	dw ClefairyIcon1
+	dw OddishIcon1
+	dw BugIcon1
+	dw GhostIcon1
+	dw LaprasIcon1
+	dw HumanshapeIcon1
+	dw FoxIcon1
+	dw EquineIcon1
+	dw ShellIcon1
+	dw BlobIcon1
+	dw SerpentIcon1
+	dw VoltorbIcon1
+	dw SquirtleIcon1
+	dw BulbasaurIcon1
+	dw CharmanderIcon1
+	dw CaterpillarIcon1
+	dw UnownIcon1
+	dw GeodudeIcon1
+	dw FighterIcon1
+	dw EggIcon1
+	dw JellyfishIcon1
+	dw MothIcon1
+	dw BatIcon1
+	dw SnorlaxIcon1
+	dw HoOhIcon1
+	dw LugiaIcon1
+	dw GyaradosIcon1
+	dw SlowpokeIcon1
+	dw SudowoodoIcon1
+	dw BigmonIcon1
 
 Icons1:
-NullIcon:
-PoliwagIcon:      INCBIN "gfx/icon/poliwag.2bpp" ; 0x8ec0d
-JigglypuffIcon:   INCBIN "gfx/icon/jigglypuff.2bpp" ; 0x8ec8d
-DiglettIcon:      INCBIN "gfx/icon/diglett.2bpp" ; 0x8ed0d
-PikachuIcon:      INCBIN "gfx/icon/pikachu.2bpp" ; 0x8ed8d
-StaryuIcon:       INCBIN "gfx/icon/staryu.2bpp" ; 0x8ee0d
-FishIcon:         INCBIN "gfx/icon/fish.2bpp" ; 0x8ee8d
-BirdIcon:         INCBIN "gfx/icon/bird.2bpp" ; 0x8ef0d
-MonsterIcon:      INCBIN "gfx/icon/monster.2bpp" ; 0x8ef8d
-ClefairyIcon:     INCBIN "gfx/icon/clefairy.2bpp" ; 0x8f00d
-OddishIcon:       INCBIN "gfx/icon/oddish.2bpp" ; 0x8f08d
-BugIcon:          INCBIN "gfx/icon/bug.2bpp" ; 0x8f10d
-GhostIcon:        INCBIN "gfx/icon/ghost.2bpp" ; 0x8f18d
-LaprasIcon:       INCBIN "gfx/icon/lapras.2bpp" ; 0x8f20d
-HumanshapeIcon:   INCBIN "gfx/icon/humanshape.2bpp" ; 0x8f28d
-FoxIcon:          INCBIN "gfx/icon/fox.2bpp" ; 0x8f30d
-EquineIcon:       INCBIN "gfx/icon/equine.2bpp" ; 0x8f38d
-ShellIcon:        INCBIN "gfx/icon/shell.2bpp" ; 0x8f40d
-BlobIcon:         INCBIN "gfx/icon/blob.2bpp" ; 0x8f48d
-SerpentIcon:      INCBIN "gfx/icon/serpent.2bpp" ; 0x8f50d
-VoltorbIcon:      INCBIN "gfx/icon/voltorb.2bpp" ; 0x8f58d
-SquirtleIcon:     INCBIN "gfx/icon/squirtle.2bpp" ; 0x8f60d
-BulbasaurIcon:    INCBIN "gfx/icon/bulbasaur.2bpp" ; 0x8f68d
-CharmanderIcon:   INCBIN "gfx/icon/charmander.2bpp" ; 0x8f70d
-CaterpillarIcon:  INCBIN "gfx/icon/caterpillar.2bpp" ; 0x8f78d
-UnownIcon:        INCBIN "gfx/icon/unown.2bpp" ; 0x8f80d
-GeodudeIcon:      INCBIN "gfx/icon/geodude.2bpp" ; 0x8f88d
-FighterIcon:      INCBIN "gfx/icon/fighter.2bpp" ; 0x8f90d
-EggIcon:          INCBIN "gfx/icon/egg.2bpp" ; 0x8f98d
-JellyfishIcon:    INCBIN "gfx/icon/jellyfish.2bpp" ; 0x8fa0d
-MothIcon:         INCBIN "gfx/icon/moth.2bpp" ; 0x8fa8d
-BatIcon:          INCBIN "gfx/icon/bat.2bpp" ; 0x8fb0d
-SnorlaxIcon:      INCBIN "gfx/icon/snorlax.2bpp" ; 0x8fb8d
-HoOhIcon:         INCBIN "gfx/icon/ho_oh.2bpp" ; 0x8fc0d
-LugiaIcon:        INCBIN "gfx/icon/lugia.2bpp" ; 0x8fc8d
-GyaradosIcon:     INCBIN "gfx/icon/gyarados.2bpp" ; 0x8fd0d
-SlowpokeIcon:     INCBIN "gfx/icon/slowpoke.2bpp" ; 0x8fd8d
-SudowoodoIcon:    INCBIN "gfx/icon/sudowoodo.2bpp" ; 0x8fe0d
-BigmonIcon:       INCBIN "gfx/icon/bigmon.2bpp" ; 0x8fe8d
+NullIcon1:
+PoliwagIcon1:     INCBIN "gfx/icon/poliwag.2bpp" ; 0x8ec0d
+JigglypuffIcon1:  INCBIN "gfx/icon/jigglypuff.2bpp" ; 0x8ec8d
+DiglettIcon1:     INCBIN "gfx/icon/diglett.2bpp" ; 0x8ed0d
+PikachuIcon1:     INCBIN "gfx/icon/pikachu.2bpp" ; 0x8ed8d
+StaryuIcon1:      INCBIN "gfx/icon/staryu.2bpp" ; 0x8ee0d
+FishIcon1:        INCBIN "gfx/icon/fish.2bpp" ; 0x8ee8d
+BirdIcon1:        INCBIN "gfx/icon/bird.2bpp" ; 0x8ef0d
+MonsterIcon1:     INCBIN "gfx/icon/monster.2bpp" ; 0x8ef8d
+ClefairyIcon1:    INCBIN "gfx/icon/clefairy.2bpp" ; 0x8f00d
+OddishIcon1:      INCBIN "gfx/icon/oddish.2bpp" ; 0x8f08d
+BugIcon1:         INCBIN "gfx/icon/bug.2bpp" ; 0x8f10d
+GhostIcon1:       INCBIN "gfx/icon/ghost.2bpp" ; 0x8f18d
+LaprasIcon1:      INCBIN "gfx/icon/lapras.2bpp" ; 0x8f20d
+HumanshapeIcon1:  INCBIN "gfx/icon/humanshape.2bpp" ; 0x8f28d
+FoxIcon1:         INCBIN "gfx/icon/fox.2bpp" ; 0x8f30d
+EquineIcon1:      INCBIN "gfx/icon/equine.2bpp" ; 0x8f38d
+ShellIcon1:       INCBIN "gfx/icon/shell.2bpp" ; 0x8f40d
+BlobIcon1:        INCBIN "gfx/icon/blob.2bpp" ; 0x8f48d
+SerpentIcon1:     INCBIN "gfx/icon/serpent.2bpp" ; 0x8f50d
+VoltorbIcon1:     INCBIN "gfx/icon/voltorb.2bpp" ; 0x8f58d
+SquirtleIcon1:    INCBIN "gfx/icon/squirtle.2bpp" ; 0x8f60d
+BulbasaurIcon1:   INCBIN "gfx/icon/bulbasaur.2bpp" ; 0x8f68d
+CharmanderIcon1:  INCBIN "gfx/icon/charmander.2bpp" ; 0x8f70d
+CaterpillarIcon1: INCBIN "gfx/icon/caterpillar.2bpp" ; 0x8f78d
+UnownIcon1:       INCBIN "gfx/icon/unown.2bpp" ; 0x8f80d
+GeodudeIcon1:     INCBIN "gfx/icon/geodude.2bpp" ; 0x8f88d
+FighterIcon1:     INCBIN "gfx/icon/fighter.2bpp" ; 0x8f90d
+EggIcon1:         INCBIN "gfx/icon/egg.2bpp" ; 0x8f98d
+JellyfishIcon1:   INCBIN "gfx/icon/jellyfish.2bpp" ; 0x8fa0d
+MothIcon1:        INCBIN "gfx/icon/moth.2bpp" ; 0x8fa8d
+BatIcon1:         INCBIN "gfx/icon/bat.2bpp" ; 0x8fb0d
+SnorlaxIcon1:     INCBIN "gfx/icon/snorlax.2bpp" ; 0x8fb8d
+HoOhIcon1:        INCBIN "gfx/icon/ho_oh.2bpp" ; 0x8fc0d
+LugiaIcon1:       INCBIN "gfx/icon/lugia.2bpp" ; 0x8fc8d
+GyaradosIcon1:    INCBIN "gfx/icon/gyarados.2bpp" ; 0x8fd0d
+SlowpokeIcon1:    INCBIN "gfx/icon/slowpoke.2bpp" ; 0x8fd8d
+SudowoodoIcon1:   INCBIN "gfx/icon/sudowoodo.2bpp" ; 0x8fe0d
+BigmonIcon1:      INCBIN "gfx/icon/bigmon.2bpp" ; 0x8fe8d
