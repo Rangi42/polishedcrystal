6574923919c3620649906aa2079d12d81026795b
Remy Oukaour <remy.oukaour@gmail.com>
2016-05-10 15:32:45 -0400

    Improve boss trainers' DVs (some need lower Attack DV for female Pokémon)

--- trainers/dvs.asm
+++ trainers/dvs.asm
@@ -23,71 +23,71 @@ endr
 TrainerClassDVs: ; 270d6
 	;  Atk  Spd
 	;  Def  Spc
-	db $9A, $77 ; falkner
-	db $88, $88 ; bugsy
-	db $98, $88 ; whitney
-	db $98, $88 ; morty
-	db $98, $88 ; pryce
-	db $98, $88 ; jasmine
-	db $98, $88 ; chuck
-	db $7C, $DD ; clair
-	db $DD, $DD ; rival1
-	db $98, $88 ; pokemon prof
-	db $DC, $DD ; will
-	db $DC, $DD ; cal
-	db $DC, $DD ; bruno
-	db $7F, $DF ; karen
-	db $DC, $DD ; koga
-	db $DC, $DD ; champion
-	db $98, $88 ; brock
-	db $78, $88 ; misty
-	db $98, $88 ; lt surge
+	db $FF, $FF ; falkner
+	db $FF, $FF ; bugsy
+	db $FF, $FF ; whitney
+	db $FF, $FF ; morty
+	db $FF, $FF ; pryce
+	db $7F, $FF ; jasmine
+	db $FF, $FF ; chuck
+	db $7F, $FF ; clair
+	db $FF, $FF ; rival1
+	db $FF, $FF ; pokemon prof
+	db $FF, $FF ; will
+	db $FF, $FF ; cal
+	db $FF, $FF ; bruno
+	db $7F, $FF ; karen
+	db $FF, $FF ; koga
+	db $FF, $FF ; champion
+	db $FF, $FF ; brock
+	db $7F, $FF ; misty
+	db $FF, $FF ; lt surge
 	db $98, $88 ; scientist
-	db $78, $88 ; erika
+	db $7F, $FF ; erika
 	db $98, $88 ; youngster
 	db $98, $88 ; schoolboy
 	db $98, $88 ; bird keeper
 	db $58, $88 ; lass
-	db $98, $88 ; janine
-	db $D8, $C8 ; cooltrainerm
-	db $7C, $C8 ; cooltrainerf
+	db $7F, $FF ; janine
+	db $FF, $FF ; cooltrainerm
+	db $7F, $FF ; cooltrainerf
 	db $69, $C8 ; beauty
-	db $98, $88 ; pokemaniac
+	db $9F, $FF ; pokemaniac
 	db $D8, $A8 ; gruntm
 	db $98, $88 ; gentleman
 	db $98, $88 ; skier
 	db $68, $88 ; teacher
-	db $7D, $87 ; sabrina
+	db $7F, $FF ; sabrina
 	db $98, $88 ; bug catcher
 	db $98, $88 ; fisher
 	db $98, $88 ; swimmerm
 	db $78, $88 ; swimmerf
 	db $98, $88 ; sailor
 	db $98, $88 ; super nerd
-	db $98, $88 ; rival2
+	db $FF, $FF ; rival2
 	db $98, $88 ; guitarist
 	db $A8, $88 ; hiker
 	db $98, $88 ; biker
-	db $98, $88 ; blaine
+	db $FF, $FF ; blaine
 	db $98, $88 ; burglar
 	db $98, $88 ; firebreather
 	db $98, $88 ; juggler
 	db $98, $88 ; blackbelt
-	db $D8, $A8 ; executivem
+	db $DF, $FF ; executivem
 	db $98, $88 ; psychic
 	db $6A, $A8 ; picnicker
 	db $98, $88 ; camper
-	db $7E, $A8 ; executivef
+	db $7F, $FF ; executivef
 	db $98, $88 ; sage
 	db $78, $88 ; medium
 	db $98, $88 ; boarder
 	db $98, $88 ; pokefanm
-	db $68, $8A ; kimono girl
+	db $6F, $FF ; kimono girl
 	db $68, $A8 ; twins
 	db $6D, $88 ; pokefanf
-	db $FD, $DE ; red
-	db $9D, $DD ; blue
+	db $FF, $FF ; red
+	db $FF, $FF ; blue
 	db $98, $88 ; officer
 	db $7E, $A8 ; gruntf
-	db $98, $88 ; mysticalman
+	db $9F, $FF ; mysticalman
 ; 2715c
