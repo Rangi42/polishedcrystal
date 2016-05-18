8db421d4dd09f0eb4b081b8ffab3f3efe876a59c
Remy Oukaour <remy.oukaour@gmail.com>
2016-05-18 00:50:13 -0400

    Add a maximum level to Wonder Traded Pokémon.

--- engine/wonder_trade.asm
+++ engine/wonder_trade.asm
@@ -588,8 +588,8 @@ GetWonderTradeHeldItem:
 	db LUCKY_EGG
 
 CheckValidLevel:
-; Don't receive Pokémon below a valid level.
-; Legendaries and other banned Pokémon have a "valid" level of 255.
+; Don't receive Pokémon outside a valid level range.
+; Legendaries and other banned Pokémon have a "valid" range of 255 to 255.
 	ld hl, PartyMon1Level
 	ld bc, PARTYMON_STRUCT_LENGTH
 	call Trade_GetAttributeOfCurrentPartymon
@@ -597,273 +597,279 @@ CheckValidLevel:
 	ld d, a
 
 	ld a, [wOTTrademonSpecies]
-	ld hl, .MinimumPokemonLevels
+	ld hl, .ValidPokemonLevels
 	ld b, 0
 	ld c, a
 	add hl, bc
-	ld a, [hl]
-	dec a
+	add hl, bc
 
+	ld a, [hli]
+	dec a
 	cp d
-	jr nc, .level_too_low
+	ret nc
+
+	ld a, [hl]
+	cp d
+	ret c
+
 	xor a
-.level_too_low
 	ret
 
-.MinimumPokemonLevels
-	db 255 ; ?????
-	db   1 ; Bulbasaur
-	db  16 ; Ivysaur
-	db  32 ; Venusaur
-	db   1 ; Charmander
-	db  16 ; Charmeleon
-	db  36 ; Charizard
-	db   1 ; Squirtle
-	db  16 ; Wartortle
-	db  36 ; Blastoise
-	db   1 ; Caterpie
-	db   7 ; Metapod
-	db  10 ; Butterfree
-	db   1 ; Weedle
-	db   7 ; Kakuna
-	db  10 ; Beedrill
-	db   1 ; Pidgey
-	db  18 ; Pidgeotto
-	db  36 ; Pidgeot
-	db   1 ; Rattata
-	db  20 ; Raticate
-	db   1 ; Spearow
-	db  20 ; Fearow
-	db   1 ; Ekans
-	db  22 ; Arbok
-	db   1 ; Pikachu
-	db  20 ; Raichu
-	db   1 ; Sandshrew
-	db  22 ; Sandslash
-	db   1 ; Nidoran♀
-	db  16 ; Nidorina
-	db  36 ; Nidoqueen
-	db   1 ; Nidoran♂
-	db  16 ; Nidorino
-	db  36 ; Nidoking
-	db   5 ; Clefairy
-	db  20 ; Clefable
-	db   1 ; Vulpix
-	db  20 ; Ninetales
-	db   5 ; Jigglypuff
-	db  20 ; Wigglytuff
-	db   1 ; Zubat
-	db  22 ; Golbat
-	db   1 ; Oddish
-	db  21 ; Gloom
-	db  32 ; Vileplume
-	db   1 ; Paras
-	db  24 ; Parasect
-	db   1 ; Venonat
-	db  31 ; Venomoth
-	db   1 ; Diglett
-	db  26 ; Dugtrio
-	db   1 ; Meowth
-	db  28 ; Persian
-	db   1 ; Psyduck
-	db  33 ; Golduck
-	db   1 ; Mankey
-	db  28 ; Primeape
-	db   1 ; Growlithe
-	db  20 ; Arcanine
-	db   1 ; Poliwag
-	db  25 ; Poliwhirl
-	db  36 ; Poliwrath
-	db   1 ; Abra
-	db  16 ; Kadabra
-	db  36 ; Alakazam
-	db   1 ; Machop
-	db  28 ; Machoke
-	db  46 ; Machamp
-	db   1 ; Bellsprout
-	db  21 ; Weepinbell
-	db  32 ; Victreebel
-	db   1 ; Tentacool
-	db  30 ; Tentacruel
-	db   1 ; Geodude
-	db  25 ; Graveler
-	db  45 ; Golem
-	db   1 ; Ponyta
-	db  40 ; Rapidash
-	db   1 ; Slowpoke
-	db  37 ; Slowbro
-	db   1 ; Magnemite
-	db  30 ; Magneton
-	db   1 ; Farfetch'd
-	db   1 ; Doduo
-	db  31 ; Dodrio
-	db   1 ; Seel
-	db  34 ; Dewgong
-	db   1 ; Grimer
-	db  38 ; Muk
-	db   1 ; Shellder
-	db  34 ; Cloyster
-	db   1 ; Gastly
-	db  25 ; Haunter
-	db  45 ; Gengar
-	db   1 ; Onix
-	db   1 ; Drowzee
-	db  26 ; Hypno
-	db   1 ; Krabby
-	db  28 ; Kingler
-	db   1 ; Voltorb
-	db  30 ; Electrode
-	db   1 ; Exeggcute
-	db  30 ; Exeggutor
-	db   1 ; Cubone
-	db  28 ; Marowak
-	db  20 ; Hitmonlee
-	db  20 ; Hitmonchan
-	db   1 ; Lickitung
-	db   1 ; Koffing
-	db  35 ; Weezing
-	db   1 ; Rhyhorn
-	db  42 ; Rhydon
-	db   1 ; Chansey
-	db   1 ; Tangela
-	db   1 ; Kangaskhan
-	db   1 ; Horsea
-	db  32 ; Seadra
-	db   1 ; Goldeen
-	db  33 ; Seaking
-	db   1 ; Staryu
-	db  33 ; Starmie
-	db   1 ; Mr.Mime
-	db   1 ; Scyther
-	db  20 ; Jynx
-	db  20 ; Electabuzz
-	db  20 ; Magmar
-	db   1 ; Pinsir
-	db   1 ; Tauros
-	db   1 ; Magikarp
-	db  20 ; Gyarados
-	db   1 ; Lapras
-	db   1 ; Ditto
-	db   1 ; Eevee
-	db  20 ; Vaporeon
-	db  20 ; Jolteon
-	db  20 ; Flareon
-	db   1 ; Porygon
-	db   1 ; Omanyte
-	db  40 ; Omastar
-	db   1 ; Kabuto
-	db  40 ; Kabutops
-	db   1 ; Aerodactyl
-	db   1 ; Snorlax
-	db 255 ; Articuno
-	db 255 ; Zapdos
-	db 255 ; Moltres
-	db   1 ; Dratini
-	db  30 ; Dragonair
-	db  55 ; Dragonite
-	db 255 ; Mewtwo
-	db 255 ; Mew
-	db   1 ; Chikorita
-	db  16 ; Bayleef
-	db  32 ; Meganium
-	db   1 ; Cyndaquil
-	db  14 ; Quilava
-	db  36 ; Typhlosion
-	db   1 ; Totodile
-	db  18 ; Croconaw
-	db  30 ; Feraligatr
-	db   1 ; Sentret
-	db  15 ; Furret
-	db   1 ; Hoothoot
-	db  20 ; Noctowl
-	db   1 ; Ledyba
-	db  18 ; Ledian
-	db   1 ; Spinarak
-	db  22 ; Ariados
-	db  32 ; Crobat
-	db   1 ; Chinchou
-	db  27 ; Lanturn
-	db   1 ; Pichu
-	db   1 ; Cleffa
-	db   1 ; Igglybuff
-	db   1 ; Togepi
-	db  20 ; Togetic
-	db   1 ; Natu
-	db  25 ; Xatu
-	db   1 ; Mareep
-	db  15 ; Flaaffy
-	db  30 ; Ampharos
-	db  32 ; Bellossom
-	db   1 ; Marill
-	db  18 ; Azumarill
-	db   1 ; Sudowoodo
-	db  36 ; Politoed
-	db   1 ; Hoppip
-	db  18 ; Skiploom
-	db  27 ; Jumpluff
-	db   1 ; Aipom
-	db   1 ; Sunkern
-	db  20 ; Sunflora
-	db   1 ; Yanma
-	db   1 ; Wooper
-	db  20 ; Quagsire
-	db  20 ; Espeon
-	db  20 ; Umbreon
-	db   1 ; Murkrow
-	db  37 ; Slowking
-	db   1 ; Misdreavus
-	db   1 ; Unown
-	db   1 ; Wobbuffet
-	db   1 ; Girafarig
-	db   1 ; Pineco
-	db  31 ; Forretress
-	db   1 ; Dunsparce
-	db   1 ; Gligar
-	db  20 ; Steelix
-	db   1 ; Snubbull
-	db  23 ; Granbull
-	db   1 ; Qwilfish
-	db  20 ; Scizor
-	db   1 ; Shuckle
-	db   1 ; Heracross
-	db   1 ; Sneasel
-	db   1 ; Teddiursa
-	db  30 ; Ursaring
-	db   1 ; Slugma
-	db  38 ; Magcargo
-	db   1 ; Swinub
-	db  33 ; Piloswine
-	db   1 ; Corsola
-	db   1 ; Remoraid
-	db  25 ; Octillery
-	db   1 ; Delibird
-	db   1 ; Mantine
-	db   1 ; Skarmory
-	db   1 ; Houndour
-	db  24 ; Houndoom
-	db  55 ; Kingdra
-	db   1 ; Phanpy
-	db  25 ; Donphan
-	db  20 ; Porygon2
-	db   1 ; Stantler
-	db   1 ; Smeargle
-	db   1 ; Tyrogue
-	db  20 ; Hitmontop
-	db   1 ; Smoochum
-	db   1 ; Elekid
-	db   1 ; Magby
-	db   1 ; Miltank
-	db  20 ; Blissey
-	db 255 ; Raikou
-	db 255 ; Entei
-	db 255 ; Suicune
-	db   1 ; Larvitar
-	db  30 ; Pupitar
-	db  55 ; Tyranitar
-	db 255 ; Lugia
-	db 255 ; Ho-Oh
-	db 255 ; Celebi
-	db 255 ; ?????
-	db 255 ; Egg
-	db 255 ; ?????
-	db 255 ; ?????
+.ValidPokemonLevels
+	;  min, max
+	db 255, 255 ; ?????
+	db   1,  15 ; Bulbasaur
+	db  16,  31 ; Ivysaur
+	db  32, 100 ; Venusaur
+	db   1,  15 ; Charmander
+	db  16,  35 ; Charmeleon
+	db  36, 100 ; Charizard
+	db   1,  15 ; Squirtle
+	db  16,  35 ; Wartortle
+	db  36, 100 ; Blastoise
+	db   1,   6 ; Caterpie
+	db   7,   9 ; Metapod
+	db  10, 100 ; Butterfree
+	db   1,   6 ; Weedle
+	db   7,   9 ; Kakuna
+	db  10, 100 ; Beedrill
+	db   1,  17 ; Pidgey
+	db  18,  35 ; Pidgeotto
+	db  36, 100 ; Pidgeot
+	db   1,  19 ; Rattata
+	db  20, 100 ; Raticate
+	db   1,  19 ; Spearow
+	db  20, 100 ; Fearow
+	db   1,  21 ; Ekans
+	db  22, 100 ; Arbok
+	db   1,  19 ; Pikachu
+	db  20, 100 ; Raichu
+	db   1,  21 ; Sandshrew
+	db  22, 100 ; Sandslash
+	db   1,  15 ; Nidoran♀
+	db  16,  35 ; Nidorina
+	db  36, 100 ; Nidoqueen
+	db   1,  15 ; Nidoran♂
+	db  16,  35 ; Nidorino
+	db  36, 100 ; Nidoking
+	db   5,  19 ; Clefairy
+	db  20, 100 ; Clefable
+	db   1,  19 ; Vulpix
+	db  20, 100 ; Ninetales
+	db   5,  19 ; Jigglypuff
+	db  20, 100 ; Wigglytuff
+	db   1,  21 ; Zubat
+	db  22, 100 ; Golbat
+	db   1,  20 ; Oddish
+	db  21,  31 ; Gloom
+	db  32, 100 ; Vileplume
+	db   1,  23 ; Paras
+	db  24, 100 ; Parasect
+	db   1,  30 ; Venonat
+	db  31, 100 ; Venomoth
+	db   1,  25 ; Diglett
+	db  26, 100 ; Dugtrio
+	db   1,  27 ; Meowth
+	db  28, 100 ; Persian
+	db   1,  32 ; Psyduck
+	db  33, 100 ; Golduck
+	db   1,  27 ; Mankey
+	db  28, 100 ; Primeape
+	db   1,  19 ; Growlithe
+	db  20, 100 ; Arcanine
+	db   1,  24 ; Poliwag
+	db  25,  35 ; Poliwhirl
+	db  36, 100 ; Poliwrath
+	db   1,  15 ; Abra
+	db  16,  35 ; Kadabra
+	db  36, 100 ; Alakazam
+	db   1,  27 ; Machop
+	db  28,  45 ; Machoke
+	db  46, 100 ; Machamp
+	db   1,  20 ; Bellsprout
+	db  21,  31 ; Weepinbell
+	db  32, 100 ; Victreebel
+	db   1,  29 ; Tentacool
+	db  30, 100 ; Tentacruel
+	db   1,  24 ; Geodude
+	db  25,  44 ; Graveler
+	db  45, 100 ; Golem
+	db   1,  39 ; Ponyta
+	db  40, 100 ; Rapidash
+	db   1,  36 ; Slowpoke
+	db  37, 100 ; Slowbro
+	db   1,  29 ; Magnemite
+	db  30, 100 ; Magneton
+	db   1, 100 ; Farfetch'd
+	db   1,  30 ; Doduo
+	db  31, 100 ; Dodrio
+	db   1,  33 ; Seel
+	db  34, 100 ; Dewgong
+	db   1,  37 ; Grimer
+	db  38, 100 ; Muk
+	db   1,  33 ; Shellder
+	db  34, 100 ; Cloyster
+	db   1,  24 ; Gastly
+	db  25,  44 ; Haunter
+	db  45, 100 ; Gengar
+	db   1, 100 ; Onix
+	db   1,  25 ; Drowzee
+	db  26, 100 ; Hypno
+	db   1,  27 ; Krabby
+	db  28, 100 ; Kingler
+	db   1,  29 ; Voltorb
+	db  30, 100 ; Electrode
+	db   1,  29 ; Exeggcute
+	db  30, 100 ; Exeggutor
+	db   1,  27 ; Cubone
+	db  28, 100 ; Marowak
+	db  20, 100 ; Hitmonlee
+	db  20, 100 ; Hitmonchan
+	db   1, 100 ; Lickitung
+	db   1,  34 ; Koffing
+	db  35, 100 ; Weezing
+	db   1,  41 ; Rhyhorn
+	db  42,  54 ; Rhydon
+	db   1, 100 ; Chansey
+	db   1,  35 ; Tangela
+	db   1, 100 ; Kangaskhan
+	db   1,  31 ; Horsea
+	db  32,  54 ; Seadra
+	db   1,  32 ; Goldeen
+	db  33, 100 ; Seaking
+	db   1,  32 ; Staryu
+	db  33, 100 ; Starmie
+	db   1, 100 ; Mr.Mime
+	db  10, 100 ; Scyther
+	db  20, 100 ; Jynx
+	db  20,  36 ; Electabuzz
+	db  20,  36 ; Magmar
+	db  10, 100 ; Pinsir
+	db   1, 100 ; Tauros
+	db   1,  19 ; Magikarp
+	db  20, 100 ; Gyarados
+	db  20, 100 ; Lapras
+	db   1, 100 ; Ditto
+	db   1,  19 ; Eevee
+	db  20, 100 ; Vaporeon
+	db  20, 100 ; Jolteon
+	db  20, 100 ; Flareon
+	db   1,  20 ; Porygon
+	db  15,  39 ; Omanyte
+	db  40, 100 ; Omastar
+	db  15,  39 ; Kabuto
+	db  40, 100 ; Kabutops
+	db  15, 100 ; Aerodactyl
+	db  20, 100 ; Snorlax
+	db 255, 255 ; Articuno
+	db 255, 255 ; Zapdos
+	db 255, 255 ; Moltres
+	db  20,  29 ; Dratini
+	db  30,  54 ; Dragonair
+	db  55, 100 ; Dragonite
+	db 255, 255 ; Mewtwo
+	db 255, 255 ; Mew
+	db   1,  15 ; Chikorita
+	db  16,  31 ; Bayleef
+	db  32, 100 ; Meganium
+	db   1,  13 ; Cyndaquil
+	db  14,  35 ; Quilava
+	db  36, 100 ; Typhlosion
+	db   1,  17 ; Totodile
+	db  18,  29 ; Croconaw
+	db  30, 100 ; Feraligatr
+	db   1,  14 ; Sentret
+	db  15, 100 ; Furret
+	db   1,  19 ; Hoothoot
+	db  20, 100 ; Noctowl
+	db   1,  17 ; Ledyba
+	db  18, 100 ; Ledian
+	db   1,  21 ; Spinarak
+	db  22, 100 ; Ariados
+	db  32, 100 ; Crobat
+	db   1,  26 ; Chinchou
+	db  27, 100 ; Lanturn
+	db   1,  19 ; Pichu
+	db   1,  19 ; Cleffa
+	db   1,  19 ; Igglybuff
+	db   1,  19 ; Togepi
+	db  20, 100 ; Togetic
+	db   1,  24 ; Natu
+	db  25, 100 ; Xatu
+	db   1,  14 ; Mareep
+	db  15,  29 ; Flaaffy
+	db  30, 100 ; Ampharos
+	db  32, 100 ; Bellossom
+	db   1,  17 ; Marill
+	db  18, 100 ; Azumarill
+	db   1, 100 ; Sudowoodo
+	db  36, 100 ; Politoed
+	db   1,  17 ; Hoppip
+	db  18,  26 ; Skiploom
+	db  27, 100 ; Jumpluff
+	db   1, 100 ; Aipom
+	db   1,  19 ; Sunkern
+	db  20, 100 ; Sunflora
+	db   1, 100 ; Yanma
+	db   1,  19 ; Wooper
+	db  20, 100 ; Quagsire
+	db  20, 100 ; Espeon
+	db  20, 100 ; Umbreon
+	db   1, 100 ; Murkrow
+	db  37, 100 ; Slowking
+	db   1, 100 ; Misdreavus
+	db   1, 100 ; Unown
+	db   1, 100 ; Wobbuffet
+	db   1, 100 ; Girafarig
+	db   1,  30 ; Pineco
+	db  31, 100 ; Forretress
+	db   1, 100 ; Dunsparce
+	db   1, 100 ; Gligar
+	db  20, 100 ; Steelix
+	db   1,  22 ; Snubbull
+	db  23, 100 ; Granbull
+	db   1, 100 ; Qwilfish
+	db  20, 100 ; Scizor
+	db   1, 100 ; Shuckle
+	db   1, 100 ; Heracross
+	db   1, 100 ; Sneasel
+	db   1,  29 ; Teddiursa
+	db  30, 100 ; Ursaring
+	db   1,  37 ; Slugma
+	db  38, 100 ; Magcargo
+	db   1,  32 ; Swinub
+	db  33, 100 ; Piloswine
+	db   1, 100 ; Corsola
+	db   1,  24 ; Remoraid
+	db  25, 100 ; Octillery
+	db   1, 100 ; Delibird
+	db   1, 100 ; Mantine
+	db   1, 100 ; Skarmory
+	db   1,  23 ; Houndour
+	db  24, 100 ; Houndoom
+	db  55, 100 ; Kingdra
+	db   1,  24 ; Phanpy
+	db  25, 100 ; Donphan
+	db  20, 100 ; Porygon2
+	db   1, 100 ; Stantler
+	db   1, 100 ; Smeargle
+	db   1,  19 ; Tyrogue
+	db  20, 100 ; Hitmontop
+	db   1,  19 ; Smoochum
+	db   1,  19 ; Elekid
+	db   1,  19 ; Magby
+	db   1,  19 ; Miltank
+	db  20, 100 ; Blissey
+	db 255, 255 ; Raikou
+	db 255, 255 ; Entei
+	db 255, 255 ; Suicune
+	db   1,  29 ; Larvitar
+	db  30,  54 ; Pupitar
+	db  55, 100 ; Tyranitar
+	db 255, 255 ; Lugia
+	db 255, 255 ; Ho-Oh
+	db 255, 255 ; Celebi
+	db 255, 255 ; ?????
+	db 255, 255 ; ?????
+	db 255, 255 ; Egg
+	db 255, 255 ; ?????
