64df00b2453980b8a060004fbfa557c6ac01aad2
Remy Oukaour <remy.oukaour@gmail.com>
2016-05-20 18:30:17 -0400

    Legendary birds won't flee

--- battle/core.asm
+++ battle/core.asm
@@ -845,9 +845,6 @@ SometimesFleeMons: ; 3c59a
 
 OftenFleeMons: ; 3c5a8
 	db CUBONE
-	db ARTICUNO
-	db ZAPDOS
-	db MOLTRES
 	db QUAGSIRE
 	db DELIBIRD
 	db PHANPY
@@ -857,7 +854,6 @@ OftenFleeMons: ; 3c5a8
 AlwaysFleeMons: ; 3c5b1
 	db RAIKOU
 	db ENTEI
-;	db SUICUNE
 	db -1
 ; 3c5b4
 
