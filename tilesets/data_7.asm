c48a048ee45aae75c0e5de4844172e5ec927daf9
Remy Oukaour <remy.oukaour@gmail.com>
2016-05-31 12:29:49 -0400

    Add metatiles/blocks for Routes 47+48.

--- tilesets/data_7.asm
+++ tilesets/data_7.asm
@@ -13,3 +13,15 @@ INCBIN "tilesets/26_metatiles.bin"
 Tileset26Coll: ; 0x1dd5a9
 INCBIN "tilesets/26_collision.bin"
 ; 0x1dd6a9
+
+Tileset20GFX: ; 0x1b43e
+INCBIN "gfx/tilesets/20.2bpp.lz"
+; 0x1b8fe
+
+Tileset20Meta: ; 0x1b8fe
+INCBIN "tilesets/20_metatiles.bin"
+; 0x1bcfe
+
+Tileset20Coll: ; 0x1bcfe
+INCBIN "tilesets/20_collision.bin"
+; 0x1bdfe
