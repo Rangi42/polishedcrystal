60ec5ef2c2c335688708b06c574175a8db50c158
Remy Oukaour <remy.oukaour@gmail.com>
2016-08-13 01:27:10 -0400

    Remove cheat code

--- maps/SeagallopFerryVermilionGate.asm
+++ maps/SeagallopFerryVermilionGate.asm
@@ -33,8 +33,6 @@ SeagallopFerryVermilionGate_PlayerArrives:
 SeagallopFerryVermilionGateSailorScript:
 	faceplayer
 	opentext
-	giveitem MYSTICTICKET
-	giveitem OLD_SEA_MAP
 	checkitem MYSTICTICKET
 	iffalse .MaybeOldSeaMap
 	checkitem OLD_SEA_MAP
