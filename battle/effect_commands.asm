07098286712c458d10c56dc865fe3c2b2d3be43d
Fredrik Ljungdahl <fredde1994@gmail.com>
2016-10-25 10:33:33 +0200

    Make Moonlight and Morning Sun reuse pretty much all code
    Also simplify the function slightly

--- battle/effect_commands.asm
+++ battle/effect_commands.asm
@@ -9198,67 +9198,64 @@ BattleCommand_ClearHazards: ; 37b39
 ; 37b74
 
 
-BattleCommand_HealMornOrDay: ; 37b74
-; healmornorday
-	ld hl, BattleMonMaxHP
-	ld de, BattleMonHP
-	ld a, [hBattleTurn]
-	and a
-	jr z, .start
-	ld hl, EnemyMonMaxHP
-	ld de, EnemyMonHP
-
-.start
-; Index for .Multipliers
-; Default restores half max HP.
-	ld c, 2
-
-; Don't bother healing if HP is already full.
-	push bc
-	call StringCmp
-	pop bc
-	jr z, .Full
-
+BattleCommand_HealMornOrDay:
+	ld d, 0
+	jr BattleCommand_HealTime
+BattleCommand_HealNite:
+	ld d, 1
+BattleCommand_HealTime:
+; d contains default state: 1 = lesser heal. Reverses during night
 ; Don't factor in time of day in link battles.
 	ld a, [wLinkMode]
 	and a
-	jr nz, .Weather
-
+	ld a, 0
+	jr nz, .timecheck_ok
 	ld a, [TimeOfDay]
 	cp NITE
-	jr nz, .Weather
-	dec c ; double
+	ld a, d
+	jr nz, .timecheck_ok
+	; on nighttime, default state is reversed
+	xor 1
+.timecheck_ok
+	add 2
+	ld d, a
+	; d=1: heal 100%, d=2: heal 50%, d=3: heal 25%, d=4: heal 12.5%
 
-.Weather:
+	farcall CheckFullHP_b
+	ld a, b
+	and a
+	jr z, .full
+
 	ld a, [Weather]
 	and a
-	jr z, .Heal
+	jr z, .heal
 
-; x2 in sun
-; /2 in rain/sandstorm/hail
-	inc c
+; Heal amount doubles in sun, halves in any other active weather
+	dec d
 	cp WEATHER_SUN
-	jr z, .Heal
-	dec c
-	dec c
+	jr z, .heal
+	inc d
+	inc d
 
-.Heal:
-	ld b, 0
-	ld hl, .Multipliers
-	add hl, bc
-	add hl, bc
-
-	ld a, [hli]
-	ld h, [hl]
-	ld l, a
-	ld a, BANK(GetMaxHP)
-	rst FarCall
-
+.heal
 	call AnimateCurrentMove
+
+	farcall GetMaxHP
+.loop
+	dec d
+	jr z, .done
+	srl b
+	rr c
+	jr .loop
+.done
+	; minimum healing cap is 1
+	ld a, c
+	or b
+	jr nz, .amount_ok
+	inc c
+.amount_ok
 	call BattleCommand_SwitchTurn
-
 	farcall RestoreHP
-
 	call BattleCommand_SwitchTurn
 	call UpdateUserInParty
 
@@ -9266,102 +9263,10 @@ BattleCommand_HealMornOrDay: ; 37b74
 	ld hl, RegainedHealthText
 	jp StdBattleTextBox
 
-.Full:
+.full
 	call AnimateFailedMove
-
-; 'hp is full!'
 	ld hl, HPIsFullText
 	jp StdBattleTextBox
-
-.Multipliers:
-	dw GetEighthMaxHP
-	dw GetQuarterMaxHP
-	dw GetHalfMaxHP
-	dw GetMaxHP
-; 37b78
-
-BattleCommand_HealNite: ; 37b7c
-; healnite
-	ld hl, BattleMonMaxHP
-	ld de, BattleMonHP
-	ld a, [hBattleTurn]
-	and a
-	jr z, .start
-	ld hl, EnemyMonMaxHP
-	ld de, EnemyMonHP
-
-.start
-; Index for .Multipliers
-; Default restores half max HP.
-	ld c, 2
-
-; Don't bother healing if HP is already full.
-	push bc
-	call StringCmp
-	pop bc
-	jr z, .Full
-
-; Don't factor in time of day in link battles.
-	ld a, [wLinkMode]
-	and a
-	jr nz, .Weather
-
-	ld a, [TimeOfDay]
-	cp NITE
-	jr z, .Weather
-	dec c ; double
-
-.Weather:
-	ld a, [Weather]
-	and a
-	jr z, .Heal
-
-; x2 in sun
-; /2 in rain/sandstorm/hail
-	inc c
-	cp WEATHER_SUN
-	jr z, .Heal
-	dec c
-	dec c
-
-.Heal:
-	ld b, 0
-	ld hl, .Multipliers
-	add hl, bc
-	add hl, bc
-
-	ld a, [hli]
-	ld h, [hl]
-	ld l, a
-	ld a, BANK(GetMaxHP)
-	rst FarCall
-
-	call AnimateCurrentMove
-	call BattleCommand_SwitchTurn
-
-	farcall RestoreHP
-
-	call BattleCommand_SwitchTurn
-	call UpdateUserInParty
-
-; 'regained health!'
-	ld hl, RegainedHealthText
-	jp StdBattleTextBox
-
-.Full:
-	call AnimateFailedMove
-
-; 'hp is full!'
-	ld hl, HPIsFullText
-	jp StdBattleTextBox
-
-.Multipliers:
-	dw GetEighthMaxHP
-	dw GetQuarterMaxHP
-	dw GetHalfMaxHP
-	dw GetMaxHP
-; 37be8
-
 
 BattleCommand_HiddenPower: ; 37be8
 ; hiddenpower
