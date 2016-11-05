d4dcde6410e1e897509b0c412ad5ee2feafc0f8b
Remy Oukaour <remy.oukaour@gmail.com>
2016-11-04 21:50:16 -0400

    Remove more unused code

--- event/battle_tower.asm
+++ event/battle_tower.asm
@@ -4,79 +4,20 @@ Special_BattleTower_InitChallengeRAM: ; 1700b0
 	ld [wNrOfBeatenBattleTowerTrainers], a
 	ld [wcf65], a
 	ld [wcf66], a
-	ld [wcd38], a
 
 	di
 	ld a, [rIE]
-	ld [wcd32], a
+	ld [wrIEBackup], a
 	xor a
 	ld [rIF], a
 	ld [wc300], a
-	ld [wc301], a
-	ld [wc302], a
-	ld [wcd80], a
-	ld [wcd65], a
-	ld [wcd66], a
-	ld [wcd67], a
-	ld [wcd68], a
-	ld [$c31a], a
-	ld [wcd89], a
-	ld [wcd8a], a
-	ld [wcd8b], a
-	ld [wc3ec], a
-	ld [wc3ed], a
-	ld [wc3ee], a
-	ld [wc3ef], a
 	ld hl, VramState
 	ld a, [hl]
-	ld [wcd7f], a
+	ld [wVramStateBackup], a
 	set 1, [hl]
 	ld a, $f
 	ld [rIE], a
 	ei
-
-	xor a
-	ld [$c319], a
-	ld [wc310], a
-	ld [wc311], a
-	ld [wc312], a
-	ld [wc313], a
-	ld [wc314], a
-	ld [wc314 + 1], a
-	ld [wc314 + 4], a
-	ld [wc3f6], a
-	ld [wc3f8], a
-	ld [wc3f2], a
-	ld [wc3f4], a
-
-	ld a, $24
-	ld [wc3f5], a
-
-	ld a, $7
-	ld [wc3f7], a
-
-	ld a, $b0
-	ld [wc30e], a
-	ld [wc3f1], a
-
-	ld a, $48
-	ld [wc30f], a
-	ld [wc3f3], a
-
-	ld a, $5
-	call GetSRAMBank
-	xor a
-	ld [$bfff], a
-	call CloseSRAM
-
-	ld a, $3
-	ld [wcd33], a
-
-	ld a, $d
-	ld [wcd34], a
-
-	ld a, $4
-	ld [wc3f0], a
 
 	ld a, [rSVBK]
 	push af
@@ -122,11 +63,11 @@ Special_BattleTower_InitChallengeRAM: ; 1700b0
 	ld [hVBlank], a
 	xor a
 	ld [rIF], a
-	ld a, [wcd32]
+	ld a, [wrIEBackup]
 	ld [rIE], a
 	ei
 
-	ld a, [wcd7f]
+	ld a, [wVramStateBackup]
 	ld [VramState], a
 
 	ld a, [wc300] ; saved value of wBTChoiceOfLvlGroup
