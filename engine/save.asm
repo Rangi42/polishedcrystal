fe07c11057160f57d4ca87acb22ee4dabe79e18d
Remy Oukaour <remy.oukaour@gmail.com>
2016-11-06 15:31:01 -0500

    Correct initial options

--- engine/save.asm
+++ engine/save.asm
@@ -693,7 +693,7 @@ DefaultOptions: ; 14f7c
 	db $01       ; TextBoxFlags: ?
 	db $0        ; ???
 	db %00000000 ; Options2: running shoes off, 12-hour clock, imperial units
-	db %11100000 ; EarlyGameOptions: natures on, abilities on, traded behavior on,
+	db %00000111 ; EarlyGameOptions: natures on, abilities on, traded behavior on,
 	             ;                   nuzlocke mode off
 	db $0        ; ???
 ; 14f84
