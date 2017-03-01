a72d6a1f92f8f9dcdeb588defc45a649562a40c6
Mero-Mero <nsmbfsp3@gmail.com>
2017-03-01 11:56:47 +0100

    Added Focus Blast animation

--- battle/objects/data.asm
+++ battle/objects/data.asm
@@ -1,8 +1,17 @@
 BattleAnimObjects: ; ccb56
 battleanimobj: MACRO
-
-; ??, ??, frameset, function, ??, tile offset
-	db \1, \2, \3, \4, \5, \6
+	db \1 ; flags
+	; bit 7: priority
+		  ; bit 6: y flip
+		  ; bit 5: x flip
+	; bit 0: enable enemy animation coord fixing (x = $b4 - x; see below for y)
+	db \2 ; enemy animation y fix param
+	; if $FF: y = y + 5 tiles
+		  ; else:   y = -y - (1 tile) * (is_softboiled_animation)
+	db \3 ; video sequence
+	db \4 ; callback
+	db \5 ; palette
+	db \6 ; tile offset
 endm
 
 
@@ -578,5 +587,8 @@ endm
 
 ; ANIM_OBJ_GEM
 	battleanimobj $01, $90, BATTLEANIMFRAMESET_14, BATTLEANIMFUNC_01, 3, $0a
+	
+; ANIM_OBJ_FOCUS_BLAST
+	battleanimobj $01, $88, BATTLEANIMFRAMESET_44, BATTLEANIMFUNC_00, 3, $12
 
 ; ccfbe
