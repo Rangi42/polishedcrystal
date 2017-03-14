6066f786c25af4b45f60c3c47eae0b0af94ff064
Fredrik Ljungdahl <fredde1994@gmail.com>
2017-03-14 13:51:03 +0100

    Formatting

--- battle/core.asm
+++ battle/core.asm
@@ -906,15 +906,15 @@ GetMovePriority: ; 3c5c5
 
 
 MoveEffectPriorities: ; 3c5df
-	db PROTECT,      4
-	db ENDURE,       4
-	db EXTREMESPEED, 2
-	db SUCKER_PUNCH, 1
-	db BULLET_PUNCH, 1
-	db ICE_SHARD,    1
-	db MACH_PUNCH,   1
-	db QUICK_ATTACK, 1
-	; everything else at 0
+	db PROTECT,       4
+	db ENDURE,        4
+	db EXTREMESPEED,  2
+	db SUCKER_PUNCH,  1
+	db BULLET_PUNCH,  1
+	db ICE_SHARD,     1
+	db MACH_PUNCH,    1
+	db QUICK_ATTACK,  1
+	; everything else 0
 	db AVALANCHE,    -4
 	db COUNTER,      -5
 	db MIRROR_COAT,  -5
