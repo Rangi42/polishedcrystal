c64e53583fa9338c51cdf63675e8bbf6529fd63b
Remy Oukaour <remy.oukaour@gmail.com>
2017-05-25 08:31:01 -0400

    HGSS crystals in Cerulean Cave

--- tilesets/cave_collision.asm
+++ tilesets/cave_collision.asm
@@ -86,7 +86,7 @@
 	tilecoll FLOOR, FLOOR, WALL, WALL
 	tilecoll FLOOR, FLOOR, FLOOR, FLOOR
 	tilecoll FLOOR, FLOOR, WALL, WALL
-	tilecoll TOP_WALL, WALL, LADDER, WALL
+	tilecoll LADDER, WALL, FLOOR, FLOOR
 	tilecoll WALL, WALL, WALL, WALL
 	tilecoll FLOOR, FLOOR, FLOOR, FLOOR
 	tilecoll WALL, WALL, WALL, WALL
@@ -206,5 +206,23 @@
 	tilecoll WALL, FLOOR, WALL, WALL
 	tilecoll FLOOR, FLOOR, FLOOR, WALL
 	tilecoll FLOOR, WALL, FLOOR, WALL
+	tilecoll WALL, WALL, WALL, FLOOR
+	tilecoll WALL, WALL, FLOOR, FLOOR
+	tilecoll WALL, WALL, FLOOR, WALL
 	tilecoll WALL, TOP_WALL, WALL, FLOOR
+	tilecoll WALL, FLOOR, WALL, FLOOR
+	tilecoll WALL, WALL, WALL, LADDER
+	tilecoll FLOOR, WALL, FLOOR, WALL
 	tilecoll TOP_WALL, WALL, FLOOR, WALL
+	tilecoll WALL, FLOOR, WALL, WALL
+	tilecoll FLOOR, FLOOR, WALL, WALL
+	tilecoll FLOOR, WALL, WALL, WALL
+	tilecoll WALL, WALL, WALL, WALL
+	tilecoll WALL, FLOOR, FLOOR, FLOOR
+	tilecoll FLOOR, FLOOR, WALL, FLOOR
+	tilecoll FLOOR, WALL, FLOOR, FLOOR
+	tilecoll FLOOR, FLOOR, FLOOR, WALL
+	tilecoll WALL, WALL, WALL, WALL
+	tilecoll WALL, WALL, WALL, WALL
+	tilecoll WALL, WALL, WALL, WALL
+	tilecoll WALL, WALL, WALL, WALL
