e5fcabed14ac3969ac16df740447d6cbbfa3c81f
Remy Oukaour <remy.oukaour@gmail.com>
2017-06-04 14:57:44 -0400

    Thief always works when possible

--- battle/effects/thief.asm
+++ battle/effects/thief.asm
@@ -1,80 +1,58 @@
 BattleCommand_Thief: ; 37492
 ; thief
 
-; The enemy can't steal items.
+; Can't steal during link battles.
+	ld a, [wLinkMode]
+	and a
+	ret nz
 
+; The enemy can't steal items.
 	ld a, [hBattleTurn]
 	and a
 	ret nz
 
 ; The player needs to be able to steal an item.
-
-	call .playeritem
-	ld a, [hl]
+	ld a, [BattleMonItem]
 	and a
 	ret nz
 
-; Sticky Hold prevents item theft.
-
-	call GetOpponentAbilityAfterMoldBreaker
-	cp STICKY_HOLD
-	ret z
-
 ; The enemy needs to have an item to steal.
-
-	call .enemyitem
-	ld a, [hl]
+	ld a, [EnemyMonItem]
 	and a
 	ret z
 
 ; Can't steal mail.
-
 	ld [wd265], a
 	ld d, a
 	farcall ItemIsMail
 	ret c
 
+; Sticky Hold prevents item theft.
+	call GetOpponentAbilityAfterMoldBreaker
+	cp STICKY_HOLD
+	ret z
+
+; effectchance may prevent theft.
 	ld a, [EffectFailed]
 	and a
 	ret nz
 
-	ld a, [wLinkMode]
-	and a
-	jr z, .stealenemyitem
-
-	ld a, [wBattleMode]
-	dec a
-	ret z
-
-.stealenemyitem
-	call .enemyitem
+; Take item from enemy.
+	ld a, MON_ITEM
+	call OTPartyAttr
 	xor a
 	ld [hl], a
-	ld [de], a
+	ld [EnemyMonItem], a
 
-	call .playeritem
+; Give item to player.
+	ld a, MON_ITEM
+	call BattlePartyAttr
 	ld a, [wd265]
 	ld [hl], a
-	ld [de], a
+	ld [BattleMonItem], a
 
 	call GetItemName
 	ld hl, StoleText
 	jp StdBattleTextBox
 
-
-.playeritem
-	ld a, MON_ITEM
-	call BattlePartyAttr
-	ld d, h
-	ld e, l
-	ld hl, BattleMonItem
-	ret
-
-.enemyitem
-	ld a, MON_ITEM
-	call OTPartyAttr
-	ld d, h
-	ld e, l
-	ld hl, EnemyMonItem
-	ret
 ; 37517
