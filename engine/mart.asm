bfc4ff8727ad3c18994caafb5b1c3cf4283ca989
Remy Oukaour <remy.oukaour@gmail.com>
2017-06-28 07:48:23 -0400

    Fix sale prices

--- engine/mart.asm
+++ engine/mart.asm
@@ -128,16 +128,16 @@ RooftopSaleData1: ; 15aee
 	dbw POKE_BALL,     150
 	dbw GREAT_BALL,    500
 	dbw SUPER_POTION,  500
-	dbw FULL_HEAL,     500
+	dbw FULL_HEAL,     300
 	dbw REVIVE,       1200
 	db -1
 RooftopSaleData2: ; 15aff
 	db 5
 	dbw HYPER_POTION, 1000
 	dbw FULL_RESTORE, 2000
-	dbw FULL_HEAL,     500
-	dbw ULTRA_BALL,   1000
-	dbw PROTEIN,      7800
+	dbw FULL_HEAL,     300
+	dbw ULTRA_BALL,    600
+	dbw PROTEIN,      8000
 	db -1
 ; 15b10
 
