a7694b3f17f663b5613435340a2d893cd7d12270
Remy Oukaour <remy.oukaour@gmail.com>
2017-08-07 16:58:02 -0400

    Fix Lyra's bookshelf

--- maps/script_data.asm
+++ maps/script_data.asm
@@ -9,16 +9,16 @@ SECTION "New Bark Scripts", ROMX
 
 INCLUDE "maps/KrissHouse1F.asm"
 INCLUDE "maps/KrissHouse2F.asm"
-INCLUDE "maps/KrissNeighborsHouse.asm"
-INCLUDE "maps/ElmsLab.asm"
-INCLUDE "maps/NewBarkTown.asm"
+INCLUDE "maps/LyrasHouse1F.asm"
+INCLUDE "maps/LyrasHouse2F.asm"
 
 
 SECTION "New Bark Scripts 2", ROMX
 
-INCLUDE "maps/LyrasHouse1F.asm"
-INCLUDE "maps/LyrasHouse2F.asm"
+INCLUDE "maps/NewBarkTown.asm"
 INCLUDE "maps/ElmsHouse.asm"
+INCLUDE "maps/ElmsLab.asm"
+INCLUDE "maps/KrissNeighborsHouse.asm"
 
 
 SECTION "Cherrygrove Scripts", ROMX
