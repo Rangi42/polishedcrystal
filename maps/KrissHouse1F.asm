3da661c460124c7dfc19433223b6ee16e4d418e8
Remy Oukaour <remy.oukaour@gmail.com>
2017-12-22 12:26:40 -0500

    Make debug script optional

--- maps/KrissHouse1F.asm
+++ maps/KrissHouse1F.asm
@@ -55,172 +55,6 @@ MomEventScript:
 	opentext
 	writetext MomIntroText
 	buttonsound
-
-if DEF(DEBUG)
-
-	; full pokegear
-	setflag ENGINE_POKEGEAR
-	setflag ENGINE_PHONE_CARD
-	setflag ENGINE_MAP_CARD
-	setflag ENGINE_RADIO_CARD
-	setflag ENGINE_EXPN_CARD
-	; pokedex
-	setflag ENGINE_POKEDEX
-	; all hms
-	givetmhm HM_CUT
-	givetmhm HM_FLY
-	givetmhm HM_SURF
-	givetmhm HM_STRENGTH
-	givetmhm HM_WATERFALL
-	givetmhm HM_WHIRLPOOL
-	; tms for party
-	givetmhm TM_ICE_BEAM
-	givetmhm TM_BLIZZARD
-	givetmhm TM_FLAMETHROWER
-	givetmhm TM_FIRE_BLAST
-	givetmhm TM_THUNDERBOLT
-	givetmhm TM_THUNDER
-	givetmhm TM_PSYCHIC
-	givetmhm TM_SHADOW_BALL
-	givetmhm TM_EARTHQUAKE
-	givetmhm TM_SWORDS_DANCE
-	givetmhm TM_CALM_MIND
-	givetmhm TM_BULK_UP
-	givetmhm TM_ROCK_SMASH
-	givetmhm TM_FLASH
-	; max money
-	givemoney $0, 1000000
-	givemoney $0, 1000000
-	givemoney $0, 1000000
-	givemoney $0, 1000000
-	givemoney $0, 1000000
-	givemoney $0, 1000000
-	givemoney $0, 1000000
-	givemoney $0, 1000000
-	givemoney $0, 1000000
-	givemoney $0, 999999
-	giveitem COIN_CASE
-	givecoins 50000
-	; all badges
-	setflag ENGINE_ZEPHYRBADGE
-	setflag ENGINE_HIVEBADGE
-	setflag ENGINE_PLAINBADGE
-	setflag ENGINE_FOGBADGE
-	setflag ENGINE_STORMBADGE
-	setflag ENGINE_MINERALBADGE
-	setflag ENGINE_GLACIERBADGE
-	setflag ENGINE_RISINGBADGE
-	setflag ENGINE_BOULDERBADGE
-	setflag ENGINE_CASCADEBADGE
-	setflag ENGINE_THUNDERBADGE
-	setflag ENGINE_RAINBOWBADGE
-	setflag ENGINE_MARSHBADGE
-	setflag ENGINE_SOULBADGE
-	setflag ENGINE_VOLCANOBADGE
-	setflag ENGINE_EARTHBADGE
-	setevent EVENT_BEAT_FALKNER
-	setevent EVENT_BEAT_BUGSY
-	setevent EVENT_BEAT_WHITNEY
-	setevent EVENT_BEAT_MORTY
-	setevent EVENT_BEAT_CHUCK
-	setevent EVENT_BEAT_JASMINE
-	setevent EVENT_BEAT_PRYCE
-	setevent EVENT_BEAT_CLAIR
-	setevent EVENT_BEAT_BROCK
-	setevent EVENT_BEAT_MISTY
-	setevent EVENT_BEAT_LTSURGE
-	setevent EVENT_BEAT_ERIKA
-	setevent EVENT_BEAT_JANINE
-	setevent EVENT_BEAT_SABRINA
-	setevent EVENT_BEAT_BLAINE
-	setevent EVENT_BEAT_BLUE
-	setevent EVENT_BEAT_ELITE_FOUR
-	setevent EVENT_BEAT_ELITE_FOUR_AGAIN
-	setevent EVENT_BATTLE_TOWER_OPEN
-	clearevent EVENT_BATTLE_TOWER_CLOSED
-	; fly anywhere
-	setflag ENGINE_FLYPOINT_NEW_BARK
-	setflag ENGINE_FLYPOINT_CHERRYGROVE
-	setflag ENGINE_FLYPOINT_VIOLET
-	setflag ENGINE_FLYPOINT_UNION_CAVE
-	setflag ENGINE_FLYPOINT_AZALEA
-	setflag ENGINE_FLYPOINT_GOLDENROD
-	setflag ENGINE_FLYPOINT_ECRUTEAK
-	setflag ENGINE_FLYPOINT_OLIVINE
-	setflag ENGINE_FLYPOINT_CIANWOOD
-	setflag ENGINE_FLYPOINT_MAHOGANY
-	setflag ENGINE_FLYPOINT_LAKE_OF_RAGE
-	setflag ENGINE_FLYPOINT_BLACKTHORN
-	setflag ENGINE_FLYPOINT_SILVER_CAVE
-	setflag ENGINE_FLYPOINT_INDIGO_PLATEAU
-	setflag ENGINE_FLYPOINT_PALLET
-	setflag ENGINE_FLYPOINT_VIRIDIAN
-	setflag ENGINE_FLYPOINT_PEWTER
-	setflag ENGINE_FLYPOINT_MT_MOON
-	setflag ENGINE_FLYPOINT_CERULEAN
-	setflag ENGINE_FLYPOINT_VERMILION
-	setflag ENGINE_FLYPOINT_CELADON
-	setflag ENGINE_FLYPOINT_ROCK_TUNNEL
-	setflag ENGINE_FLYPOINT_LAVENDER
-	setflag ENGINE_FLYPOINT_FUCHSIA
-	setflag ENGINE_FLYPOINT_SAFFRON
-	setflag ENGINE_FLYPOINT_CINNABAR
-	; magnet train works
-	setevent EVENT_RESTORED_POWER_TO_KANTO
-	giveitem PASS
-	; cycling road works
-	giveitem BICYCLE
-	; useful items
-	giveitem ITEMFINDER
-	giveitem SQUIRTBOTTLE
-	giveitem MYSTICTICKET
-	giveitem OLD_SEA_MAP
-	giveitem MAX_REPEL, 99
-	giveitem MAX_REVIVE, 99
-	giveitem FULL_RESTORE, 99
-	giveitem MAX_ELIXER, 99
-	giveitem RARE_CANDY, 99
-	giveitem ESCAPE_ROPE, 99
-	giveitem SILVER_LEAF, 99
-	giveitem GOLD_LEAF, 99
-	giveitem HP_UP, 99
-	giveitem PP_UP, 99
-	giveitem PROTEIN, 99
-	giveitem IRON, 99
-	giveitem CARBOS, 99
-	giveitem CALCIUM, 99
-	giveitem ZINC, 99
-	giveitem MASTER_BALL, 99
-	giveitem EXP_SHARE, 2
-	setflag ENGINE_CREDITS_SKIP
-	giveitem SHINY_CHARM
-	setflag ENGINE_HAVE_SHINY_CHARM
-	; good party
-	givepoke MEWTWO, 100, LEFTOVERS
-	givepoke LUGIA, 100, WISE_GLASSES
-	givepoke HO_OH, 100, MUSCLE_BAND
-	; hm slaves
-	givepoke MEW, 100, LEFTOVERS
-	givepoke MEW, 100, LEFTOVERS
-	callasm TeachHMSlaveMoves
-	; pokedex
-	callasm FillPokedex
-	; intro events
-	addcellnum PHONE_MOM
-;	; prof.elm events
-;	addcellnum PHONE_ELM
-;	setevent EVENT_GOT_CYNDAQUIL_FROM_ELM
-;	setevent EVENT_CYNDAQUIL_POKEBALL_IN_ELMS_LAB
-;	setevent EVENT_CHIKORITA_POKEBALL_IN_ELMS_LAB
-;	setevent EVENT_GOT_A_POKEMON_FROM_ELM
-;	setevent EVENT_RIVAL_CHERRYGROVE_CITY
-;	setevent EVENT_LYRA_IN_ELMS_LAB
-;	domaptrigger ELMS_LAB, $5
-;	domaptrigger NEW_BARK_TOWN, $2
-	dotrigger $1
-
-else
-
 	stringtotext GearName, $1
 	callstd receiveitem
 	setflag ENGINE_POKEGEAR
@@ -240,7 +74,6 @@ else
 	yesorno
 	iffalse .InitialSetDSTFlag
 	jump .InitializedDSTFlag
-
 .NotDST:
 	special Special_InitialClearDSTFlag
 	yesorno
@@ -254,76 +87,13 @@ else
 .NoInstructions:
 	writetext MomOutroText
 	waitbutton
-
-endc
-
 	closetext
 	spriteface KRISSHOUSE1F_MOM1, LEFT
 	special RestartMapMusic
 	end
 
-if DEF(DEBUG)
-
-TeachHMSlaveMoves:
-	ld hl, PartyMon4Moves
-	ld a, FLY
-	ld [hli], a
-	ld a, SURF
-	ld [hli], a
-	ld a, STRENGTH
-	ld [hli], a
-	ld a, CUT
-	ld [hl], a
-	ld hl, PartyMon4PP
-	ld a, 15
-	ld [hli], a
-;	ld a, 15
-	ld [hli], a
-;	ld a, 15
-	ld [hli], a
-	ld a, 30
-	ld [hl], a
-	ld hl, PartyMon5Moves
-	ld a, FLASH
-	ld [hli], a
-	ld a, ROCK_SMASH
-	ld [hli], a
-	ld a, WHIRLPOOL
-	ld [hli], a
-	ld a, WATERFALL
-	ld [hl], a
-	ld hl, PartyMon5PP
-	ld a, 20
-	ld [hli], a
-	ld a, 15
-	ld [hli], a
-;	ld a, 15
-	ld [hli], a
-;	ld a, 15
-	ld [hl], a
-	ret
-
-FillPokedex:
-	ld a, 1
-	ld [wFirstUnownSeen], a
-	ld [wFirstMagikarpSeen], a
-	ld hl, PokedexSeen
-	call .Fill
-	ld hl, PokedexCaught
-.Fill:
-	ld a, %11111111
-	ld bc, 31 ; 001-248
-	call ByteFill
-	ld a, %00011111
-	ld [hl], a ; 249-253
-	ret
-
-else
-
 GearName:
 	db "#gear@"
-
-endc
 
 FridgeText:
 	text "Let's see what's"
