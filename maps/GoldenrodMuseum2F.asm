3625b7cd1496a666bee40340f920dcbdb29e3419
Rangi <remy.oukaour+rangi42@gmail.com>
2018-02-08 12:36:25 -0500

    Paintings face inwards

--- maps/GoldenrodMuseum2F.asm
+++ maps/GoldenrodMuseum2F.asm
@@ -9,26 +9,15 @@ GoldenrodMuseum2F_MapScriptHeader:
 	db 0 ; coord events
 
 	db 6 ; bg events
-	bg_event  1,  0, SIGNPOST_READ, HoOhPaintingScript
-	bg_event  2,  0, SIGNPOST_READ, HoOhPaintingScript
+	bg_event  1,  0, SIGNPOST_READ, LugiaPaintingScript
+	bg_event  2,  0, SIGNPOST_READ, LugiaPaintingScript
 	bg_event  4,  0, SIGNPOST_READ, BellTowerPaintingScript
 	bg_event  5,  0, SIGNPOST_READ, BellTowerPaintingScript
-	bg_event  7,  0, SIGNPOST_READ, LugiaPaintingScript
-	bg_event  8,  0, SIGNPOST_READ, LugiaPaintingScript
+	bg_event  7,  0, SIGNPOST_READ, HoOhPaintingScript
+	bg_event  8,  0, SIGNPOST_READ, HoOhPaintingScript
 
 	db 1 ; object events
 	object_event  6,  3, SPRITE_BIG_LAPRAS, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_ROCK, PERSONTYPE_COMMAND, jumptext, GoldenrodMuseum2FBigPearlText, -1
-
-HoOhPaintingScript:
-	refreshscreen
-	paintingpic HO_OH_PAINTING
-	waitbutton
-	closepokepic
-	thistext
-
-	text "   The Phoenix"
-	line "    Ascending"
-	done
 
 LugiaPaintingScript:
 	refreshscreen
@@ -39,6 +28,17 @@ LugiaPaintingScript:
 
 	text "  The Leviathan"
 	line "     Awakens"
+	done
+
+HoOhPaintingScript:
+	refreshscreen
+	paintingpic HO_OH_PAINTING
+	waitbutton
+	closepokepic
+	thistext
+
+	text "   The Phoenix"
+	line "    Ascending"
 	done
 
 BellTowerPaintingScript:
