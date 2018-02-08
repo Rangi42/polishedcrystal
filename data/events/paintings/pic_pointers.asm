f8f67eb12255dccda08d72ff2ac6d515037e0b73
Rangi <remy.oukaour+rangi42@gmail.com>
2018-02-08 13:27:17 -0500

    More museum exhibits

--- data/events/paintings/pic_pointers.asm
+++ data/events/paintings/pic_pointers.asm
@@ -2,3 +2,7 @@ PaintingPicPointers::
 	dba HoOhPaintingPic
 	dba LugiaPaintingPic
 	dba BellTowerPaintingPic
+	dba KabutoPuzzlePic
+	dba OmanytePuzzlePic
+	dba AerodactylPuzzlePic
+	dba HoOhPuzzlePic
