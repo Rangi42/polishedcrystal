f0af974c25a33fd2cce5c5e128e769eff6838a49
Fredrik Ljungdahl <fredde1994@gmail.com>
2019-01-14 03:38:04 +0100

    Low Kick base power now depends on weight
    Fixes #214

--- engine/battle/effect_commands.asm
+++ engine/battle/effect_commands.asm
@@ -8575,12 +8575,65 @@ BattleCommand_GyroBall:
 	ret
 
 BattleCommand_LowKick:
-	; todo
 	push bc
 	push de
+	ld a, [hBattleTurn]
+	and a
+	ld hl, BattleMonSpecies
+	jr z, .got_species
+	ld hl, EnemyMonSpecies
+.got_species
+	ld a, [hl]
+	dec a
+	ld e, a
+	ld d, 0
+	add hl, de
+	add hl, de
+	ld a, BANK(PokedexDataPointerTable)
+	call GetFarHalfword
+
+	; skip the pokémon "type" (seed for bulbasaur, genetic for mewtwo, etc)
+.loop
+	call GetPokedexEntryBank
+	call GetFarByte
+	inc hl
+	cp "@"
+	jr nz, .loop
+
+	; skip height by inc hl twice
+	call GetPokedexEntryBank
+	push bc
+	inc hl
+	inc hl
+	call GetFarHalfword ; now we have weight in hl
+	ld d, h
+	ld e, l
+
+	ld hl, .WeightTable
+.loop2
+	ld a, [hli]
+	ld c, a
+	ld a, [hli]
+	ld b, a
+	ld a, [hli]
+	sub e
+	ld a, b
+	sbc d
+	jr nc, .loop2
+.got_power
 	pop de
+	ld d, c
 	pop bc
 	ret
+
+.WeightTable
+	;    BP, weight
+	dbw 120, 4407
+	dbw 100, 2202
+	dbw  80, 1100
+	dbw  60, 550
+	dbw  40, 218
+	dbw  20, 0
 
 CheckAnyOtherAliveMons:
 ; These return nz if any is alive
