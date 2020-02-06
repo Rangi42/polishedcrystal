e19911ae91af8bd8e3772290b373151c077fb592
Fredrik Ljungdahl <fredde1994@gmail.com>
2020-02-06 22:47:28 +0100

    Move the gfx part of mail back to gfx/, chmod fix

---
+++ gfx/mail.asm
@@ -0,0 +1,132 @@
+MorphMailDividerGFX:
+INCBIN "gfx/mail/0b9926.1bpp"
+
+BlueSkyMailGrassGFX:
+INCBIN "gfx/mail/0b992e.1bpp"
+
+PortraitMailSmallPokeballGFX:
+INCBIN "gfx/mail/0b9936.1bpp"
+
+MorphMailBorderCorner2GFX:
+INCBIN "gfx/mail/0b993e.1bpp"
+
+MusicMailSmallNoteGFX:
+INCBIN "gfx/mail/0b9946.1bpp"
+
+SurfMailWaveGFX:
+INCBIN "gfx/mail/0b994e.1bpp"
+
+PortraitMailUnderlineGFX:
+INCBIN "gfx/mail/0b995e.1bpp"
+
+LovelyMailUnderlineGFX:
+INCBIN "gfx/mail/0b9966.1bpp"
+
+LovelyMailSmallHeartGFX:
+INCBIN "gfx/mail/0b996e.1bpp"
+
+SurfLiteBlueMailSmallShapesGFX:
+INCBIN "gfx/mail/0b9976.1bpp"
+
+EonMailBorder1GFX:
+INCBIN "gfx/mail/0b997e.1bpp"
+
+EonMailBorder2GFX:
+INCBIN "gfx/mail/0b998e.1bpp"
+
+MailNatuGFX:
+INCBIN "gfx/mail/0b9996.1bpp"
+
+MailDratiniGFX:
+INCBIN "gfx/mail/0b99c6.1bpp"
+
+MailPoliwagGFX:
+INCBIN "gfx/mail/0b99f6.1bpp"
+
+MailLaprasGFX:
+INCBIN "gfx/mail/0b9a26.1bpp"
+
+MailEeveeGFX:
+INCBIN "gfx/mail/0b9a56.1bpp"
+
+MailDittoGFX:
+INCBIN "gfx/mail/0b9a86.1bpp"
+
+MailMewGFX:
+INCBIN "gfx/mail/0b9ab6.1bpp"
+
+MailDragoniteGFX:
+INCBIN "gfx/mail/0b9b46.1bpp"
+
+; unused?
+MailSentretGFX:
+INCBIN "gfx/mail/0b9bce.1bpp"
+
+PortraitMailLargePokeballGFX:
+INCBIN "gfx/mail/0b9bfe.1bpp"
+
+MailOddishGFX:
+INCBIN "gfx/mail/0b9c1e.1bpp"
+
+SurfLiteBlueMailLargeShapesGFX:
+INCBIN "gfx/mail/0b9c3e.1bpp"
+
+LovelyMailLargeHeartGFX:
+INCBIN "gfx/mail/0b9c5e.1bpp"
+
+MorphMailBorderCorner1GFX:
+INCBIN "gfx/mail/0b9c7e.1bpp"
+
+MorphMailBorderGFX:
+INCBIN "gfx/mail/0b9c96.1bpp"
+
+MailLargeCircleGFX:
+INCBIN "gfx/mail/0b9c9e.1bpp"
+
+FlowerMailLargeFlowerGFX:
+INCBIN "gfx/mail/0b9cbe.1bpp"
+;FlowerMailSmallFlowerGFX:
+; indirectly referenced
+INCBIN "gfx/mail/0b9cde.1bpp"
+
+MusicMailLargeNoteGFX:
+INCBIN "gfx/mail/0b9cfe.1bpp"
+
+MailCloudGFX:
+INCBIN "gfx/mail/0b9d16.1bpp"
+
+MailCloud2GFX:
+INCBIN "gfx/mail/0b9d26.1bpp"
+
+MailCloud3GFX:
+INCBIN "gfx/mail/0b9d3e.1bpp"
+
+SurfMailBorderGFX:
+INCBIN "gfx/mail/0b9d46.1bpp"
+
+EonMailBorder3GFX:
+INCBIN "gfx/mail/0b9d76.1bpp"
+
+FlowerMailBorderGFX:
+INCBIN "gfx/mail/0b9d86.1bpp"
+
+FlowerMailSenderUnderlineGFX:
+INCBIN "gfx/mail/0b9db6.1bpp"
+
+LiteBlueMailBorderGFX:
+INCBIN "gfx/mail/0b9dc6.1bpp"
+
+MirageMailTopDividerGFX:
+INCBIN "gfx/mail/0b9dce.1bpp"
+
+MirageMailBottomDividerGFX:
+INCBIN "gfx/mail/0b9df6.1bpp"
+
+MusicMailBorderGFX:
+INCBIN "gfx/mail/0b9e06.1bpp"
+
+LovelyMailBorderGFX:
+INCBIN "gfx/mail/0b9e26.1bpp"
+
+PortraitMailBorderGFX:
+INCBIN "gfx/mail/0b9e4e.1bpp"
