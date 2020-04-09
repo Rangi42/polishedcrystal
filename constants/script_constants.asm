dfe6d240c96419c7bce71d4ff9775ad750485056
itsdarsh <twitteruserdarsh@gmail.com>
2020-04-09 12:42:51 -0400

    fix spacing and constant values

--- constants/script_constants.asm
+++ constants/script_constants.asm
@@ -70,11 +70,11 @@ NUM_PLAYER_EVENTS EQU const_value
 	const PLAYERMOVEMENT_JUMP
 
 ; script data sizes (see macros/scripts/maps.asm)
-SCENE_SCRIPT_SIZE EQU 4 ; scene_script
-CALLBACK_SIZE     EQU 3 ; callback
-WARP_EVENT_SIZE   EQU 5 ; warp_event
-COORD_EVENT_SIZE  EQU 8 ; coord_event (TODO: shouldn't this be 5?)
-BG_EVENT_SIZE     EQU 5 ; bg_event
+SCENE_SCRIPT_SIZE EQU 2  ; scene_script
+CALLBACK_SIZE     EQU 3  ; callback
+WARP_EVENT_SIZE   EQU 5  ; warp_event
+COORD_EVENT_SIZE  EQU 5  ; coord_event
+BG_EVENT_SIZE     EQU 5  ; bg_event
 OBJECT_EVENT_SIZE EQU 13 ; object_event
 
 ; see engine/events.asm:TryReadSign.signs
