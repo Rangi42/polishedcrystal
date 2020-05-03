1327638788a276b73f7506d2b73eb0b21be5ec48
Fredrik Ljungdahl <fredde1994@gmail.com>
2020-05-03 21:44:29 +0200

    Fix incorrect Weezing ability distributions

--- data/pokemon/base_stats/weezing_plain.asm
+++ data/pokemon/base_stats/weezing_plain.asm
@@ -9,7 +9,7 @@
 	dn FEMALE_50, 3 ; gender, step cycles to hatch
 	dn 7, 7 ; frontpic dimensions
 	db LEVITATE ; ability 1
-	db LEVITATE ; ability 2
+	db NEUTRALIZING_GAS ; ability 2
 	db STENCH ; hidden ability
 	db MEDIUM_FAST ; growth rate
 	dn AMORPHOUS, AMORPHOUS ; egg groups
