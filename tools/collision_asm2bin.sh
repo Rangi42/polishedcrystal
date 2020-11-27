99c3f4a003b85c965dc263cf747ae51398d2a7a4
Portron Clovis <clovis@synapse-medicine.com>
2020-11-27 18:01:46 +0100

    Updating script to manage posix/gnu head/tail version

--- tools/collision_asm2bin.sh
+++ tools/collision_asm2bin.sh
@@ -3,6 +3,16 @@
 
 TEMP_ASM=$(mktemp collision.asm.XXX)
 TEMP_O=$(mktemp collision.o.XXX)
+OS=$(uname -s)
+
+HEAD=head
+tail=tail
+
+if [ $OS = "Darwin" ]
+then
+    HEAD=ghead
+    TAIL=gtail
+fi
 
 echo 'INCLUDE "constants/collision_constants.asm"' > "$TEMP_ASM"
 echo 'INCLUDE "macros/collision.asm"' >> "$TEMP_ASM"
@@ -10,5 +20,5 @@ echo 'SECTION "C", ROM0[$0]' >> "$TEMP_ASM"
 echo "INCLUDE \"$1\"" >> "$TEMP_ASM"
 
 ${RGBDS_DIR}rgbasm -o "$TEMP_O" "$TEMP_ASM"
-tail -c +37 "$TEMP_O" | head -c -8 > $2
+$TAIL -c +37 "$TEMP_O" | $HEAD -c -8 > $2
 rm -f "$TEMP_ASM" "$TEMP_O"
