2719842b20d618c88939cce092bcec08fbe8a3e2
Rangi <remy.oukaour+rangi42@gmail.com>
2020-12-14 18:36:16 -0500

    Fix collision_asm2bin.sh for rgbds 0.4.2

--- tools/collision_asm2bin.sh
+++ tools/collision_asm2bin.sh
@@ -20,5 +20,5 @@ echo 'SECTION "C", ROM0[$0]' >> "$TEMP_ASM"
 echo "INCLUDE \"$1\"" >> "$TEMP_ASM"
 
 ${RGBDS_DIR}rgbasm -o "$TEMP_O" "$TEMP_ASM"
-$TAIL -c +37 "$TEMP_O" | $HEAD -c -8 > $2
+$TAIL -c +41 "$TEMP_O" | $HEAD -c -8 > $2
 rm -f "$TEMP_ASM" "$TEMP_O"
