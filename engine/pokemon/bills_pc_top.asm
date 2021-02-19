wowjinxy/polishedcrystal-MBC5

Restore map data after PC use

engine/pokemon/bills_pc_top.asm
@@ -7,7 +7,9 @@ _BillsPC:
 	set NO_TEXT_SCROLL, [hl]
 	ld a, 72
 	ldh [rLYC], a
+	call LoadStandardMenuDataHeader
 	call UseBillsPC
+	call ReturnToMapFromSubmenu
 
 	ld hl, rIE
 	res LCD_STAT, [hl]
@@ -33,7 +35,6 @@ _BillsPC:
 	text_end
 
 UseBillsPC:
-	call LoadStandardMenuDataHeader
 	call ClearTileMap
 	call ClearPalettes
 	farcall WipeAttrMap

Rangi42/polishedcrystal

engine/pokemon/bills_pc_top.asm
@@ -7,7 +7,9 @@ _BillsPC:
 	set NO_TEXT_SCROLL, [hl]
 	ld a, 72
 	ldh [rLYC], a
+	call LoadStandardMenuHeader
 	call UseBillsPC
+	call ReturnToMapFromSubmenu
 
 	ld hl, rIE
 	res LCD_STAT, [hl]
@@ -33,7 +35,6 @@ _BillsPC:
 	text_end
 
 UseBillsPC:
-	call LoadStandardMenuHeader
 	call ClearTileMap
 	call ClearPalettes
 	farcall WipeAttrMap