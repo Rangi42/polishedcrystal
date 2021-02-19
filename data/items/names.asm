wowjinxy/polishedcrystal-MBC5

Box UI: Item icon + item VWF

data/items/names.asm
@@ -1,294 +1,294 @@
 ItemNames::
-	db "# Ball@"
-	db "Great Ball@"
-	db "Ultra Ball@"
-	db "Master Ball@"
-	db "Safari Ball@"
-	db "Level Ball@"
-	db "Lure Ball@"
-	db "Moon Ball@"
-	db "Friend Ball@"
-	db "Fast Ball@"
-	db "Heavy Ball@"
-	db "Love Ball@"
-	db "Park Ball@"
-	db "Repeat Ball@"
-	db "Timer Ball@"
-	db "Nest Ball@"
-	db "Net Ball@"
-	db "Dive Ball@"
-	db "Luxury Ball@"
-	db "Heal Ball@"
-	db "Quick Ball@"
-	db "Dusk Ball@"
-	db "Dream Ball@"
-	db "Premier Ball@"
-	db "Cherish Ball@"
-	db "Potion@"
-	db "Super Potion@"
-	db "Hyper Potion@"
-	db "Max Potion@"
-	db "Antidote@"
-	db "Burn Heal@"
-	db "ParalyzeHeal@"
-	db "Awakening@"
-	db "Ice Heal@"
-	db "Full Heal@"
-	db "Full Restore@"
-	db "Revive@"
-	db "Max Revive@"
-	db "Ether@"
-	db "Max Ether@"
-	db "Elixir@"
-	db "Max Elixir@"
-	db "HP Up@"
-	db "Protein@"
-	db "Iron@"
-	db "Carbos@"
-	db "Calcium@"
-	db "Zinc@"
-	db "Rare Candy@"
-	db "PP Up@"
-	db "PP Max@"
-	db "Fresh Water@"
-	db "Soda Pop@"
-	db "Lemonade@"
-	db "Moomoo Milk@"
+	rawchar "# Ball@"
+	rawchar "Great Ball@"
+	rawchar "Ultra Ball@"
+	rawchar "Master Ball@"
+	rawchar "Safari Ball@"
+	rawchar "Level Ball@"
+	rawchar "Lure Ball@"
+	rawchar "Moon Ball@"
+	rawchar "Friend Ball@"
+	rawchar "Fast Ball@"
+	rawchar "Heavy Ball@"
+	rawchar "Love Ball@"
+	rawchar "Park Ball@"
+	rawchar "Repeat Ball@"
+	rawchar "Timer Ball@"
+	rawchar "Nest Ball@"
+	rawchar "Net Ball@"
+	rawchar "Dive Ball@"
+	rawchar "Luxury Ball@"
+	rawchar "Heal Ball@"
+	rawchar "Quick Ball@"
+	rawchar "Dusk Ball@"
+	rawchar "Dream Ball@"
+	rawchar "Premier Ball@"
+	rawchar "Cherish Ball@"
+	rawchar "Potion@"
+	rawchar "Super Potion@"
+	rawchar "Hyper Potion@"
+	rawchar "Max Potion@"
+	rawchar "Antidote@"
+	rawchar "Burn Heal@"
+	rawchar "ParalyzeHeal@"
+	rawchar "Awakening@"
+	rawchar "Ice Heal@"
+	rawchar "Full Heal@"
+	rawchar "Full Restore@"
+	rawchar "Revive@"
+	rawchar "Max Revive@"
+	rawchar "Ether@"
+	rawchar "Max Ether@"
+	rawchar "Elixir@"
+	rawchar "Max Elixir@"
+	rawchar "HP Up@"
+	rawchar "Protein@"
+	rawchar "Iron@"
+	rawchar "Carbos@"
+	rawchar "Calcium@"
+	rawchar "Zinc@"
+	rawchar "Rare Candy@"
+	rawchar "PP Up@"
+	rawchar "PP Max@"
+	rawchar "Fresh Water@"
+	rawchar "Soda Pop@"
+	rawchar "Lemonade@"
+	rawchar "Moomoo Milk@"
 if DEF(FAITHFUL)
-	db "RageCandyBar@"
+	rawchar "RageCandyBar@"
 else
-	db "Cake of Rage@"
+	rawchar "Cake of Rage@"
 endc
-	db "PewterCrunch@"
-	db "Sacred Ash@"
-	db "EnergyPowder@"
-	db "Energy Root@"
-	db "Heal Powder@"
-	db "Revival Herb@"
-	db "X Attack@"
-	db "X Defend@"
-	db "X Speed@"
-	db "X Spcl.Atk@"
-	db "X Spcl.Def@"
-	db "X Accuracy@"
-	db "Dire Hit@"
+	rawchar "PewterCrunch@"
+	rawchar "Sacred Ash@"
+	rawchar "EnergyPowder@"
+	rawchar "Energy Root@"
+	rawchar "Heal Powder@"
+	rawchar "Revival Herb@"
+	rawchar "X Attack@"
+	rawchar "X Defend@"
+	rawchar "X Speed@"
+	rawchar "X Spcl.Atk@"
+	rawchar "X Spcl.Def@"
+	rawchar "X Accuracy@"
+	rawchar "Dire Hit@"
 if DEF(FAITHFUL)
-	db "Guard Spec.@"
+	rawchar "Guard Spec.@"
 else
-	db "Guard Stats@"
+	rawchar "Guard Stats@"
 endc
-	db "Repel@"
-	db "Super Repel@"
-	db "Max Repel@"
-	db "Escape Rope@"
-	db "# Doll@"
-	db "Ability Cap@"
-	db "Leaf Stone@"
-	db "Fire Stone@"
-	db "Water Stone@"
-	db "ThunderStone@"
-	db "Moon Stone@"
-	db "Sun Stone@"
-	db "Dusk Stone@"
-	db "Dawn Stone@"
-	db "Shiny Stone@"
-	db "Ice Stone@"
-	db "Everstone@"
-	db "Exp.Share@"
-	db "Cheri Berry@"
-	db "Chesto Berry@"
-	db "Pecha Berry@"
-	db "Rawst Berry@"
-	db "Aspear Berry@"
-	db "Leppa Berry@"
-	db "Oran Berry@"
-	db "Persim Berry@"
-	db "Lum Berry@"
-	db "Sitrus Berry@"
-	db "Figy Berry@"
-	db "Pomeg Berry@"
-	db "Kelpsy Berry@"
-	db "Qualot Berry@"
-	db "Hondew Berry@"
-	db "Grepa Berry@"
-	db "Tamato Berry@"
-	db "Liechi Berry@"
-	db "Ganlon Berry@"
-	db "Salac Berry@"
-	db "Petaya Berry@"
-	db "Apicot Berry@"
-	db "Lansat Berry@"
-	db "Starf Berry@"
-	db "Enigma Berry@"
-	db "Custap Berry@"
-	db "Jaboca Berry@"
-	db "Rowap Berry@"
-	db "Kee Berry@"
-	db "MarangaBerry@"
-	db "Berry Juice@"
-	db "Silk Scarf@"
-	db "Black Belt@"
-	db "Sharp Beak@"
-	db "Poison Barb@"
-	db "Soft Sand@"
-	db "Hard Stone@"
-	db "SilverPowder@"
-	db "Spell Tag@"
-	db "Metal Coat@"
-	db "Charcoal@"
-	db "Mystic Water@"
-	db "Miracle Seed@"
-	db "Magnet@"
-	db "TwistedSpoon@"
-	db "NeverMeltIce@"
-	db "Dragon Fang@"
-	db "BlackGlasses@"
-	db "Pink Bow@"
-	db "BrightPowder@"
-	db "Scope Lens@"
-	db "Quick Claw@"
-	db "King's Rock@"
-	db "Focus Band@"
-	db "Leftovers@"
-	db "Lucky Egg@"
-	db "Amulet Coin@"
-	db "Cleanse Tag@"
-	db "Smoke Ball@"
-	db "Berserk Gene@"
-	db "Light Ball@"
-	db "Stick@"
-	db "Thick Club@"
-	db "Lucky Punch@"
-	db "Metal Powder@"
-	db "Quick Powder@"
-	db "Armor Suit@"
-	db "Air Balloon@"
-	db "Assault Vest@"
-	db "Big Root@"
-	db "Binding Band@"
-	db "Destiny Knot@"
-	db "Eviolite@"
-	db "Expert Belt@"
-	db "Focus Sash@"
-	db "Grip Claw@"
-	db "Life Orb@"
-	db "Light Clay@"
-	db "Metronome@"
-	db "Muscle Band@"
-	db "Protect Pads@"
-	db "Rocky Helmet@"
-	db "Safe Goggles@"
-	db "Shed Shell@"
-	db "Shell Bell@"
-	db "Soothe Bell@"
-	db "Weak Policy@"
-	db "Wide Lens@"
-	db "Wise Glasses@"
-	db "Zoom Lens@"
-	db "Eject Button@"
-	db "Lagging Tail@"
-	db "Iron Ball@"
-	db "Ring Target@"
-	db "Red Card@"
-	db "Absorb Bulb@"
-	db "Cell Battery@"
-	db "LuminousMoss@"
-	db "Snowball@"
-	db "Eject Pack@"
-	db "Room Service@"
-	db "BlundrPolicy@"
-	db "Throat Spray@"
-	db "Heavy Boots@"
-	db "UtilUmbrella@"
-	db "Mental Herb@"
-	db "Power Herb@"
-	db "White Herb@"
-	db "Damp Rock@"
-	db "Heat Rock@"
-	db "Smooth Rock@"
-	db "Icy Rock@"
-	db "Choice Band@"
-	db "Choice Scarf@"
-	db "Choice Specs@"
-	db "Flame Orb@"
-	db "Toxic Orb@"
-	db "Black Sludge@"
-	db "Macho Brace@"
-	db "Power Weight@"
-	db "Power Bracer@"
-	db "Power Belt@"
-	db "Power Lens@"
-	db "Power Band@"
-	db "Power Anklet@"
-	db "Dragon Scale@"
-	db "Up-Grade@"
-	db "Dubious Disc@"
-	db "Protector@"
-	db "Electirizer@"
-	db "Magmarizer@"
-	db "Razor Fang@"
-	db "Razor Claw@"
-	db "Odd Souvenir@"
-	db "Nugget@"
-	db "Big Nugget@"
-	db "TinyMushroom@"
-	db "Big Mushroom@"
-	db "BalmMushroom@"
-	db "Pearl@"
-	db "Big Pearl@"
-	db "Pearl String@"
-	db "Stardust@"
-	db "Star Piece@"
-	db "Brick Piece@"
-	db "Rare Bone@"
-	db "Silver Leaf@"
-	db "Gold Leaf@"
-	db "SlowpokeTail@"
-	db "Bottle Cap@"
-	db "Helix Fossil@"
-	db "Dome Fossil@"
-	db "Old Amber@"
-	db "Mulch@"
-	db "Sweet Honey@"
-	db "Mint@"
-	db "Flower Mail@"
-	db "Surf Mail@"
-	db "LiteBlueMail@"
-	db "PortraitMail@"
-	db "Lovely Mail@"
-	db "Eon Mail@"
-	db "Morph Mail@"
-	db "BlueSky Mail@"
-	db "Music Mail@"
-	db "Mirage Mail@"
+	rawchar "Repel@"
+	rawchar "Super Repel@"
+	rawchar "Max Repel@"
+	rawchar "Escape Rope@"
+	rawchar "# Doll@"
+	rawchar "Ability Cap@"
+	rawchar "Leaf Stone@"
+	rawchar "Fire Stone@"
+	rawchar "Water Stone@"
+	rawchar "ThunderStone@"
+	rawchar "Moon Stone@"
+	rawchar "Sun Stone@"
+	rawchar "Dusk Stone@"
+	rawchar "Dawn Stone@"
+	rawchar "Shiny Stone@"
+	rawchar "Ice Stone@"
+	rawchar "Everstone@"
+	rawchar "Exp.Share@"
+	rawchar "Cheri Berry@"
+	rawchar "Chesto Berry@"
+	rawchar "Pecha Berry@"
+	rawchar "Rawst Berry@"
+	rawchar "Aspear Berry@"
+	rawchar "Leppa Berry@"
+	rawchar "Oran Berry@"
+	rawchar "Persim Berry@"
+	rawchar "Lum Berry@"
+	rawchar "Sitrus Berry@"
+	rawchar "Figy Berry@"
+	rawchar "Pomeg Berry@"
+	rawchar "Kelpsy Berry@"
+	rawchar "Qualot Berry@"
+	rawchar "Hondew Berry@"
+	rawchar "Grepa Berry@"
+	rawchar "Tamato Berry@"
+	rawchar "Liechi Berry@"
+	rawchar "Ganlon Berry@"
+	rawchar "Salac Berry@"
+	rawchar "Petaya Berry@"
+	rawchar "Apicot Berry@"
+	rawchar "Lansat Berry@"
+	rawchar "Starf Berry@"
+	rawchar "Enigma Berry@"
+	rawchar "Custap Berry@"
+	rawchar "Jaboca Berry@"
+	rawchar "Rowap Berry@"
+	rawchar "Kee Berry@"
+	rawchar "MarangaBerry@"
+	rawchar "Berry Juice@"
+	rawchar "Silk Scarf@"
+	rawchar "Black Belt@"
+	rawchar "Sharp Beak@"
+	rawchar "Poison Barb@"
+	rawchar "Soft Sand@"
+	rawchar "Hard Stone@"
+	rawchar "SilverPowder@"
+	rawchar "Spell Tag@"
+	rawchar "Metal Coat@"
+	rawchar "Charcoal@"
+	rawchar "Mystic Water@"
+	rawchar "Miracle Seed@"
+	rawchar "Magnet@"
+	rawchar "TwistedSpoon@"
+	rawchar "NeverMeltIce@"
+	rawchar "Dragon Fang@"
+	rawchar "BlackGlasses@"
+	rawchar "Pink Bow@"
+	rawchar "BrightPowder@"
+	rawchar "Scope Lens@"
+	rawchar "Quick Claw@"
+	rawchar "King's Rock@"
+	rawchar "Focus Band@"
+	rawchar "Leftovers@"
+	rawchar "Lucky Egg@"
+	rawchar "Amulet Coin@"
+	rawchar "Cleanse Tag@"
+	rawchar "Smoke Ball@"
+	rawchar "Berserk Gene@"
+	rawchar "Light Ball@"
+	rawchar "Stick@"
+	rawchar "Thick Club@"
+	rawchar "Lucky Punch@"
+	rawchar "Metal Powder@"
+	rawchar "Quick Powder@"
+	rawchar "Armor Suit@"
+	rawchar "Air Balloon@"
+	rawchar "Assault Vest@"
+	rawchar "Big Root@"
+	rawchar "Binding Band@"
+	rawchar "Destiny Knot@"
+	rawchar "Eviolite@"
+	rawchar "Expert Belt@"
+	rawchar "Focus Sash@"
+	rawchar "Grip Claw@"
+	rawchar "Life Orb@"
+	rawchar "Light Clay@"
+	rawchar "Metronome@"
+	rawchar "Muscle Band@"
+	rawchar "Protect Pads@"
+	rawchar "Rocky Helmet@"
+	rawchar "Safe Goggles@"
+	rawchar "Shed Shell@"
+	rawchar "Shell Bell@"
+	rawchar "Soothe Bell@"
+	rawchar "Weak Policy@"
+	rawchar "Wide Lens@"
+	rawchar "Wise Glasses@"
+	rawchar "Zoom Lens@"
+	rawchar "Eject Button@"
+	rawchar "Lagging Tail@"
+	rawchar "Iron Ball@"
+	rawchar "Ring Target@"
+	rawchar "Red Card@"
+	rawchar "Absorb Bulb@"
+	rawchar "Cell Battery@"
+	rawchar "LuminousMoss@"
+	rawchar "Snowball@"
+	rawchar "Eject Pack@"
+	rawchar "Room Service@"
+	rawchar "BlundrPolicy@"
+	rawchar "Throat Spray@"
+	rawchar "Heavy Boots@"
+	rawchar "UtilUmbrella@"
+	rawchar "Mental Herb@"
+	rawchar "Power Herb@"
+	rawchar "White Herb@"
+	rawchar "Damp Rock@"
+	rawchar "Heat Rock@"
+	rawchar "Smooth Rock@"
+	rawchar "Icy Rock@"
+	rawchar "Choice Band@"
+	rawchar "Choice Scarf@"
+	rawchar "Choice Specs@"
+	rawchar "Flame Orb@"
+	rawchar "Toxic Orb@"
+	rawchar "Black Sludge@"
+	rawchar "Macho Brace@"
+	rawchar "Power Weight@"
+	rawchar "Power Bracer@"
+	rawchar "Power Belt@"
+	rawchar "Power Lens@"
+	rawchar "Power Band@"
+	rawchar "Power Anklet@"
+	rawchar "Dragon Scale@"
+	rawchar "Up-Grade@"
+	rawchar "Dubious Disc@"
+	rawchar "Protector@"
+	rawchar "Electirizer@"
+	rawchar "Magmarizer@"
+	rawchar "Razor Fang@"
+	rawchar "Razor Claw@"
+	rawchar "Odd Souvenir@"
+	rawchar "Nugget@"
+	rawchar "Big Nugget@"
+	rawchar "TinyMushroom@"
+	rawchar "Big Mushroom@"
+	rawchar "BalmMushroom@"
+	rawchar "Pearl@"
+	rawchar "Big Pearl@"
+	rawchar "Pearl String@"
+	rawchar "Stardust@"
+	rawchar "Star Piece@"
+	rawchar "Brick Piece@"
+	rawchar "Rare Bone@"
+	rawchar "Silver Leaf@"
+	rawchar "Gold Leaf@"
+	rawchar "SlowpokeTail@"
+	rawchar "Bottle Cap@"
+	rawchar "Helix Fossil@"
+	rawchar "Dome Fossil@"
+	rawchar "Old Amber@"
+	rawchar "Mulch@"
+	rawchar "Sweet Honey@"
+	rawchar "Mint@"
+	rawchar "Flower Mail@"
+	rawchar "Surf Mail@"
+	rawchar "LiteBlueMail@"
+	rawchar "PortraitMail@"
+	rawchar "Lovely Mail@"
+	rawchar "Eon Mail@"
+	rawchar "Morph Mail@"
+	rawchar "BlueSky Mail@"
+	rawchar "Music Mail@"
+	rawchar "Mirage Mail@"
 
 KeyItemNames::
-	db "Bicycle@"
-	db "Old Rod@"
-	db "Good Rod@"
-	db "Super Rod@"
-	db "Coin Case@"
-	db "Itemfinder@"
-	db "Mystery Egg@"
-	db "SquirtBottle@"
-	db "SecretPotion@"
-	db "Red Scale@"
-	db "Card Key@"
-	db "Basement Key@"
-	db "S.S.Ticket@"
-	db "Pass@"
-	db "Machine Part@"
-	db "Lost Item@"
-	db "Rainbow Wing@"
-	db "Silver Wing@"
-	db "Clear Bell@"
-	db "GS Ball@"
-	db "Blue Card@"
-	db "OrangeTicket@"
-	db "MysticTicket@"
-	db "Old Sea Map@"
-	db "Shiny Charm@"
-	db "Oval Charm@"
-	db "Catch Charm@"
-	db "SilphScope2@"
-	db "Apricorn Box@"
+	rawchar "Bicycle@"
+	rawchar "Old Rod@"
+	rawchar "Good Rod@"
+	rawchar "Super Rod@"
+	rawchar "Coin Case@"
+	rawchar "Itemfinder@"
+	rawchar "Mystery Egg@"
+	rawchar "SquirtBottle@"
+	rawchar "SecretPotion@"
+	rawchar "Red Scale@"
+	rawchar "Card Key@"
+	rawchar "Basement Key@"
+	rawchar "S.S.Ticket@"
+	rawchar "Pass@"
+	rawchar "Machine Part@"
+	rawchar "Lost Item@"
+	rawchar "Rainbow Wing@"
+	rawchar "Silver Wing@"
+	rawchar "Clear Bell@"
+	rawchar "GS Ball@"
+	rawchar "Blue Card@"
+	rawchar "OrangeTicket@"
+	rawchar "MysticTicket@"
+	rawchar "Old Sea Map@"
+	rawchar "Shiny Charm@"
+	rawchar "Oval Charm@"
+	rawchar "Catch Charm@"
+	rawchar "SilphScope2@"
+	rawchar "Apricorn Box@"

Rangi42/polishedcrystal

data/items/names.asm
@@ -1,264 +1,264 @@
 ItemNames::
-	db "Park Ball@"
-	db "# Ball@"
-	db "Great Ball@"
-	db "Ultra Ball@"
-	db "Master Ball@"
-	db "Safari Ball@"
-	db "Level Ball@"
-	db "Lure Ball@"
-	db "Moon Ball@"
-	db "Friend Ball@"
-	db "Fast Ball@"
-	db "Heavy Ball@"
-	db "Love Ball@"
-	db "AbilityPatch@"
-	db "Repeat Ball@"
-	db "Timer Ball@"
-	db "Nest Ball@"
-	db "Net Ball@"
-	db "Dive Ball@"
-	db "Luxury Ball@"
-	db "Heal Ball@"
-	db "Quick Ball@"
-	db "Dusk Ball@"
-	db "Dream Ball@"
-	db "Premier Ball@"
-	db "Cherish Ball@"
-	db "Potion@"
-	db "Super Potion@"
-	db "Hyper Potion@"
-	db "Max Potion@"
-	db "Antidote@"
-	db "Burn Heal@"
-	db "ParalyzeHeal@"
-	db "Awakening@"
-	db "Ice Heal@"
-	db "Full Heal@"
-	db "Full Restore@"
-	db "Revive@"
-	db "Max Revive@"
-	db "Ether@"
-	db "Max Ether@"
-	db "Elixir@"
-	db "Max Elixir@"
-	db "HP Up@"
-	db "Protein@"
-	db "Iron@"
-	db "Carbos@"
-	db "Calcium@"
-	db "Zinc@"
-	db "Rare Candy@"
-	db "PP Up@"
-	db "PP Max@"
-	db "Fresh Water@"
-	db "Soda Pop@"
-	db "Lemonade@"
-	db "Moomoo Milk@"
+	rawchar "Park Ball@"
+	rawchar "# Ball@"
+	rawchar "Great Ball@"
+	rawchar "Ultra Ball@"
+	rawchar "Master Ball@"
+	rawchar "Safari Ball@"
+	rawchar "Level Ball@"
+	rawchar "Lure Ball@"
+	rawchar "Moon Ball@"
+	rawchar "Friend Ball@"
+	rawchar "Fast Ball@"
+	rawchar "Heavy Ball@"
+	rawchar "Love Ball@"
+	rawchar "AbilityPatch@"
+	rawchar "Repeat Ball@"
+	rawchar "Timer Ball@"
+	rawchar "Nest Ball@"
+	rawchar "Net Ball@"
+	rawchar "Dive Ball@"
+	rawchar "Luxury Ball@"
+	rawchar "Heal Ball@"
+	rawchar "Quick Ball@"
+	rawchar "Dusk Ball@"
+	rawchar "Dream Ball@"
+	rawchar "Premier Ball@"
+	rawchar "Cherish Ball@"
+	rawchar "Potion@"
+	rawchar "Super Potion@"
+	rawchar "Hyper Potion@"
+	rawchar "Max Potion@"
+	rawchar "Antidote@"
+	rawchar "Burn Heal@"
+	rawchar "ParalyzeHeal@"
+	rawchar "Awakening@"
+	rawchar "Ice Heal@"
+	rawchar "Full Heal@"
+	rawchar "Full Restore@"
+	rawchar "Revive@"
+	rawchar "Max Revive@"
+	rawchar "Ether@"
+	rawchar "Max Ether@"
+	rawchar "Elixir@"
+	rawchar "Max Elixir@"
+	rawchar "HP Up@"
+	rawchar "Protein@"
+	rawchar "Iron@"
+	rawchar "Carbos@"
+	rawchar "Calcium@"
+	rawchar "Zinc@"
+	rawchar "Rare Candy@"
+	rawchar "PP Up@"
+	rawchar "PP Max@"
+	rawchar "Fresh Water@"
+	rawchar "Soda Pop@"
+	rawchar "Lemonade@"
+	rawchar "Moomoo Milk@"
 if DEF(FAITHFUL)
-	db "RageCandyBar@"
+	rawchar "RageCandyBar@"
 else
-	db "Cake of Rage@"
+	rawchar "Cake of Rage@"
 endc
-	db "PewterCrunch@"
-	db "Sacred Ash@"
-	db "EnergyPowder@"
-	db "Energy Root@"
-	db "Heal Powder@"
-	db "Revival Herb@"
-	db "X Attack@"
-	db "X Defend@"
-	db "X Speed@"
-	db "X Spcl.Atk@"
-	db "X Spcl.Def@"
-	db "X Accuracy@"
-	db "Dire Hit@"
+	rawchar "PewterCrunch@"
+	rawchar "Sacred Ash@"
+	rawchar "EnergyPowder@"
+	rawchar "Energy Root@"
+	rawchar "Heal Powder@"
+	rawchar "Revival Herb@"
+	rawchar "X Attack@"
+	rawchar "X Defend@"
+	rawchar "X Speed@"
+	rawchar "X Spcl.Atk@"
+	rawchar "X Spcl.Def@"
+	rawchar "X Accuracy@"
+	rawchar "Dire Hit@"
 if DEF(FAITHFUL)
-	db "Guard Spec.@"
+	rawchar "Guard Spec.@"
 else
-	db "Guard Stats@"
+	rawchar "Guard Stats@"
 endc
-	db "Repel@"
-	db "Super Repel@"
-	db "Max Repel@"
-	db "Escape Rope@"
-	db "# Doll@"
-	db "Ability Cap@"
-	db "Leaf Stone@"
-	db "Fire Stone@"
-	db "Water Stone@"
-	db "ThunderStone@"
-	db "Moon Stone@"
-	db "Sun Stone@"
-	db "Dusk Stone@"
-	db "Dawn Stone@"
-	db "Shiny Stone@"
-	db "Ice Stone@"
-	db "Everstone@"
-	db "Exp.Share@"
-	db "Cheri Berry@"
-	db "Chesto Berry@"
-	db "Pecha Berry@"
-	db "Rawst Berry@"
-	db "Aspear Berry@"
-	db "Leppa Berry@"
-	db "Oran Berry@"
-	db "Persim Berry@"
-	db "Lum Berry@"
-	db "Sitrus Berry@"
-	db "Figy Berry@"
-	db "Pomeg Berry@"
-	db "Kelpsy Berry@"
-	db "Qualot Berry@"
-	db "Hondew Berry@"
-	db "Grepa Berry@"
-	db "Tamato Berry@"
-	db "Liechi Berry@"
-	db "Ganlon Berry@"
-	db "Salac Berry@"
-	db "Petaya Berry@"
-	db "Apicot Berry@"
-	db "Lansat Berry@"
-	db "Starf Berry@"
-	db "Enigma Berry@"
-	db "Custap Berry@"
-	db "Jaboca Berry@"
-	db "Rowap Berry@"
-	db "Kee Berry@"
-	db "MarangaBerry@"
-	db "Berry Juice@"
-	db "Silk Scarf@"
-	db "Black Belt@"
-	db "Sharp Beak@"
-	db "Poison Barb@"
-	db "Soft Sand@"
-	db "Hard Stone@"
-	db "SilverPowder@"
-	db "Spell Tag@"
-	db "Metal Coat@"
-	db "Charcoal@"
-	db "Mystic Water@"
-	db "Miracle Seed@"
-	db "Magnet@"
-	db "TwistedSpoon@"
-	db "NeverMeltIce@"
-	db "Dragon Fang@"
-	db "BlackGlasses@"
-	db "Pink Bow@"
-	db "BrightPowder@"
-	db "Scope Lens@"
-	db "Quick Claw@"
-	db "King's Rock@"
-	db "Focus Band@"
-	db "Leftovers@"
-	db "Lucky Egg@"
-	db "Amulet Coin@"
-	db "Cleanse Tag@"
-	db "Smoke Ball@"
-	db "Berserk Gene@"
-	db "Light Ball@"
-	db "Stick@"
-	db "Thick Club@"
-	db "Lucky Punch@"
-	db "Metal Powder@"
-	db "Quick Powder@"
-	db "Armor Suit@"
-	db "Air Balloon@"
-	db "Assault Vest@"
-	db "Big Root@"
-	db "Binding Band@"
-	db "Destiny Knot@"
-	db "Eviolite@"
-	db "Expert Belt@"
-	db "Focus Sash@"
-	db "Grip Claw@"
-	db "Life Orb@"
-	db "Light Clay@"
-	db "Metronome@"
-	db "Muscle Band@"
-	db "Protect Pads@"
-	db "Rocky Helmet@"
-	db "Safe Goggles@"
-	db "Shed Shell@"
-	db "Shell Bell@"
-	db "Soothe Bell@"
-	db "Weak Policy@"
-	db "Wide Lens@"
-	db "Wise Glasses@"
-	db "Zoom Lens@"
-	db "Eject Button@"
-	db "Lagging Tail@"
-	db "Iron Ball@"
-	db "Ring Target@"
-	db "Red Card@"
-	db "Absorb Bulb@"
-	db "Cell Battery@"
-	db "LuminousMoss@"
-	db "Snowball@"
-	db "Eject Pack@"
-	db "Room Service@"
-	db "BlundrPolicy@"
-	db "Throat Spray@"
-	db "Heavy Boots@"
-	db "UtilUmbrella@"
-	db "Mental Herb@"
-	db "Power Herb@"
-	db "White Herb@"
-	db "Damp Rock@"
-	db "Heat Rock@"
-	db "Smooth Rock@"
-	db "Icy Rock@"
-	db "Choice Band@"
-	db "Choice Scarf@"
-	db "Choice Specs@"
-	db "Flame Orb@"
-	db "Toxic Orb@"
-	db "Black Sludge@"
-	db "Macho Brace@"
-	db "Power Weight@"
-	db "Power Bracer@"
-	db "Power Belt@"
-	db "Power Lens@"
-	db "Power Band@"
-	db "Power Anklet@"
-	db "Dragon Scale@"
-	db "Up-Grade@"
-	db "Dubious Disc@"
-	db "Protector@"
-	db "Electirizer@"
-	db "Magmarizer@"
-	db "Razor Fang@"
-	db "Razor Claw@"
-	db "Odd Souvenir@"
-	db "Nugget@"
-	db "Big Nugget@"
-	db "TinyMushroom@"
-	db "Big Mushroom@"
-	db "BalmMushroom@"
-	db "Pearl@"
-	db "Big Pearl@"
-	db "Pearl String@"
-	db "Stardust@"
-	db "Star Piece@"
-	db "Brick Piece@"
-	db "Rare Bone@"
-	db "Silver Leaf@"
-	db "Gold Leaf@"
-	db "SlowpokeTail@"
-	db "Bottle Cap@"
-	db "Helix Fossil@"
-	db "Dome Fossil@"
-	db "Old Amber@"
-	db "Mulch@"
-	db "Sweet Honey@"
-	db "Mint Leaf@"
-	db "Flower Mail@"
-	db "Surf Mail@"
-	db "LiteBlueMail@"
-	db "PortraitMail@"
-	db "Lovely Mail@"
-	db "Eon Mail@"
-	db "Morph Mail@"
-	db "BlueSky Mail@"
-	db "Music Mail@"
-	db "Mirage Mail@"
+	rawchar "Repel@"
+	rawchar "Super Repel@"
+	rawchar "Max Repel@"
+	rawchar "Escape Rope@"
+	rawchar "# Doll@"
+	rawchar "Ability Cap@"
+	rawchar "Leaf Stone@"
+	rawchar "Fire Stone@"
+	rawchar "Water Stone@"
+	rawchar "ThunderStone@"
+	rawchar "Moon Stone@"
+	rawchar "Sun Stone@"
+	rawchar "Dusk Stone@"
+	rawchar "Dawn Stone@"
+	rawchar "Shiny Stone@"
+	rawchar "Ice Stone@"
+	rawchar "Everstone@"
+	rawchar "Exp.Share@"
+	rawchar "Cheri Berry@"
+	rawchar "Chesto Berry@"
+	rawchar "Pecha Berry@"
+	rawchar "Rawst Berry@"
+	rawchar "Aspear Berry@"
+	rawchar "Leppa Berry@"
+	rawchar "Oran Berry@"
+	rawchar "Persim Berry@"
+	rawchar "Lum Berry@"
+	rawchar "Sitrus Berry@"
+	rawchar "Figy Berry@"
+	rawchar "Pomeg Berry@"
+	rawchar "Kelpsy Berry@"
+	rawchar "Qualot Berry@"
+	rawchar "Hondew Berry@"
+	rawchar "Grepa Berry@"
+	rawchar "Tamato Berry@"
+	rawchar "Liechi Berry@"
+	rawchar "Ganlon Berry@"
+	rawchar "Salac Berry@"
+	rawchar "Petaya Berry@"
+	rawchar "Apicot Berry@"
+	rawchar "Lansat Berry@"
+	rawchar "Starf Berry@"
+	rawchar "Enigma Berry@"
+	rawchar "Custap Berry@"
+	rawchar "Jaboca Berry@"
+	rawchar "Rowap Berry@"
+	rawchar "Kee Berry@"
+	rawchar "MarangaBerry@"
+	rawchar "Berry Juice@"
+	rawchar "Silk Scarf@"
+	rawchar "Black Belt@"
+	rawchar "Sharp Beak@"
+	rawchar "Poison Barb@"
+	rawchar "Soft Sand@"
+	rawchar "Hard Stone@"
+	rawchar "SilverPowder@"
+	rawchar "Spell Tag@"
+	rawchar "Metal Coat@"
+	rawchar "Charcoal@"
+	rawchar "Mystic Water@"
+	rawchar "Miracle Seed@"
+	rawchar "Magnet@"
+	rawchar "TwistedSpoon@"
+	rawchar "NeverMeltIce@"
+	rawchar "Dragon Fang@"
+	rawchar "BlackGlasses@"
+	rawchar "Pink Bow@"
+	rawchar "BrightPowder@"
+	rawchar "Scope Lens@"
+	rawchar "Quick Claw@"
+	rawchar "King's Rock@"
+	rawchar "Focus Band@"
+	rawchar "Leftovers@"
+	rawchar "Lucky Egg@"
+	rawchar "Amulet Coin@"
+	rawchar "Cleanse Tag@"
+	rawchar "Smoke Ball@"
+	rawchar "Berserk Gene@"
+	rawchar "Light Ball@"
+	rawchar "Stick@"
+	rawchar "Thick Club@"
+	rawchar "Lucky Punch@"
+	rawchar "Metal Powder@"
+	rawchar "Quick Powder@"
+	rawchar "Armor Suit@"
+	rawchar "Air Balloon@"
+	rawchar "Assault Vest@"
+	rawchar "Big Root@"
+	rawchar "Binding Band@"
+	rawchar "Destiny Knot@"
+	rawchar "Eviolite@"
+	rawchar "Expert Belt@"
+	rawchar "Focus Sash@"
+	rawchar "Grip Claw@"
+	rawchar "Life Orb@"
+	rawchar "Light Clay@"
+	rawchar "Metronome@"
+	rawchar "Muscle Band@"
+	rawchar "Protect Pads@"
+	rawchar "Rocky Helmet@"
+	rawchar "Safe Goggles@"
+	rawchar "Shed Shell@"
+	rawchar "Shell Bell@"
+	rawchar "Soothe Bell@"
+	rawchar "Weak Policy@"
+	rawchar "Wide Lens@"
+	rawchar "Wise Glasses@"
+	rawchar "Zoom Lens@"
+	rawchar "Eject Button@"
+	rawchar "Lagging Tail@"
+	rawchar "Iron Ball@"
+	rawchar "Ring Target@"
+	rawchar "Red Card@"
+	rawchar "Absorb Bulb@"
+	rawchar "Cell Battery@"
+	rawchar "LuminousMoss@"
+	rawchar "Snowball@"
+	rawchar "Eject Pack@"
+	rawchar "Room Service@"
+	rawchar "BlundrPolicy@"
+	rawchar "Throat Spray@"
+	rawchar "Heavy Boots@"
+	rawchar "UtilUmbrella@"
+	rawchar "Mental Herb@"
+	rawchar "Power Herb@"
+	rawchar "White Herb@"
+	rawchar "Damp Rock@"
+	rawchar "Heat Rock@"
+	rawchar "Smooth Rock@"
+	rawchar "Icy Rock@"
+	rawchar "Choice Band@"
+	rawchar "Choice Scarf@"
+	rawchar "Choice Specs@"
+	rawchar "Flame Orb@"
+	rawchar "Toxic Orb@"
+	rawchar "Black Sludge@"
+	rawchar "Macho Brace@"
+	rawchar "Power Weight@"
+	rawchar "Power Bracer@"
+	rawchar "Power Belt@"
+	rawchar "Power Lens@"
+	rawchar "Power Band@"
+	rawchar "Power Anklet@"
+	rawchar "Dragon Scale@"
+	rawchar "Up-Grade@"
+	rawchar "Dubious Disc@"
+	rawchar "Protector@"
+	rawchar "Electirizer@"
+	rawchar "Magmarizer@"
+	rawchar "Razor Fang@"
+	rawchar "Razor Claw@"
+	rawchar "Odd Souvenir@"
+	rawchar "Nugget@"
+	rawchar "Big Nugget@"
+	rawchar "TinyMushroom@"
+	rawchar "Big Mushroom@"
+	rawchar "BalmMushroom@"
+	rawchar "Pearl@"
+	rawchar "Big Pearl@"
+	rawchar "Pearl String@"
+	rawchar "Stardust@"
+	rawchar "Star Piece@"
+	rawchar "Brick Piece@"
+	rawchar "Rare Bone@"
+	rawchar "Silver Leaf@"
+	rawchar "Gold Leaf@"
+	rawchar "SlowpokeTail@"
+	rawchar "Bottle Cap@"
+	rawchar "Helix Fossil@"
+	rawchar "Dome Fossil@"
+	rawchar "Old Amber@"
+	rawchar "Mulch@"
+	rawchar "Sweet Honey@"
+	rawchar "Mint@"
+	rawchar "Flower Mail@"
+	rawchar "Surf Mail@"
+	rawchar "LiteBlueMail@"
+	rawchar "PortraitMail@"
+	rawchar "Lovely Mail@"
+	rawchar "Eon Mail@"
+	rawchar "Morph Mail@"
+	rawchar "BlueSky Mail@"
+	rawchar "Music Mail@"
+	rawchar "Mirage Mail@"