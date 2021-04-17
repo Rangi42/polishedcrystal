0b0d33c9137e766d59de4a8afa1d8a9642dc1813
teddy <jasonkolanda87@msn.com>
2021-04-17 10:04:34 -0400

    Updated sprites + anims for Clefable, Crobat, and Dodrio with SoupPotato's

---
+++ gfx/pokemon/crobat/anim.asm
@@ -0,0 +1,14 @@
+    setrepeat 2
+    frame 1, 04
+    frame 2, 04
+    frame 3, 04
+    frame 5, 04
+    dorepeat 1
+    frame 5, 08
+    setrepeat 2
+    frame 1, 04
+    frame 2, 04
+    frame 3, 04
+    frame 5, 04
+    dorepeat 8
+    endanim
---
+++ gfx/pokemon/dodrio/anim_idle.asm
@@ -0,0 +1,6 @@
+    setrepeat 3
+	frame 6, 08
+	frame 0, 08
+	dorepeat 1
+	endanim
+	
