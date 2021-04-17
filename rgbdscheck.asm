5fbbb8396e73999b05ce14d6d0cf1656ba4ac57e
Rangi <remy.oukaour+rangi42@gmail.com>
2021-04-17 17:55:09 -0400

    Require rgbds 0.5.0

--- rgbdscheck.asm
+++ rgbdscheck.asm
@@ -1,22 +1,22 @@
-FAIL_MESSAGE EQUS "fail \"polishedcrystal requires rgbds 0.5.0-rc2 or newer.\""
-
 MAJOR EQU 0
 MINOR EQU 5
 PATCH EQU 0
-RC    EQU 2
+RC    EQU 0
+
+wrong_rgbds: MACRO
+	fail "polishedcrystal requires rgbds v0.5.0 or newer."
+ENDM
 
 if !DEF(__RGBDS_MAJOR__) || !DEF(__RGBDS_MINOR__) || !DEF(__RGBDS_PATCH__)
-	FAIL_MESSAGE
+	wrong_rgbds
 else
-if (__RGBDS_MAJOR__ < MAJOR) || \
-	(__RGBDS_MAJOR__ == MAJOR && __RGBDS_MINOR__ < MINOR) || \
-	(__RGBDS_MAJOR__ == MAJOR && __RGBDS_MINOR__ == MINOR && __RGBDS_PATCH__ < PATCH)
-	FAIL_MESSAGE
-else
-if DEF(__RGBDS_RC__)
-if __RGBDS_RC__ > 0 && __RGBDS_RC__ < RC
-	FAIL_MESSAGE
-endc
-endc
-endc
+	if (__RGBDS_MAJOR__ < MAJOR) || \
+		(__RGBDS_MAJOR__ == MAJOR && __RGBDS_MINOR__ < MINOR) || \
+		(__RGBDS_MAJOR__ == MAJOR && __RGBDS_MINOR__ == MINOR && __RGBDS_PATCH__ < PATCH)
+		wrong_rgbds
+	else
+		if DEF(__RGBDS_RC__)
+			wrong_rgbds
+		endc
+	endc
 endc
