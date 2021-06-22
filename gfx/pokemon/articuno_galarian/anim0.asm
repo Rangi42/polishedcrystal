6f6f5dee81296a32a15a23789e2e8cf739f6eda6
flempa316 <58637645+flempa316@users.noreply.github.com>
2021-06-22 19:32:25 -0400

    Articuno Animation

---
+++ gfx/pokemon/articuno_galarian/anim0.asm
@@ -0,0 +1,16 @@
+	frame 0, 12
+	frame 1, 32
+	frame 2, 08
+	frame 3, 08
+	frame 4, 08
+	frame 2, 08
+	frame 3, 08
+	frame 4, 08
+	frame 2, 08
+	frame 3, 08
+	frame 4, 08
+	frame 5, 26
+	frame 6, 20
+	frame 7, 32
+	frame 1, 18	
+	endanim
