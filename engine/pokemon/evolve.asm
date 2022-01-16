9171eeef4557207f1feb19a45a0a8408f55ca67c
Fredrik Ljungdahl <fredde1994@gmail.com>
2022-01-16 02:47:22 +0100

    Fix evolution problems

--- engine/pokemon/evolve.asm
+++ engine/pokemon/evolve.asm
@@ -318,13 +318,11 @@ endr
 	and FORM_MASK
 	ld a, [wTempMonForm]
 	jr z, .keep_old_form
-	and $ff - FORM_MASK
+	and ~SPECIESFORM_MASK
 .keep_old_form
-	and $ff - EXTSPECIES_MASK
+	and ~EXTSPECIES_MASK
 	or c
 	ld [wEvolutionNewForm], a
-	and SPECIESFORM_MASK
-	ld [wCurForm], a
 	ld a, [wCurPartyMon]
 	ld hl, wPartyMonNicknames
 	call GetNickname
