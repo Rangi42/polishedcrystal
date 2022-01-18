63ba2d934e9a44519867bea6dcd8f81edbd4dc66
Rangi <remy.oukaour+rangi42@gmail.com>
2022-01-17 20:18:57 -0500

    Use `ctxt` in data/battle_tower/trainer_text.asm

--- data/battle_tower/trainer_text.asm
+++ data/battle_tower/trainer_text.asm
@@ -1,5 +1,5 @@
 _BTGreetingM1Text:
-	text "Hello, glad to"
+	ctxt "Hello, glad to"
 	line "meet you!"
 
 	para "I do hope we have"
@@ -7,122 +7,122 @@ _BTGreetingM1Text:
 	done
 
 _BTLossM1Text:
-	text "Thank you! A most"
+	ctxt "Thank you! A most"
 	line "enjoyable battle!"
 	done
 
 _BTWinM1Text:
-	text "Thank you. You are"
+	ctxt "Thank you. You are"
 	line "formidable."
 	done
 
 _BTGreetingM2Text:
-	text "Work, work, work…"
+	ctxt "Work, work, work…"
 	line "I'm always busy!"
 	done
 
 _BTLossM2Text:
-	text "But, I work hard"
+	ctxt "But, I work hard"
 	line "in battle too!"
 	done
 
 _BTWinM2Text:
-	text "I'm too busy to be"
+	ctxt "I'm too busy to be"
 	line "battling!"
 	done
 
 _BTGreetingM3Text:
-	text "Brace yourself for"
+	ctxt "Brace yourself for"
 	line "my all-out attack!"
 	done
 
 _BTLossM3Text:
-	text "What a cakewalk!"
+	ctxt "What a cakewalk!"
 	line "You're too easy!"
 	done
 
 _BTWinM3Text:
-	text "I won't lose next"
+	ctxt "I won't lose next"
 	line "time, all right?"
 	done
 
 _BTGreetingM4Text:
-	text "Heh, your #mon"
+	ctxt "Heh, your #mon"
 	line "look pretty cool."
 	done
 
 _BTLossM4Text:
-	text "Hey, hey, no way!"
+	ctxt "Hey, hey, no way!"
 	line "You won't win!"
 	done
 
 _BTWinM4Text:
-	text "You serious?"
+	ctxt "You serious?"
 	line "This is brutal!"
 	done
 
 _BTGreetingM5Text:
-	text "#mon every day!"
+	ctxt "#mon every day!"
 	line "I love battling!"
 	done
 
 _BTLossM5Text:
-	text "I'm on top of my"
+	ctxt "I'm on top of my"
 	line "game, but not you!"
 	done
 
 _BTWinM5Text:
-	text "I don't care that"
+	ctxt "I don't care that"
 	line "I lost, really!"
 	done
 
 _BTGreetingM6Text:
-	text "Hi, there! Let's"
+	ctxt "Hi, there! Let's"
 	line "keep this clean!"
 	done
 
 _BTLossM6Text:
-	text "Whoops, sorry for"
+	ctxt "Whoops, sorry for"
 	line "that wipeout!"
 	done
 
 _BTWinM6Text:
-	text "Whoops! Come on,"
+	ctxt "Whoops! Come on,"
 	line "let me win one!"
 	done
 
 _BTGreetingM7Text:
-	text "Do you want to see"
+	ctxt "Do you want to see"
 	line "my battle level?"
 	done
 
 _BTLossM7Text:
-	text "Hehehe, I know"
+	ctxt "Hehehe, I know"
 	line "your level now!"
 	done
 
 _BTWinM7Text:
-	text "Ouch… I'm just too"
+	ctxt "Ouch… I'm just too"
 	line "weak…"
 	done
 
 _BTGreetingM8Text:
-	text "Hey, let's battle."
+	ctxt "Hey, let's battle."
 	line "I'm your opponent."
 	done
 
 _BTLossM8Text:
-	text "Wow, you're not"
+	ctxt "Wow, you're not"
 	line "serious about it!"
 	done
 
 _BTWinM8Text:
-	text "…Urgh… Nothing"
+	ctxt "…Urgh… Nothing"
 	line "positive here…"
 	done
 
 _BTGreetingM9Text:
-	text "I'm your"
+	ctxt "I'm your"
 	line "opponent."
 
 	para "Get ready to be"
@@ -130,47 +130,47 @@ _BTGreetingM9Text:
 	done
 
 _BTLossM9Text:
-	text "Hahah! That was a"
+	ctxt "Hahah! That was a"
 	line "pushover!"
 	done
 
 _BTWinM9Text:
-	text "No way! There has"
+	ctxt "No way! There has"
 	line "to be a mistake!"
 	done
 
 _BTGreetingM10Text:
-	text "Hah!"
+	ctxt "Hah!"
 	line "Let's get rolling!"
 	done
 
 _BTLossM10Text:
-	text "Wahahaha! Didn't"
+	ctxt "Wahahaha! Didn't"
 	line "break a sweat!"
 	done
 
 _BTWinM10Text:
-	text "Tough! I'm no"
+	ctxt "Tough! I'm no"
 	line "match for you!"
 	done
 
 _BTGreetingM11Text:
-	text "………………"
+	ctxt "………………"
 	line "…………Battle?"
 	done
 
 _BTLossM11Text:
-	text "………………"
+	ctxt "………………"
 	line "…………I won?"
 	done
 
 _BTWinM11Text:
-	text "………………"
+	ctxt "………………"
 	line "…………I lost?"
 	done
 
 _BTGreetingM12Text:
-	text "You want to be a"
+	ctxt "You want to be a"
 	line "leader?"
 
 	para "Let me battle you,"
@@ -178,7 +178,7 @@ _BTGreetingM12Text:
 	done
 
 _BTLossM12Text:
-	text "You need another"
+	ctxt "You need another"
 	line "ten years of"
 
 	para "training to get"
@@ -186,7 +186,7 @@ _BTLossM12Text:
 	done
 
 _BTWinM12Text:
-	text "You're incredibly"
+	ctxt "You're incredibly"
 	line "talented."
 
 	para "No one can touch"
@@ -194,72 +194,72 @@ _BTWinM12Text:
 	done
 
 _BTGreetingM13Text:
-	text "Today, I'm going"
+	ctxt "Today, I'm going"
 	line "to whomp you."
 	done
 
 _BTLossM13Text:
-	text "I knew I'd win."
+	ctxt "I knew I'd win."
 	line "I'm so great!"
 	done
 
 _BTWinM13Text:
-	text "Uh? My plans are"
+	ctxt "Uh? My plans are"
 	line "out of whack…"
 	done
 
 _BTGreetingM14Text:
-	text "I bet you can't"
+	ctxt "I bet you can't"
 	line "beat me!"
 	done
 
 _BTLossM14Text:
-	text "Those #mon"
+	ctxt "Those #mon"
 	line "aren't enough!"
 	done
 
 _BTWinM14Text:
-	text "I want your"
+	ctxt "I want your"
 	line "#mon. Please?"
 	done
 
 _BTGreetingM15Text:
-	text "I'll show you a"
+	ctxt "I'll show you a"
 	line "real battle!"
 	done
 
 _BTLossM15Text:
-	text "This battle…"
+	ctxt "This battle…"
 	line "I'm bored!"
 	done
 
 _BTWinM15Text:
-	text "…I won't turn tail"
+	ctxt "…I won't turn tail"
 	line "in battle!"
 	done
 
 _BTGreetingM16Text:
-	text "Let's go!"
+	ctxt "Let's go!"
 	line "No holds barred!"
 	done
 
 _BTLossM16Text:
-	text "Sorry! I wanted"
+	ctxt "Sorry! I wanted"
 	line "the win more!"
 	done
 
 _BTWinM16Text:
-	text "Wahah! Congrats!"
+	ctxt "Wahah! Congrats!"
 	line "I can't do better!"
 	done
 
 _BTGreetingM17Text:
-	text "My #mon skills"
+	ctxt "My #mon skills"
 	line "are phenomenal!"
 	done
 
 _BTLossM17Text:
-	text "You've got a long"
+	ctxt "You've got a long"
 	line "way to go."
 
 	para "Well, keep trying!"
@@ -267,17 +267,17 @@ _BTLossM17Text:
 	done
 
 _BTWinM17Text:
-	text "Aww… Don't lose"
+	ctxt "Aww… Don't lose"
 	line "after beating me."
 	done
 
 _BTGreetingM18Text:
-	text "Who are you?"
+	ctxt "Who are you?"
 	line "I don't know you…"
 	done
 
 _BTLossM18Text:
-	text "I must've imagined"
+	ctxt "I must've imagined"
 	line "that."
 
 	para "There seems to be"
@@ -285,42 +285,42 @@ _BTLossM18Text:
 	done
 
 _BTWinM18Text:
-	text "Who am I?"
+	ctxt "Who am I?"
 	line "I don't know…"
 	done
 
 _BTGreetingM19Text:
-	text "Um… Are you that…"
+	ctxt "Um… Are you that…"
 	line "um…person?"
 	done
 
 _BTLossM19Text:
-	text "It doesn't appear"
+	ctxt "It doesn't appear"
 	line "to be you…"
 	done
 
 _BTWinM19Text:
-	text "Then you really"
+	ctxt "Then you really"
 	line "are the legendary…"
 	done
 
 _BTGreetingM20Text:
-	text "I heard that"
+	ctxt "I heard that"
 	line "you're hot!"
 	done
 
 _BTLossM20Text:
-	text "Not bad. I was"
+	ctxt "Not bad. I was"
 	line "just a bit better."
 	done
 
 _BTWinM20Text:
-	text "Eh, you're not"
+	ctxt "Eh, you're not"
 	line "that special."
 	done
 
 _BTGreetingM21Text:
-	text "I'm scared about"
+	ctxt "I'm scared about"
 	line "what might happen."
 
 	para "My #mon are way"
@@ -328,302 +328,302 @@ _BTGreetingM21Text:
 	done
 
 _BTLossM21Text:
-	text "See? My #mon"
+	ctxt "See? My #mon"
 	line "were too strong."
 	done
 
 _BTWinM21Text:
-	text "Graa! My #mon"
+	ctxt "Graa! My #mon"
 	line "were total wimps!"
 	done
 
 _BTGreetingM22Text:
-	text "Hey, there!"
+	ctxt "Hey, there!"
 	line "I'll take you on!"
 	done
 
 _BTLossM22Text:
-	text "Don't you have a"
+	ctxt "Don't you have a"
 	line "better strategy?"
 	done
 
 _BTWinM22Text:
-	text "You've got decent"
+	ctxt "You've got decent"
 	line "style!"
 	done
 
 _BTGreetingM23Text:
-	text "I wonder if I can"
+	ctxt "I wonder if I can"
 	line "battle properly…"
 	done
 
 _BTLossM23Text:
-	text "Um… Sorry…"
+	ctxt "Um… Sorry…"
 	line "I think I won."
 	done
 
 _BTWinM23Text:
-	text "I guess I'm not"
+	ctxt "I guess I'm not"
 	line "good enough yet…"
 	done
 
 _BTGreetingM24Text:
-	text "Wrrooar!"
+	ctxt "Wrrooar!"
 	line "I won't lose!"
 	done
 
 _BTLossM24Text:
-	text "Wrrooar! I knew"
+	ctxt "Wrrooar! I knew"
 	line "I was a genius!"
 	done
 
 _BTWinM24Text:
-	text "Arrooh! I hate it"
+	ctxt "Arrooh! I hate it"
 	line "when I lose!"
 	done
 
 _BTGreetingM25Text:
-	text "Sorry, but I'm"
+	ctxt "Sorry, but I'm"
 	line "going to win."
 	done
 
 _BTLossM25Text:
-	text "Yeah! My #mon"
+	ctxt "Yeah! My #mon"
 	line "rule!"
 	done
 
 _BTWinM25Text:
-	text "Oh, close! I lost"
+	ctxt "Oh, close! I lost"
 	line "by just a bit!"
 	done
 
 _BTGreetingF1Text:
-	text "OK, I'm not"
+	ctxt "OK, I'm not"
 	line "fooling around!"
 	done
 
 _BTLossF1Text:
-	text "Yay! Too easy!"
+	ctxt "Yay! Too easy!"
 	line "Like, no way!"
 	done
 
 _BTWinF1Text:
-	text "No!"
+	ctxt "No!"
 	line "Like, no way!"
 	done
 
 _BTGreetingF2Text:
-	text "Look! My #mon"
+	ctxt "Look! My #mon"
 	line "are really cute!"
 	done
 
 _BTLossF2Text:
-	text "Aren't they really"
+	ctxt "Aren't they really"
 	line "adorable?"
 	done
 
 _BTWinF2Text:
-	text "I'm sorry, it's"
+	ctxt "I'm sorry, it's"
 	line "all my fault!"
 	done
 
 _BTGreetingF3Text:
-	text "Let's get our"
+	ctxt "Let's get our"
 	line "battle started!"
 	done
 
 _BTLossF3Text:
-	text "Was I too strong"
+	ctxt "Was I too strong"
 	line "for you?"
 	done
 
 _BTWinF3Text:
-	text "Ooh, you're in a"
+	ctxt "Ooh, you're in a"
 	line "different class."
 	done
 
 _BTGreetingF4Text:
-	text "Are we going to"
+	ctxt "Are we going to"
 	line "battle? Let's!"
 	done
 
 _BTLossF4Text:
-	text "Oh, you're too"
+	ctxt "Oh, you're too"
 	line "weak. Shame."
 	done
 
 _BTWinF4Text:
-	text "Wow! Are you quite"
+	ctxt "Wow! Are you quite"
 	line "satisfied?"
 	done
 
 _BTGreetingF5Text:
-	text "Oh, you have some"
+	ctxt "Oh, you have some"
 	line "rare #mon."
 	done
 
 _BTLossF5Text:
-	text "May I have one of"
+	ctxt "May I have one of"
 	line "your #mon?"
 	done
 
 _BTWinF5Text:
-	text "…I want one of"
+	ctxt "…I want one of"
 	line "your #mon."
 	done
 
 _BTGreetingF6Text:
-	text "Want to hear about"
+	ctxt "Want to hear about"
 	line "my cute #mon?"
 	done
 
 _BTLossF6Text:
-	text "What do you think"
+	ctxt "What do you think"
 	line "about my cuties?"
 	done
 
 _BTWinF6Text:
-	text "Oh! My! You're a"
+	ctxt "Oh! My! You're a"
 	line "dreadful trainer!"
 	done
 
 _BTGreetingF7Text:
-	text "Battle? Sure!"
+	ctxt "Battle? Sure!"
 	line "Right now!"
 	done
 
 _BTLossF7Text:
-	text "Oh, I love it!"
+	ctxt "Oh, I love it!"
 	line "Battling is wild!"
 	done
 
 _BTWinF7Text:
-	text "Oh, how rude! Wait"
+	ctxt "Oh, how rude! Wait"
 	line "till next time!"
 	done
 
 _BTGreetingF8Text:
-	text "Please let me win!"
+	ctxt "Please let me win!"
 	line "Please?"
 	done
 
 _BTLossF8Text:
-	text "Wow, thank you!"
+	ctxt "Wow, thank you!"
 	line "You're so nice!"
 	done
 
 _BTWinF8Text:
-	text "You're mean!"
+	ctxt "You're mean!"
 	line "I hate meanies!"
 	done
 
 _BTGreetingF9Text:
-	text "Well, can we"
+	ctxt "Well, can we"
 	line "begin?"
 	done
 
 _BTLossF9Text:
-	text "Well, I beg your"
+	ctxt "Well, I beg your"
 	line "pardon…"
 	done
 
 _BTWinF9Text:
-	text "Sob… That's not"
+	ctxt "Sob… That's not"
 	line "fair!"
 	done
 
 _BTGreetingF10Text:
-	text "I'm good!"
+	ctxt "I'm good!"
 	line "You can't win."
 	done
 
 _BTLossF10Text:
-	text "Giving up? You're"
+	ctxt "Giving up? You're"
 	line "pretty weak!"
 	done
 
 _BTWinF10Text:
-	text "I won't accept"
+	ctxt "I won't accept"
 	line "this… No way!"
 	done
 
 _BTGreetingF11Text:
-	text "Are you treating"
+	ctxt "Are you treating"
 	line "this seriously?"
 	done
 
 _BTLossF11Text:
-	text "Oh, sorry! Looks"
+	ctxt "Oh, sorry! Looks"
 	line "like I won!"
 	done
 
 _BTWinF11Text:
-	text "Oh, how nasty!"
+	ctxt "Oh, how nasty!"
 	line "You were serious!"
 	done
 
 _BTGreetingF12Text:
-	text "Ahahah! I'll take"
+	ctxt "Ahahah! I'll take"
 	line "it easy on you!"
 	done
 
 _BTLossF12Text:
-	text "Oops, sorry! But"
+	ctxt "Oops, sorry! But"
 	line "I'm happy too!"
 	done
 
 _BTWinF12Text:
-	text "Oh, oh, I lost!"
+	ctxt "Oh, oh, I lost!"
 	line "Thanks. Bye!"
 	done
 
 _BTGreetingF13Text:
-	text "Battle Tower is a"
+	ctxt "Battle Tower is a"
 	line "tough place!"
 	done
 
 _BTLossF13Text:
-	text "You might have a"
+	ctxt "You might have a"
 	line "hard time."
 	done
 
 _BTWinF13Text:
-	text "Ooh, you might"
+	ctxt "Ooh, you might"
 	line "make a run here!"
 	done
 
 _BTGreetingF14Text:
-	text "I want to see your"
+	ctxt "I want to see your"
 	line "style in action!"
 	done
 
 _BTLossF14Text:
-	text "Every battle is a"
+	ctxt "Every battle is a"
 	line "drama!"
 	done
 
 _BTWinF14Text:
-	text "Oh… Want to trade"
+	ctxt "Oh… Want to trade"
 	line "something?"
 	done
 
 _BTGreetingF15Text:
-	text "OK, here goes!"
+	ctxt "OK, here goes!"
 	line "I have momentum!"
 	done
 
 _BTLossF15Text:
-	text "See, I rolled"
+	ctxt "See, I rolled"
 	line "right over you!"
 	done
 
 _BTWinF15Text:
-	text "No! This did not"
+	ctxt "No! This did not"
 	line "happen!"
 	done
 
 BattleTowerText_GreetingTycoon:
-	text "Hi! My name's"
+	ctxt "Hi! My name's"
 	line "Palmer."
 
 	para "You're <PLAYER>"
@@ -645,12 +645,12 @@ BattleTowerText_GreetingTycoon:
 	done
 
 BattleTowerText_LossTycoon:
-	text "Ah, wonderful!"
+	ctxt "Ah, wonderful!"
 	line "Do come again!"
 	done
 
 BattleTowerText_WinTycoon:
-	text "I have no problem"
+	ctxt "I have no problem"
 	line "losing to a spec-"
 
 	para "tacular trainer"
