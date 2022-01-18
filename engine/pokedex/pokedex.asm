7f5a47ee6fb205344695e4f769e58b7151b49917
Fredrik Ljungdahl <fredde1994@gmail.com>
2022-01-18 04:07:07 +0100

    Minor optimization

--- engine/pokedex/pokedex.asm
+++ engine/pokedex/pokedex.asm
@@ -2640,15 +2640,13 @@ Pokedex_GetSearchResults:
 
 	; Check if we're doing a null search. - 1 to exclude search order.
 	ld hl, wPokedex_SearchData
-	push bc
-	ld b, NUM_DEXSEARCH - 1
+	ld d, NUM_DEXSEARCH - 1
 	xor a
 .check_null_search
 	or [hl]
 	inc hl
-	dec b
+	dec d
 	jr nz, .check_null_search
-	pop bc
 
 	; If we aren't, skip this entry.
 	and a
