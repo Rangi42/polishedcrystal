953b2b1226ce71304472752cb460856ce1ad862e
Rangi <35663410+Rangi42@users.noreply.github.com>
2022-01-29 10:32:22 -0500

    Update Route35.asm

--- maps/Route35.asm
+++ maps/Route35.asm
@@ -14,9 +14,9 @@ Route35_MapScriptHeader:
 	def_bg_events
 	bg_event  5,  7, BGEVENT_JUMPTEXT, Route35SignText
 	bg_event 15, 31, BGEVENT_JUMPTEXT, Route35SignText
+	bg_event  4, 11, BGEVENT_ITEM + NUGGET, EVENT_ROUTE_35_HIDDEN_NUGGET
 	bg_event 14, 15, BGEVENT_JUMPSTD, treegrotto, HIDDENGROTTO_ROUTE_35
 	bg_event 15, 15, BGEVENT_JUMPSTD, treegrotto, HIDDENGROTTO_ROUTE_35
-	bg_event  4, 11, BGEVENT_ITEM + NUGGET, EVENT_ROUTE_35_HIDDEN_NUGGET
 
 	def_object_events
 	object_event  8, 19, SPRITE_CAMPER, SPRITEMOVEDATA_SPINRANDOM_FAST, 0, 0, -1, -1, 0, OBJECTTYPE_GENERICTRAINER, 2, GenericTrainerCamperIvan, -1
