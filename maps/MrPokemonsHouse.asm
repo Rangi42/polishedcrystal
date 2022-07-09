cf9bab051c5b6b09c5748f240641775ac8f45ae0
Rik Smeets <30838294+rik-smeets@users.noreply.github.com>
2022-07-09 10:29:01 +0200

    Add odd souvenir dialogue to Mr. Pokémon

--- maps/MrPokemonsHouse.asm
+++ maps/MrPokemonsHouse.asm
@@ -63,6 +63,8 @@ MrPokemonsHouse_MrPokemonScript:
 	opentext
 	checkkeyitem RED_SCALE
 	iftruefwd .RedScale
+	checkitem ODD_SOUVENIR
+	iftrue_jumpopenedtext MrPokemonText_OddSouvenir
 	checkevent EVENT_GAVE_MYSTERY_EGG_TO_ELM
 	iftrue_jumpopenedtext MrPokemonText_AlwaysNewDiscoveries
 	jumpopenedtext MrPokemonText_ImDependingOnYou
@@ -234,6 +236,26 @@ MrPokemonsHouse_MrPokemonHealText:
 	para "Here. Your #mon"
 	line "should have some"
 	cont "rest."
+	done
+
+MrPokemonText_OddSouvenir:
+	text "Oh! That souvenir!"
+
+	para "I got one of those"
+	line "on my trip to the"
+	cont "Orange Islands."
+
+	para "Did you know some"
+	line "#mon species"
+	cont "have other forms?"
+
+	para "Hmm… I wonder…"
+
+	para "What happens if"
+	line "a #mon with an"
+
+	para "Alolan form holds"
+	line "that souvenir?"
 	done
 
 MrPokemonText_ImDependingOnYou:
