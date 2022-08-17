a9e5ab27269a9cc1fbde2e6c232907f450ca6c3b
The Void <9028176+etdv-thevoid@users.noreply.github.com>
2022-08-17 10:36:48 -0500

    Load Surf Fishing GFX

--- engine/events/fishing_gfx.asm
+++ engine/events/fishing_gfx.asm
@@ -4,11 +4,23 @@ LoadFishingGFX:
 	xor a
 	ldh [rVBK], a
 
+	ld a, [wPlayerState]
+	cp PLAYER_SURF
+	jr z, .surfing
+
 	ld de, FishingGFX
 	ld a, [wPlayerGender]
 	bit 0, a
 	jr z, .got_gender
 	ld de, KrisFishingGFX
+	jr .got_gender
+.surfing
+
+	ld de, SurfFishingGFX
+	ld a, [wPlayerGender]
+	bit 0, a
+	jr z, .got_gender
+	ld de, KrisSurfFishingGFX
 .got_gender
 
 	ld hl, vTiles0 tile $02
@@ -38,3 +50,9 @@ INCBIN "gfx/overworld/chris_fish.2bpp"
 
 KrisFishingGFX:
 INCBIN "gfx/overworld/kris_fish.2bpp"
+
+SurfFishingGFX:
+INCBIN "gfx/overworld/chris_surf_fish.2bpp"
+
+KrisSurfFishingGFX:
+INCBIN "gfx/overworld/kris_surf_fish.2bpp"
