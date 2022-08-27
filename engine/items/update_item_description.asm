54f29dd075d6b6b32e30722ca5a620acc9e8409b
Rangi <remy.oukaour+rangi42@gmail.com>
2022-08-27 01:25:58 -0400

    Handle 1000 items

--- engine/items/update_item_description.asm
+++ engine/items/update_item_description.asm
@@ -6,14 +6,23 @@ UpdateItemDescriptionAndBagQuantity:
 	cp -1
 	jr z, UpdateItemDescription
 	hlcoord 1, 1
-	ld de, BagXString
+	ld de, BagString
 	rst PlaceString
 	ld a, [wMenuSelection]
 	call GetQuantityInBag
-	hlcoord 6, 1
+	hlcoord 5, 1
+	push hl
 	ld de, wBuffer1
-	lb bc, 2, 3
+	lb bc, 2, 4
 	call PrintNum
+	pop hl
+	; "Bag ×  9", "Bag × 99" "Bag ×999", or "Bag×9999"
+	ld a, [hl]
+	cp " "
+	jr z, .print_x
+	dec hl
+.print_x
+	ld [hl], "×"
 UpdateItemDescription:
 	ld a, [wMenuSelection]
 	ld [wCurSpecies], a
@@ -26,8 +35,8 @@ UpdateItemDescription:
 	decoord 1, 14
 	farjp PrintItemDescription
 
-BagXString:
-	db "Bag ×@"
+BagString:
+	db "Bag @"
 
 UpdateTMHMDescriptionAndOwnership:
 	hlcoord 1, 1
