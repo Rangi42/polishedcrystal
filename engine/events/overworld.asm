fc1aa45c27d427d1efb14026eb93c52bd40e32b1
The Void <9028176+etdv-thevoid@users.noreply.github.com>
2022-09-01 08:34:09 -0500

    Update to most optimized version of fix

--- engine/events/overworld.asm
+++ engine/events/overworld.asm
@@ -480,13 +480,13 @@ SurfFunction:
 	jr z, .alreadyfail
 	cp PLAYER_SURF_PIKA
 	jr z, .alreadyfail
-	call GetFacingObject
-	jr nc, .cannotsurf
 	call GetFacingTileCoord
 	call GetTileCollision
 	dec a ; cp WATER_TILE
 	jr nz, .cannotsurf
 	call CheckDirection
+	jr c, .cannotsurf
+	farcall CheckFacingObject
 	jr c, .cannotsurf
 	ld a, $1
 	ret
@@ -1552,12 +1552,12 @@ FishFunction:
 	ld a, [wPlayerState]
 	cp PLAYER_SURF_PIKA
 	jr z, .fail
-	call GetFacingObject
-	jr nc, .fail
 	call GetFacingTileCoord
 	call GetTileCollision
 	dec a ; cp WATER_TILE
-	jr z, .facingwater
+	jr nz, .fail
+	farcall CheckFacingObject
+	jr nc, .facingwater
 .fail
 	ld a, $3
 	ret
