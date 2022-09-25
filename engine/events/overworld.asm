9f6eef1d2027035fb4969cca10cc06b1905fad22
The Void <9028176+etdv-thevoid@users.noreply.github.com>
2022-09-25 13:53:59 -0500

    Add check for facing sprite when fishing (#745)
    Fix from https://pret.github.io/pokecrystal/bugs_and_glitches.html#you-can-fish-on-top-of-npcs

--- engine/events/overworld.asm
+++ engine/events/overworld.asm
@@ -1557,7 +1557,9 @@ FishFunction:
 	call GetFacingTileCoord
 	call GetTileCollision
 	dec a ; cp WATER_TILE
-	jr z, .facingwater
+	jr nz, .fail
+	farcall CheckFacingObject
+	jr nc, .facingwater
 .fail
 	ld a, $3
 	ret
