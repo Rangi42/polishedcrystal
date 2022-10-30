f8b71c5c538a0cba0e02b9130ae890cc2beebebb
Rangi <remy.oukaour+rangi42@gmail.com>
2022-10-30 00:37:01 -0400

    Fix entering Rugged Road's bridge from the north

--- maps/RuggedRoadNorth.asm
+++ maps/RuggedRoadNorth.asm
@@ -2,6 +2,7 @@ RuggedRoadNorth_MapScriptHeader:
 	def_scene_scripts
 
 	def_callbacks
+	callback MAPCALLBACK_TILES, RuggedRoadNorthBridgeCallback
 
 	def_warp_events
 	warp_event 10,  5, SNOWTOP_MOUNTAIN_INSIDE, 1
@@ -13,3 +14,13 @@ RuggedRoadNorth_MapScriptHeader:
 	bg_event 18, 15, BGEVENT_JUMPSTD, cavegrotto, HIDDENGROTTO_RUGGED_ROAD_NORTH
 
 	def_object_events
+
+RuggedRoadNorthBridgeCallback:
+	callasm .RuggedRoadSouthBridgeUnderfoot
+	endcallback
+
+.RuggedRoadSouthBridgeUnderfoot:
+	ld a, $1
+	ld [wWalkingOnBridge], a
+	ld [wRuggedRoadSouthSceneID], a ; setscene a
+	ret
