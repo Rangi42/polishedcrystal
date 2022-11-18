e9d3a9e5eb63adaeb24ebfb0ac8dde7b3e3f846c
ShadowOne333 <shadow_one666@hotmail.com>
2022-11-18 14:57:34 -0600

    Prep work to avoid conflicts on new commit pull from master

--- gfx/pokemon/arcanine_hisuian/anim_idle.asm
+++
@@ -1,7 +0,0 @@
-	setrepeat 2
-	frame 0, 08
-	frame 1, 05
-	frame 2, 05
-	frame 1, 05
-	dorepeat 1
-	endanim
--- gfx/pokemon/kleavor/anim_idle.asm
+++
@@ -1,7 +0,0 @@
-	frame 0, 08
-	setrepeat 1
-	frame 4, 04
-	frame 5, 04
-	frame 4, 04
-	dorepeat 2
-	endanim
