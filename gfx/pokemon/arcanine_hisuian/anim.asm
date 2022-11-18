7bf74341cefcf0683dcad1071e05eff63f6bb037
Rangi <remy.oukaour+rangi42@gmail.com>
2022-11-18 00:05:09 -0500

    Use Cure's sprites of Kleavor and Hisuian Arcanine (animated by ShadowOne333)

--- gfx/pokemon/arcanine_hisuian/anim.asm
+++ gfx/pokemon/arcanine_hisuian/anim.asm
@@ -1,16 +1,13 @@
 	frame 0, 08
-	frame 1, 05
-	frame 2, 05
-	frame 1, 05
-	frame 0, 08
-	frame 3, 06
+	setrepeat 2
+	frame 2, 07
+	frame 3, 07
+	dorepeat 2
+	frame 0, 06
 	frame 4, 06
 	frame 5, 06
-	setrepeat 2
 	frame 6, 06
 	frame 7, 06
-	frame 8, 08
-	dorepeat 9
-	frame 9, 06
-	frame 10, 06
+	frame 5, 06
+	frame 4, 06
 	endanim
