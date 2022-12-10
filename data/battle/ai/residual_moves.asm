8df44fbe78dd33f3288f44a35b8ee08065468783
vulcandth <vulcandth@gmail.com>
2022-12-09 21:46:03 -0600

    Refactor Battle AI Scoring

---
+++ data/battle/ai/residual_moves.asm
@@ -0,0 +1,14 @@
+; AI_CAUTIOUS discourages these moves after the first turn.
+
+ResidualMoves:
+	db CONVERSION
+	db FOCUS_ENERGY
+	db LEECH_SEED
+	db POISONPOWDER
+	db SPIKES
+	db STUN_SPORE
+	db SUBSTITUTE
+	db THUNDER_WAVE
+	db TOXIC_SPIKES
+	db TRANSFORM
+	db -1 ; end
---
+++ data/battle/ai/useful_moves.asm
@@ -0,0 +1,28 @@
+; AI_SMART knows these moves are usable all-around
+
+UsefulMoves:
+; Moves that are usable all-around.
+	db DOUBLE_EDGE
+	db SING
+	db FLAMETHROWER
+	db HYDRO_PUMP
+	db SURF
+	db ICE_BEAM
+	db BLIZZARD
+	db HYPER_BEAM
+	db SLEEP_POWDER
+	db THUNDERBOLT
+	db THUNDER
+	db EARTHQUAKE
+	db TOXIC
+	db PSYCHIC_M
+	db HYPNOSIS
+	db WILL_O_WISP
+	db RECOVER
+	db FIRE_BLAST
+	db FRESH_SNACK
+	db SUPER_FANG
+	db MOONBLAST
+	db PLAY_ROUGH
+	db HURRICANE
+	db -1 ; end
