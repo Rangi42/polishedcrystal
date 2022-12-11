d26ece3f20363d26dd19f997ef7b6772a182037f
vulcandth <vulcandth@gmail.com>
2022-12-10 18:44:00 -0600

    Refactor Battle AI Scoring (#760)

---
+++ data/battle/ai/risky_effects.asm
@@ -0,0 +1,6 @@
+; AI_RISKY will not use these effects at 50%+ HP.
+; even if they would KO the player.
+
+RiskyEffects:
+	db EFFECT_EXPLOSION
+	db -1 ; end
---
+++ data/battle/ai/status_only_effects.asm
@@ -0,0 +1,11 @@
+; AI_BASIC discourages these effects if the player
+; already has a status condition.
+
+StatusOnlyEffects:
+	db EFFECT_SLEEP
+	db EFFECT_TOXIC
+	db EFFECT_POISON
+	db EFFECT_PARALYZE
+	db EFFECT_BURN
+	db EFFECT_FREEZE
+	db -1 ; end
