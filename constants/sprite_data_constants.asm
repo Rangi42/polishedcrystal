e235bf37e5393f2caf0f3e3a4cd50642f3f065b4
vulcandth <vulcandth@gmail.com>
2023-01-14 17:15:01 -0600

    Add `PAL_NPC_LIGHTNING_CRYSTAL`

--- constants/sprite_data_constants.asm
+++ constants/sprite_data_constants.asm
@@ -24,27 +24,28 @@ ENDM
 ; sprite palettes
 ; MapObjectPals indexes (see gfx/overworld/npc_sprites.pal)
 	const_def
-	ow_npc_pal_const RED            ; 00
-	ow_npc_pal_const BLUE           ; 01
-	ow_npc_pal_const GREEN          ; 02
-	ow_npc_pal_const BROWN          ; 03
-	ow_npc_pal_const PURPLE         ; 04
-	ow_npc_pal_const SILVER         ; 05
-	ow_npc_pal_const TREE           ; 06
-	ow_npc_pal_const ROCK           ; 07
-	ow_npc_pal_const GRAY           ; 08
-	ow_npc_pal_const PINK           ; 09
-	ow_npc_pal_const TEAL           ; 0a
-	ow_npc_pal_const EMOTE_BLACK    ; 0b
-	ow_npc_pal_const EMOTE_GREEN    ; 0c
-	ow_npc_pal_const EMOTE_PURPLE   ; 0d
-	ow_npc_pal_const EMOTE_RED      ; 0e
-	ow_npc_pal_const EMOTE_ORANGE   ; 0f
-	ow_npc_pal_const EMOTE_BLUE     ; 10
-	ow_npc_pal_const MARLON         ; 11
-	ow_npc_pal_const ELECTRIC_FENCE ; 12
+	ow_npc_pal_const RED               ; 00
+	ow_npc_pal_const BLUE              ; 01
+	ow_npc_pal_const GREEN             ; 02
+	ow_npc_pal_const BROWN             ; 03
+	ow_npc_pal_const PURPLE            ; 04
+	ow_npc_pal_const SILVER            ; 05
+	ow_npc_pal_const TREE              ; 06
+	ow_npc_pal_const ROCK              ; 07
+	ow_npc_pal_const GRAY              ; 08
+	ow_npc_pal_const PINK              ; 09
+	ow_npc_pal_const TEAL              ; 0a
+	ow_npc_pal_const EMOTE_BLACK       ; 0b
+	ow_npc_pal_const EMOTE_GREEN       ; 0c
+	ow_npc_pal_const EMOTE_PURPLE      ; 0d
+	ow_npc_pal_const EMOTE_RED         ; 0e
+	ow_npc_pal_const EMOTE_ORANGE      ; 0f
+	ow_npc_pal_const EMOTE_BLUE        ; 10
+	ow_npc_pal_const MARLON            ; 11
+	ow_npc_pal_const ELECTRIC_FENCE    ; 12
+	ow_npc_pal_const LIGHTNING_CRYSTAL ; 13
 DEF NUM_OW_STD_PALS EQU const_value
-	ow_npc_pal_const COPY_BG_GREEN ; 13
-	ow_npc_pal_const COPY_BG_BROWN ; 14
+	ow_npc_pal_const COPY_BG_GREEN     ; 14
+	ow_npc_pal_const COPY_BG_BROWN     ; 15
 DEF NUM_OW_PALS EQU const_value
 	assert NUM_OW_PALS < 32 ; PAL_NPC_* must fit in 5 bits
