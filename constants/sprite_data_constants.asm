a8b54123c54aedc45e04be07fe79e57e30a866ec
Rangi <remy.oukaour+rangi42@gmail.com>
2023-02-16 22:57:09 -0500

    Make emotes be individual (non-time-of-day) palettes

--- constants/sprite_data_constants.asm
+++ constants/sprite_data_constants.asm
@@ -29,21 +29,21 @@ ENDM
 	ow_npc_pal_const GREEN             ; 02
 	ow_npc_pal_const BROWN             ; 03
 	ow_npc_pal_const PURPLE            ; 04
-	ow_npc_pal_const SILVER            ; 05
-	ow_npc_pal_const TREE              ; 06
-	ow_npc_pal_const ROCK              ; 07
-	ow_npc_pal_const GRAY              ; 08
-	ow_npc_pal_const PINK              ; 09
-	ow_npc_pal_const TEAL              ; 0a
-	ow_npc_pal_const EMOTE_BLACK       ; 0b
-	ow_npc_pal_const EMOTE_GREEN       ; 0c
-	ow_npc_pal_const EMOTE_PURPLE      ; 0d
-	ow_npc_pal_const EMOTE_RED         ; 0e
-	ow_npc_pal_const EMOTE_ORANGE      ; 0f
-	ow_npc_pal_const EMOTE_BLUE        ; 10
-	ow_npc_pal_const MARLON            ; 11
-	ow_npc_pal_const SAILBOAT          ; 12
+	ow_npc_pal_const GRAY              ; 05
+	ow_npc_pal_const PINK              ; 06
+	ow_npc_pal_const TEAL              ; 07
+	ow_npc_pal_const MARLON            ; 08
+	ow_npc_pal_const TREE              ; 09
+	ow_npc_pal_const ROCK              ; 0a
+	ow_npc_pal_const SAILBOAT          ; 0b
 DEF NUM_OW_TIME_OF_DAY_PALS EQU const_value
+	ow_npc_pal_const SILVER            ; 0c
+	ow_npc_pal_const EMOTE_BLACK       ; 0d
+	ow_npc_pal_const EMOTE_GREEN       ; 0e
+	ow_npc_pal_const EMOTE_PURPLE      ; 0f
+	ow_npc_pal_const EMOTE_RED         ; 10
+	ow_npc_pal_const EMOTE_ORANGE      ; 11
+	ow_npc_pal_const EMOTE_BLUE        ; 12
 	ow_npc_pal_const ELECTRIC_FENCE    ; 13
 	ow_npc_pal_const LIGHTNING_CRYSTAL ; 14
 	ow_npc_pal_const ROCK_CRYSTAL      ; 15
