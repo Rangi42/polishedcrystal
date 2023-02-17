e736e98a32434ea4aede018545e49c2fedf0a458
Rangi <remy.oukaour+rangi42@gmail.com>
2023-02-17 12:27:32 -0500

    Use `PAL_NPC_POKE_BALL`, `PAL_NPC_KEY_ITEM` (green), `PAL_NPC_DECO_ITEM` (blue), and `PAL_NPC_YELLOW` (TMs) for item balls

--- constants/sprite_data_constants.asm
+++ constants/sprite_data_constants.asm
@@ -34,36 +34,40 @@ ENDM
 	ow_npc_pal_const TEAL              ; 07
 	ow_npc_pal_const YELLOW            ; 08
 	ow_npc_pal_const ORANGE            ; 09
-	ow_npc_pal_const WHITE             ; 0a
-	ow_npc_pal_const BLACK             ; 0b
-	ow_npc_pal_const MARLON            ; 0c
-	ow_npc_pal_const SAILBOAT          ; 0d
+	ow_npc_pal_const AZURE             ; 0a
+	ow_npc_pal_const WHITE             ; 0b
+	ow_npc_pal_const BLACK             ; 0c
+	ow_npc_pal_const POKE_BALL         ; 0d (red)
+	ow_npc_pal_const DECO_ITEM         ; 0e (blue)
+	ow_npc_pal_const KEY_ITEM          ; 0f (green)
+	ow_npc_pal_const MARLON            ; 10
+	ow_npc_pal_const SAILBOAT          ; 11
 DEF NUM_OW_TIME_OF_DAY_PALS EQU const_value
-	ow_npc_pal_const EMOTE_GRAY        ; 0e
-	ow_npc_pal_const EMOTE_BLACK       ; 0f
-	ow_npc_pal_const EMOTE_GREEN       ; 10
-	ow_npc_pal_const EMOTE_PURPLE      ; 11
-	ow_npc_pal_const EMOTE_RED         ; 12
-	ow_npc_pal_const EMOTE_ORANGE      ; 13
-	ow_npc_pal_const EMOTE_BLUE        ; 14
-	ow_npc_pal_const ELECTRIC_FENCE    ; 15
-	ow_npc_pal_const LIGHTNING_CRYSTAL ; 16
-	ow_npc_pal_const ROCK_CRYSTAL      ; 17
-	ow_npc_pal_const N64               ; 18
-	ow_npc_pal_const POKECOM_SIGN      ; 19
-	ow_npc_pal_const RANGI             ; 1a
-	ow_npc_pal_const DRILL             ; 1b
-	ow_npc_pal_const PEARL             ; 1c
-	ow_npc_pal_const TINY_WINDOW       ; 1d
+	ow_npc_pal_const EMOTE_GRAY        ; 12
+	ow_npc_pal_const EMOTE_BLACK       ; 13
+	ow_npc_pal_const EMOTE_GREEN       ; 14
+	ow_npc_pal_const EMOTE_PURPLE      ; 15
+	ow_npc_pal_const EMOTE_RED         ; 16
+	ow_npc_pal_const EMOTE_ORANGE      ; 17
+	ow_npc_pal_const EMOTE_BLUE        ; 18
+	ow_npc_pal_const ELECTRIC_FENCE    ; 19
+	ow_npc_pal_const LIGHTNING_CRYSTAL ; 1a
+	ow_npc_pal_const ROCK_CRYSTAL      ; 1b
+	ow_npc_pal_const N64               ; 1c
+	ow_npc_pal_const POKECOM_SIGN      ; 1d
+	ow_npc_pal_const RANGI             ; 1e
+	ow_npc_pal_const DRILL             ; 1f
+	ow_npc_pal_const PEARL             ; 20
+	ow_npc_pal_const TINY_WINDOW       ; 21
 DEF NUM_OW_INDIVIDUAL_PALS EQU const_value - NUM_OW_TIME_OF_DAY_PALS
 DEF FIRST_COPY_BG_PAL EQU const_value
-	ow_npc_pal_const COPY_BG_GRAY      ; 1e
-	ow_npc_pal_const COPY_BG_RED       ; 1f
-	ow_npc_pal_const COPY_BG_GREEN     ; 20
-	ow_npc_pal_const COPY_BG_WATER     ; 21
-	ow_npc_pal_const COPY_BG_YELLOW    ; 22
-	ow_npc_pal_const COPY_BG_BROWN     ; 23
-	ow_npc_pal_const COPY_BG_ROOF      ; 24
-	ow_npc_pal_const COPY_BG_TEXT      ; 25
+	ow_npc_pal_const COPY_BG_GRAY      ; 22
+	ow_npc_pal_const COPY_BG_RED       ; 23
+	ow_npc_pal_const COPY_BG_GREEN     ; 24
+	ow_npc_pal_const COPY_BG_WATER     ; 25
+	ow_npc_pal_const COPY_BG_YELLOW    ; 26
+	ow_npc_pal_const COPY_BG_BROWN     ; 27
+	ow_npc_pal_const COPY_BG_ROOF      ; 28
+	ow_npc_pal_const COPY_BG_TEXT      ; 29
 DEF NUM_OW_BG_COPY_PALS EQU const_value - NUM_OW_INDIVIDUAL_PALS
 DEF NUM_OW_PALS EQU const_value
