f5af56c20fb2db2fff31abd92b25131f2b19ef4b
Andrew Cook <ariscop@gmail.com>
2023-03-31 08:08:48 +1100

    Fix Violet gym guy throwing you across the street (#835)
    * Fix Violet gym guy throwing you across the street
    
    Likely introduced by 9b322012
    
    This uses the warp event instead of hard coding warp coordinates
    
    * warpfacing -> warpcheck Route34 lyra encounter
    
    * warp -> warpcheck EcruteakGymClosed

--- maps/VioletGym.asm
+++ maps/VioletGym.asm
@@ -33,11 +33,9 @@ VioletGymFalknerAwayScript:
 	applyonemovement VIOLETGYM_GYM_GUY2, step_down
 	showtext VioletGymGuyFalknerAwayText
 	turnobject PLAYER, DOWN
-	pause 10
-	special FadeOutPalettes
-	playsound SFX_ENTER_DOOR
-	waitsfx
-	warp VIOLET_CITY, 18, 22
+	warpcheck
+	warpsound
+	newloadmap MAPSETUP_DOOR
 	end
 
 VioletGymFalknerScript:
