18cd9faf74d9d56c954c3c42d05826f6cc099975
vulcandth <vulcandth@gmail.com>
2023-08-03 10:14:47 -0500

    Fix `*hl++|*hl-- = b|c|d|e` "Good" comment

--- utils/optimize.py
+++ utils/optimize.py
@@ -338,7 +338,7 @@ patterns = {
 ],
 '*hl++|*hl-- = b|c|d|e': [
 	# Bad: ld [hl], b|c|d|e / inc|dec hl (unless you can't use a)
-	# Good: ld [hli|hld], a / ld b|c|d|e, a
+	# Good: ld a, b|c|d|e / ld [hli|hld], a
 	(lambda line1, prev: re.match(r'ld \[hl\], [bcde]', line1.code)),
 	(lambda line2, prev: line2.code in {'inc hl', 'dec hl'}),
 ],
