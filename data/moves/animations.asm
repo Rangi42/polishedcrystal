4c3e0e2f8a8a5827de08a01c9b842ee31a4249d5
vulcandth <vulcandth@gmail.com>
2024-01-01 13:27:33 -0600

    Bugfix: Tackle animation sprite limit
    Co-Authored-By: 8bitZeta <37845033+8bitZeta@users.noreply.github.com>
    Co-Authored-By: SoupPotato <22991349+souppotato@users.noreply.github.com>
    Co-Authored-By: Orion <36967886+monstarules@users.noreply.github.com>

--- data/moves/animations.asm
+++ data/moves/animations.asm
@@ -2351,7 +2351,7 @@ BattleAnim_ZenHeadbutt:
 
 BattleAnim_Tackle:
 	anim_1gfx ANIM_GFX_HIT
-	anim_call BattleAnim_TargetObj_2Row
+	anim_call BattleAnim_TargetObj_1Row
 	anim_bgeffect ANIM_BG_TACKLE, $0, $1, $0
 	anim_wait 4
 	anim_sound 0, 1, SFX_TACKLE
