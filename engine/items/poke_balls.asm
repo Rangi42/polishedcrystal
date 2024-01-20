284a63296b9051e0bf72d266039c2a77a7cef404
Fredrik Ljungdahl <fredde1994@gmail.com>
2024-01-20 03:24:33 +0100

    Fix Fast Ball

--- engine/items/poke_balls.asm
+++ engine/items/poke_balls.asm
@@ -371,9 +371,12 @@ LoveBallMultiplier:
 FastBallMultiplier:
 ; multiply catch rate by 4 if enemy mon has base speed 100+
 	ld a, [wOTPartyMon1Species]
-	dec a
+	ld c, a
+	ld a, [wOTPartyMon1Form]
+	ld b, a
+	call GetSpeciesAndFormIndex
 	ld hl, BaseData + BASE_SPE
-	ld bc, BASE_DATA_SIZE
+	ld a, BASE_DATA_SIZE
 	rst AddNTimes
 	ld a, BANK(BaseData)
 	call GetFarByte
