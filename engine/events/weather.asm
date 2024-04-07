423e6ae062ed6844ff324d477f8530e508a5f38e
vulcandth <vulcandth@gmail.com>
2024-04-06 22:44:38 -0500

    remove commented out code

--- engine/events/weather.asm
+++ engine/events/weather.asm
@@ -105,39 +105,3 @@ SetCurrentWeather::
 	ld b, h
 	ld c, l
 	jr .loop
-
-
-
-
-
-;SetCurrentWeather::
-; This could be populated with a more robust weather system, but for now it's just random
-;	ld a, [wCurrentWeather]
-;	ld b, a
-;	call GetMapEnvironment
-;	call CheckOutdoorMap
-;	ld a, OW_WEATHER_NONE
-;	jr nz, .got_weather ; no weather indoors.
-;	ld a, NUM_WEATHERS
-;	call RandomRange
-;.got_weather
-;	ld [wCurrentWeather], a
-;	dec a ; OW_WEATHER_RAIN
-;	ld a, WEATHER_RAIN
-;	ld [wBattleWeather], a
-;	jr z, .done_battle_weather
-;	; OW_WEATHER_HAIL
-;	ld a, WEATHER_HAIL
-;	ld [wBattleWeather], a
-;	farcall LoadWeatherGraphics
-;.done_battle_weather
-;	ld a, 255
-;	ld [wWeatherCount], a
-;	xor a
-;	ld [wOverworldWeatherCooldown], a
-;	ld a, [wCurrentWeather]
-;	cp b
-;	ret z
-;	ld a, 90
-;	ld [wOverworldWeatherCooldown], a
-;	ret
