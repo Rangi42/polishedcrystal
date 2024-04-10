f9ac6c27a56dfe09c408cc954083df04679f3aa6
vulcandth <vulcandth@gmail.com>
2024-04-10 17:59:07 -0500

    Tie Weather into overcast system

--- engine/events/weather.asm
+++ engine/events/weather.asm
@@ -8,78 +8,11 @@ MACRO ow_weather_map
 	endr
 ENDM
 
-OverworldWeatherMaps:
-	; map, event, rain %, thunderstorm %, snow %, sandstorm %
-	; first matching map & event will be used
-	ow_weather_map STORMY_BEACH,                                       -1, 100,   0,   0,   0
-	ow_weather_map RUGGED_ROAD_NORTH,                                  -1,   0,   0,   0, 100
-	ow_weather_map RUGGED_ROAD_SOUTH,                                  -1,   0,   0,   0, 100
-	ow_weather_map SNOWTOP_MOUNTAIN_INSIDE,                            -1,   0,   0, 100,   0
-	ow_weather_map SNOWTOP_MOUNTAIN_OUTSIDE,                           -1,   0,   0, 100,   0
-	ow_weather_map LAKE_OF_RAGE,             EVENT_LAKE_OF_RAGE_CIVILIANS, 100, 100,   0,   0
-	ow_weather_map LAKE_OF_RAGE,                                       -1,  20,   0,   0,   0
-	db -1 ; end
-
-
 SetCurrentWeather::
-	ld bc, OverworldWeatherMaps
-	ld hl, OverworldWeatherMaps
-.loop
-	ld a, [wMapGroup]
-	ld d, a
-	ld a, [wMapNumber]
-	ld e, a
-	ld a, [hli]
-	cp -1
-	jr z, .no_weather
-	cp d
-	jr nz, .skip_row
-	ld a, [hli]
-	cp e
-	jr nz, .skip_row
-	ld a, [hli]
-	cp -1
-	jr z, .ignore_flag
-	ld e, a
-	ld a, [hl]
-	ld d, a
-	push hl
-	push de
-	push bc
-	ld b, CHECK_FLAG
-	call EventFlagAction
-	pop bc
-	pop de
-	pop hl
-	jr z, .skip_row
-.ignore_flag
-	inc hl
-	call Random
-	ld b, a
-	ld a, [hli] ; rain %
-	cp b
+	farcall GetOvercastIndex
+	and a
+	jr z, .not_overcast
 	ld a, OW_WEATHER_RAIN
-	jr nc, .set_weather
-	call Random
-	ld b, a
-	ld a, [hli] ; thunderstorm %
-	cp b
-	ld a, OW_WEATHER_THUNDERSTORM
-	jr nc, .set_weather
-	call Random
-	ld b, a
-	ld a, [hli] ; snow %
-	cp b
-	ld a, OW_WEATHER_SNOW
-	jr nc, .set_weather
-	call Random
-	ld b, a
-	ld a, [hl] ; sandstorm %
-	cp b
-	ld a, OW_WEATHER_SANDSTORM
-	jr nc, .set_weather
-.no_weather
-	ld a, OW_WEATHER_NONE
 .set_weather
 	ld b, a
 	ld a, [wCurrentWeather]
@@ -87,7 +20,7 @@ SetCurrentWeather::
 	jr nz, .do_cooldown
 	ld a, b
 	ld [wCurrentWeather], a
-	farcall LoadWeatherGraphics
+	call LoadWeatherGraphics
 	xor a
 	ld [wOverworldWeatherCooldown], a
 	ret
@@ -100,10 +33,133 @@ SetCurrentWeather::
 	ld [wOverworldWeatherCooldown], a
 	ret
 
+.not_overcast
+	ld a, [wMapGroup]
+	cp GROUP_SNOWTOP_MOUNTAIN_OUTSIDE
+	jr nz, .not_snowing
+	ld a, [wMapNumber]
+	cp MAP_SNOWTOP_MOUNTAIN_OUTSIDE
+	jr z, .snowing
+	cp MAP_SNOWTOP_MOUNTAIN_INSIDE
+	jr nz, .not_snowing
+.snowing
+	ld a, OW_WEATHER_SNOW
+	jr .set_weather
 
-.skip_row
-	ld hl, 8
-	add hl, bc
-	ld b, h
-	ld c, l
-	jr .loop
+.not_snowing
+	ld a, [wMapGroup]
+	cp GROUP_RUGGED_ROAD_NORTH
+	jr nz, .no_weather
+	ld a, [wMapNumber]
+	cp MAP_RUGGED_ROAD_NORTH
+	jr z, .sandstorm
+	cp MAP_RUGGED_ROAD_SOUTH
+	jr nz, .no_weather
+.sandstorm
+	ld a, OW_WEATHER_SANDSTORM
+	jr .set_weather
+
+.no_weather
+	ld a, OW_WEATHER_NONE
+	jr .set_weather
+
+
+;OverworldWeatherMaps:
+	; map, event, rain %, thunderstorm %, snow %, sandstorm %
+	; first matching map & event will be used
+;	ow_weather_map STORMY_BEACH,                                       -1, 100,   0,   0,   0
+;	ow_weather_map RUGGED_ROAD_NORTH,                                  -1,   0,   0,   0, 100
+;	ow_weather_map RUGGED_ROAD_SOUTH,                                  -1,   0,   0,   0, 100
+;	ow_weather_map SNOWTOP_MOUNTAIN_INSIDE,                            -1,   0,   0, 100,   0
+;	ow_weather_map SNOWTOP_MOUNTAIN_OUTSIDE,                           -1,   0,   0, 100,   0
+;	ow_weather_map LAKE_OF_RAGE,             EVENT_LAKE_OF_RAGE_CIVILIANS, 100, 100,   0,   0
+;	ow_weather_map LAKE_OF_RAGE,                                       -1,  20,   0,   0,   0
+;	db -1 ; end
+
+
+;SetCurrentWeather::
+;	ld bc, OverworldWeatherMaps
+;	ld hl, OverworldWeatherMaps
+;.loop
+;	ld a, [wMapGroup]
+;	ld d, a
+;	ld a, [wMapNumber]
+;	ld e, a
+;	ld a, [hli]
+;	cp -1
+;	jr z, .no_weather
+;	cp d
+;	jr nz, .skip_row
+;	ld a, [hli]
+;	cp e
+;	jr nz, .skip_row
+;	ld a, [hli]
+;	cp -1
+;	jr z, .ignore_flag
+;	ld e, a
+;	ld a, [hl]
+;	ld d, a
+;	push hl
+;	push de
+;	push bc
+;	ld b, CHECK_FLAG
+;	call EventFlagAction
+;	pop bc
+;	pop de
+;	pop hl
+;	jr z, .skip_row
+;.ignore_flag
+;	inc hl
+;	call Random
+;	ld b, a
+;	ld a, [hli] ; rain %
+;	cp b
+;	ld a, OW_WEATHER_RAIN
+;	jr nc, .set_weather
+;	call Random
+;	ld b, a
+;	ld a, [hli] ; thunderstorm %
+;	cp b
+;	ld a, OW_WEATHER_THUNDERSTORM
+;	jr nc, .set_weather
+;	call Random
+;	ld b, a
+;	ld a, [hli] ; snow %
+;	cp b
+;	ld a, OW_WEATHER_SNOW
+;	jr nc, .set_weather
+;	call Random
+;	ld b, a
+;	ld a, [hl] ; sandstorm %
+;	cp b
+;	ld a, OW_WEATHER_SANDSTORM
+;	jr nc, .set_weather
+;.no_weather
+;	ld a, OW_WEATHER_NONE
+;.set_weather
+;	ld b, a
+;	ld a, [wCurrentWeather]
+;	cp OW_WEATHER_NONE
+;	jr nz, .do_cooldown
+;	ld a, b
+;	ld [wCurrentWeather], a
+;	farcall LoadWeatherGraphics
+;	xor a
+;	ld [wOverworldWeatherCooldown], a
+;	ret
+;
+;.do_cooldown
+;	ld [wPrevWeather], a
+;	ld a, b
+;	ld [wCurrentWeather], a
+;	ld a, 32
+;	ld [wOverworldWeatherCooldown], a
+;	ret
+;
+;
+;.skip_row
+;	ld hl, 8
+;	add hl, bc
+;	ld b, h
+;	ld c, l
+;	jr .loop
