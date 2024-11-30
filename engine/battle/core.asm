6f91ed30798642dfebfbf42450dfdfb1dcc2fc46
vulcandth <vulcandth@gmail.com>
2024-11-29 19:27:59 -0600

    Don't farcall EvolveDuringBattle and cleanup commented out code

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -6681,13 +6681,8 @@ GiveExperiencePoints:
 	pop af
 	ld [wCurPartyLevel], a
 
-;	ld hl, wEvolvableFlags
-;	ld a, [wCurPartyMon]
-;	ld c, a
-;	ld b, SET_FLAG
-;	predef FlagPredef
 	call LoadTileMapToTempTileMap
-	farcall EvolveDuringBattle
+	call EvolveDuringBattle
 	call UpdatePlayerHPPal
 	call _LoadBattleFontsHPBar
 	call GetMonBackpic
