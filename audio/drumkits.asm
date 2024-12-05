79f270473d16ef38fcfcf68045ddacf707f69cf4
Rangi42 <sylvie.oukaour+rangi42@gmail.com>
2024-12-05 16:06:41 -0500

    Fix indentation

--- audio/drumkits.asm
+++ audio/drumkits.asm
@@ -92,18 +92,18 @@ Drumkit5:
 	dr Snare13
 	dr Kick2
 Drumkit6:
-    dr Drum00
-    dr Kick2
-    dr Snare9
-    dr Snare10
-    dr Snare11
-    dr Crash2
-    dr Crash1
-    dr Snare9
-    dr Snare9
-    dr Snare9
-    dr Snare9
-    dr Snare9
+	dr Drum00
+	dr Kick2
+	dr Snare9
+	dr Snare10
+	dr Snare11
+	dr Crash2
+	dr Crash1
+	dr Snare9
+	dr Snare9
+	dr Snare9
+	dr Snare9
+	dr Snare9
 
 Drum00:
 ; unused
