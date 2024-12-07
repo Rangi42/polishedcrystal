26bb644e26df550cf0287d4859938e59cc8e0b90
vulcandth <vulcandth@gmail.com>
2024-12-07 11:34:47 -0600

    Fix Evolve During Battle Sound Stack Imbalance

--- engine/battle/evolve_battle.asm
+++ engine/battle/evolve_battle.asm
@@ -7,6 +7,9 @@ EvolveDuringBattle::
 	ld a, [hl]
 	ld [wEvolutionOldForm], a
 
+	farcall CheckHowToEvolve
+	ret z
+
 	ld a, BANK("Sound Stack")
 	ldh [rSVBK], a
 	ld hl, wSoundEngineBackup
@@ -23,9 +26,6 @@ EvolveDuringBattle::
 	ei
 	ld a, $1
 	ldh [rSVBK], a
-
-	farcall CheckHowToEvolve
-	ret z
 
 	farcall TryToEvolve
 	jr c, .canceled
