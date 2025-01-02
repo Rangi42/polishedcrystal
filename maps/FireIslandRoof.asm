68669959f8a356c8c20726df9b1522e287dbf29a
vulcandth <vulcandth@gmail.com>
2025-01-02 12:04:56 -0600

    Remove pGO leaders from Shamouti Island

--- maps/FireIslandRoof.asm
+++ maps/FireIslandRoof.asm
@@ -11,51 +11,5 @@ FireIslandRoof_MapScriptHeader:
 	def_bg_events
 
 	def_object_events
-	object_event  5,  5, SPRITE_CANDELA, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, FireIslandRoofCandelaScript, EVENT_SHAMOUTI_COAST_CANDELA
 
 	object_const_def
-	const FIREISLANDROOF_CANDELA
-
-FireIslandRoofCandelaScript:
-	faceplayer
-	opentext
-	writetext .GreetingText
-	yesorno
-	iffalsefwd .Refused
-	writetext .SeenText
-	waitbutton
-	closetext
-	winlosstext .BeatenText, 0
-	loadtrainer CANDELA, 1
-	startbattle
-	reloadmapafterbattle
-	setevent EVENT_BEAT_CANDELA
-	showtext .AfterText
-	playsound SFX_WARP_TO
-	applyonemovement FIREISLANDROOF_CANDELA, teleport_from
-	disappear FIREISLANDROOF_CANDELA
-	clearevent EVENT_CELADON_UNIVERSITY_CANDELA
-	end
-
-.Refused:
-	jumpopenedtext .RefusedText
-
-.GreetingText:
-	text "TODO"
-	done
-
-.SeenText:
-	text "TODO"
-	done
-
-.BeatenText:
-	text "TODO"
-	done
-
-.AfterText:
-	text "TODO"
-	done
-
-.RefusedText:
-	text "TODO"
-	done
