1d3345e8329e77c8726db298d7862835f45b5666
SoupPotato <22991349+souppotato@users.noreply.github.com>
2025-01-11 23:31:25 -0600

    Updated to use all latest SourCrystal sprites

--- gfx/pokemon/dewgong/anim_idle.asm
+++ gfx/pokemon/dewgong/anim_idle.asm
@@ -3,6 +3,6 @@
 	frame 6, 06
 	frame 7, 06
 	frame 8, 06
-	frame 0, 06
+	frame 9, 06
 	dorepeat 1
 	endanim
--- gfx/pokemon/lugia/anim_idle.asm
+++ gfx/pokemon/lugia/anim_idle.asm
@@ -1,5 +1,5 @@
 	setrepeat 3
-	frame 0, 10
+	frame 9, 10
 	frame 7, 10
 	dorepeat 1
 	frame 8, 10
