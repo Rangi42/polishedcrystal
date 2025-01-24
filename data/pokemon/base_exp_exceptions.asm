a3b6b225153adfd8b4a45ed15963cf878f04ffb8
Fredrik Ljungdahl <fredde1994@gmail.com>
2025-01-24 06:27:46 +0100

    typo

--- data/pokemon/base_exp_exceptions.asm
+++ data/pokemon/base_exp_exceptions.asm
@@ -6,5 +6,5 @@ ENDM
 
 NewBaseExpExceptions:
 	base_exception CHANSEY, 395 ; BST x 87.7% as of gen V (unchanged in VIII)
-	base_exception BLISSEY, 634 ; BST x 117.5% as of gen VIII
+	base_exception BLISSEY, 635 ; BST x 117.5% as of gen VIII
 	dw -1
