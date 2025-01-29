16abe88278722d9b4142314686d8a8289f77fb4a
emilyploszaj <emilyploszaj@gmail.com>
2025-01-28 19:31:49 -0600

    Store abnormal contact moves instead of all contact moves

---
+++ data/moves/abnormal_contact_moves.asm
@@ -0,0 +1,27 @@
+; Typically, a move makes contact if a move's category is Physical
+; This lists all exceptions
+AbnormalContactMoves:
+	; Special moves that make contact
+	db DRAINING_KISS
+	db PETAL_DANCE
+	; Physical moves that don't make contact
+	db BONEMERANG
+	db BULLDOZE
+	db EARTHQUAKE
+	db EXPLOSION
+	db GUNK_SHOT
+	db ICE_SHARD
+	db ICICLE_CRASH
+	db ICICLE_SPEAR
+	db MAGNITUDE
+	db PAY_DAY
+	db PIN_MISSILE
+	db POISON_STING
+	db RAZOR_LEAF
+	db ROCK_BLAST
+	db ROCK_SLIDE
+	db ROCK_THROW
+	db SACRED_FIRE
+	db SEED_BOMB
+	db STONE_EDGE
+	db STRUGGLE ; -1
