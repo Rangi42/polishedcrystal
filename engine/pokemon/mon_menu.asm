0058c7a7f51ebce3b77111b0ab6a2b18d182c3e6
vulcandth <vulcandth@gmail.com>
2025-01-31 19:26:08 -0600

    Fix stats screen pokemon cry & music volume issues
    Resolves #1005

--- engine/pokemon/mon_menu.asm
+++ engine/pokemon/mon_menu.asm
@@ -733,13 +733,14 @@ _OpenPartyStats:
 	ld a, TEMPMON
 	ld [wMonType], a
 	predef StatsScreenInit
-	; This ensures that MaxVolume works as it should if we're in the middle of
-	; playing a cry.
+	; check if the cry is still playing
+	call CheckSFX
 	ld a, MAX_VOLUME
+	jr nz, .still_playing_cry
+	xor a
+.still_playing_cry
 	ld [wLastVolume], a
 	call MaxVolume
-	xor a
-	ld [wLastVolume], a
 	call ExitMenu
 	xor a
 	ret
