e4ecfb2f1caae34e06cb9685d3a6734f58c595fd
darsh <twitteruserdarsh@gmail.com>
2025-03-23 03:04:33 -0400

    initial magikarp should be 3'6"

--- constants/misc_constants.asm
+++ constants/misc_constants.asm
@@ -44,7 +44,7 @@ DEF HOF_MASTER_COUNT EQU 200
 DEF KIRK_SHUCKIE_ID EQU 00518
 
 ; initial best Magikarp length (in mm)
-DEF BEST_MAGIKARP_LENGTH EQU 1053
+DEF BEST_MAGIKARP_LENGTH EQU 1067
 
 ; crash error codes
 	const_def
