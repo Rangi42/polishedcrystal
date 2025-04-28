Rangi42/polishedcrystal

commit 7ba67e3dddf2ae4690f18657762d541e6e76e4df

Use `rBGPI`/`rOBPI` hardware constants

engine/pc/bills_pc_ui.asm
@@ -3647,7 +3647,7 @@ wLCDBillsPC1::
 
 	; start of VRAM writes
 	; second box mon
-	ld a, $80 | $2a
+	ld a, (1 << rBGPI_AUTO_INCREMENT) | (0 palette 5 color 2)
 	ldh [rBGPI], a
 rept 4
 	ld a, [hli]
@@ -3655,7 +3655,7 @@ rept 4
 endr
 
 	; third box mon
-	ld a, $80 | $32
+	ld a, (1 << rBGPI_AUTO_INCREMENT) | (0 palette 6 color 2)
 	ldh [rBGPI], a
 rept 4
 	ld a, [hli]
@@ -3663,7 +3663,7 @@ rept 4
 endr
 
 	; fourth box mon
-	ld a, $80 | $3a
+	ld a, (1 << rBGPI_AUTO_INCREMENT) | (0 palette 7 color 2)
 	ldh [rBGPI], a
 rept 4
 	ld a, [hli]
@@ -3690,7 +3690,7 @@ wLCDBillsPC2::
 
 	; start of VRAM writes
 	; first party mon
-	ld a, $80 | $12
+	ld a, (1 << rBGPI_AUTO_INCREMENT) | (0 palette 2 color 2)
 	ldh [rBGPI], a
 rept 4
 	ld a, [hli]
@@ -3698,7 +3698,7 @@ rept 4
 endr
 
 	; second party mon
-	ld a, $80 | $1a
+	ld a, (1 << rBGPI_AUTO_INCREMENT) | (0 palette 3 color 2)
 	ldh [rBGPI], a
 rept 4
 	ld a, [hli]
@@ -3706,7 +3706,7 @@ rept 4
 endr
 
 	; first box mon
-	ld a, $80 | $22
+	ld a, (1 << rBGPI_AUTO_INCREMENT) | (0 palette 4 color 2)
 	ldh [rBGPI], a
 rept 4
 	ld a, [hli]
@@ -3777,8 +3777,7 @@ wLCDBillsPC3:
 .got_pal
 
 	; start of VRAM writes
-	; BG3 color 0
-	ld a, $80 | $18
+	ld a, (1 << rBGPI_AUTO_INCREMENT) | (0 palette 3 color 0)
 	ldh [rBGPI], a
 rept 2
 	ld a, [hli]

engine/pokedex/lcd.asm
@@ -876,14 +876,14 @@ PHB_LoadRow:
 	; Pal col 1 (BG2).
 	pop hl
 	inc hl
-	ld a, $80 | $12
+	ld a, (1 << rBGPI_AUTO_INCREMENT) | (0 palette 2 color 2)
 	ld c, LOW(rBGPD)
 	ldh [rBGPI], a
 rept 6
 	ld a, [hli]
 	ldh [c], a
 endr
-	ld a, $80 | $2a
+	ld a, (1 << rOBPI_AUTO_INCREMENT) | (0 palette 5 color 2)
 	ldh [rOBPI], a
 	pop de
 	push de
@@ -940,7 +940,7 @@ endr
 	ldh [c], a
 
 	; Prepare this for later.
-	ld a, $80 | $1a
+	ld a, (1 << rBGPI_AUTO_INCREMENT) | (0 palette 3 color 2)
 	ldh [rBGPI], a
 	ld a, e
 	ret

engine/battle/anim_hp_bar.asm
@@ -256,13 +256,13 @@ HPBarAnim_BGMapUpdate:
 	jmp DelayFrame
 
 .enemy_hp_bar
-	lb bc, $80 | palette PAL_BATTLE_BG_ENEMY_HP color 2, 0
+	lb bc, (1 << rBGPI_AUTO_INCREMENT) | (0 palette PAL_BATTLE_BG_ENEMY_HP color 2), 0
 	ld hl, wBGPals2 palette PAL_BATTLE_BG_ENEMY_HP color 2
 	jr .finish
 
 .player_hp_bar
-	lb bc, $80 | palette PAL_BATTLE_BG_PLAYER_HP color 2, 1
-	ld hl, wBGPals2 palette PAL_BATTLE_BG_PLAYER_HP palettes color 2
+	lb bc, (1 << rBGPI_AUTO_INCREMENT) | (0 palette PAL_BATTLE_BG_PLAYER_HP color 2), 1
+	ld hl, wBGPals2 palette PAL_BATTLE_BG_PLAYER_HP color 2
 .finish
 	xor a
 	ldh [hCGBPalUpdate], a

engine/gfx/color.asm
@@ -833,7 +833,7 @@ InitCGBPals::
 	rst ByteFill
 	xor a
 	ldh [rVBK], a
-	ld a, $80
+	ld a, 1 << rBGPI_AUTO_INCREMENT
 	ldh [rBGPI], a
 	ld c, 4 * 8
 if !DEF(MONOCHROME)
@@ -851,7 +851,7 @@ else
 endc
 	dec c
 	jr nz, .bgpals_loop
-	ld a, $80
+	ld a, 1 << rOBPI_AUTO_INCREMENT
 	ldh [rOBPI], a
 	ld c, 4 * 8
 if !DEF(MONOCHROME)

engine/pokemon/summary_screen.asm
@@ -845,7 +845,7 @@ SummaryScreen_SwitchPage:
 	ld c, 6 * 2
 	ld hl, wSummaryScreenPals + 2 palettes
 	call .WaitUntilHBlank
-	ld a, (2 palettes) | (1 << rBGPI_AUTO_INCREMENT)
+	ld a, (1 << rBGPI_AUTO_INCREMENT) | (0 palette 2)
 	ldh [rBGPI], a
 	call .CopyPalettes
 

engine/tilesets/tileset_anims.asm
@@ -1197,7 +1197,7 @@ FlickeringCaveEntrancePalette:
 	ld a, BANK(wBGPals1)
 	ldh [rSVBK], a
 
-	ld a, %10100000 ; auto-increment, index $20 (pal 4 color 0)
+	ld a, (1 << rBGPI_AUTO_INCREMENT) | (0 palette 4)
 	ldh [rBGPI], a
 
 	ldh a, [hVBlankCounter]

home/palettes.asm
@@ -26,7 +26,7 @@ ForceUpdateCGBPals::
 	ld hl, wBGPals2
 
 ; copy 8 pals to bgpd
-	ld a, %10000000 ; auto increment, index 0
+	ld a, 1 << rBGPI_AUTO_INCREMENT
 	ldh [rBGPI], a
 	lb bc, 4, LOW(rBGPD) ; 4 = NUM_PALS / 2
 .bgp

commit 3bd3d42cc78fb1813599328c6e7f7b5db67c9284

Fix pals broken in 7ba67e3dddf2ae

engine/overworld/wildmons.asm
@@ -517,7 +517,7 @@ ApplyAbilityEffectsOnEncounterMon:
 	rrca
 	ret c
 	ld a, c
-	cp 100
+	cp MAX_LEVEL
 	ret nc
 	inc c
 	ret

engine/pc/bills_pc_ui.asm
@@ -3647,7 +3647,7 @@ wLCDBillsPC1::
 
 	; start of VRAM writes
 	; second box mon
-	ld a, (1 << rBGPI_AUTO_INCREMENT) | (0 palette 5 color 2)
+	ld a, (1 << rBGPI_AUTO_INCREMENT) | (0 palette 5 color 1)
 	ldh [rBGPI], a
 rept 4
 	ld a, [hli]
@@ -3655,7 +3655,7 @@ rept 4
 endr
 
 	; third box mon
-	ld a, (1 << rBGPI_AUTO_INCREMENT) | (0 palette 6 color 2)
+	ld a, (1 << rBGPI_AUTO_INCREMENT) | (0 palette 6 color 1)
 	ldh [rBGPI], a
 rept 4
 	ld a, [hli]
@@ -3663,7 +3663,7 @@ rept 4
 endr
 
 	; fourth box mon
-	ld a, (1 << rBGPI_AUTO_INCREMENT) | (0 palette 7 color 2)
+	ld a, (1 << rBGPI_AUTO_INCREMENT) | (0 palette 7 color 1)
 	ldh [rBGPI], a
 rept 4
 	ld a, [hli]
@@ -3690,7 +3690,7 @@ wLCDBillsPC2::
 
 	; start of VRAM writes
 	; first party mon
-	ld a, (1 << rBGPI_AUTO_INCREMENT) | (0 palette 2 color 2)
+	ld a, (1 << rBGPI_AUTO_INCREMENT) | (0 palette 2 color 1)
 	ldh [rBGPI], a
 rept 4
 	ld a, [hli]
@@ -3698,7 +3698,7 @@ rept 4
 endr
 
 	; second party mon
-	ld a, (1 << rBGPI_AUTO_INCREMENT) | (0 palette 3 color 2)
+	ld a, (1 << rBGPI_AUTO_INCREMENT) | (0 palette 3 color 1)
 	ldh [rBGPI], a
 rept 4
 	ld a, [hli]
@@ -3706,7 +3706,7 @@ rept 4
 endr
 
 	; first box mon
-	ld a, (1 << rBGPI_AUTO_INCREMENT) | (0 palette 4 color 2)
+	ld a, (1 << rBGPI_AUTO_INCREMENT) | (0 palette 4 color 1)
 	ldh [rBGPI], a
 rept 4
 	ld a, [hli]

engine/pokedex/lcd.asm
@@ -876,14 +876,14 @@ PHB_LoadRow:
 	; Pal col 1 (BG2).
 	pop hl
 	inc hl
-	ld a, (1 << rBGPI_AUTO_INCREMENT) | (0 palette 2 color 2)
+	ld a, (1 << rBGPI_AUTO_INCREMENT) | (0 palette 2 color 1)
 	ld c, LOW(rBGPD)
 	ldh [rBGPI], a
 rept 6
 	ld a, [hli]
 	ldh [c], a
 endr
-	ld a, (1 << rOBPI_AUTO_INCREMENT) | (0 palette 5 color 2)
+	ld a, (1 << rOBPI_AUTO_INCREMENT) | (0 palette 5 color 1)
 	ldh [rOBPI], a
 	pop de
 	push de
@@ -940,7 +940,7 @@ endr
 	ldh [c], a
 
 	; Prepare this for later.
-	ld a, (1 << rBGPI_AUTO_INCREMENT) | (0 palette 3 color 2)
+	ld a, (1 << rBGPI_AUTO_INCREMENT) | (0 palette 3 color 1)
 	ldh [rBGPI], a
 	ld a, e
 	ret