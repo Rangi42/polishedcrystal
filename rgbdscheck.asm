ec4f567b7d8c1be47a7f3a1ac07a3385ca0d8e10
Rangi <35663410+Rangi42@users.noreply.github.com>
2025-05-11 16:28:54 -0400

    Use features new to RGBDS 0.9.2 (#1186)

--- rgbdscheck.asm
+++ rgbdscheck.asm
@@ -1,3 +1,3 @@
-if __RGBDS_MAJOR__ == 0 && __RGBDS_MINOR__ < 8
-	fail "polishedcrystal requires rgbds v0.8.0 or newer."
+if __RGBDS_MAJOR__ == 0 && (__RGBDS_MINOR__ < 9 || (__RGBDS_MINOR__ == 9 && __RGBDS_PATCH__ < 2))
+	fail "polishedcrystal requires rgbds v0.9.2 or newer."
 endc
