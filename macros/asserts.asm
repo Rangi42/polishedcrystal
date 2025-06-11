2b43e746dbfc8ff2bd94eef05d7ca62e03014e34
Rangi42 <sylvie.oukaour+rangi42@gmail.com>
2025-06-11 15:49:48 -0400

    Simplify `li` macro

--- macros/asserts.asm
+++ macros/asserts.asm
@@ -62,7 +62,7 @@ MACRO list_start
 ENDM
 
 MACRO li
-	assert STRFIND(\1, "@") == -1, STRCAT("String terminator \"@\" in list entry: ", \1)
+	assert STRFIND(\1, "@") == -1, "String terminator \"@\" in list entry: \1"
 	db \1, "@"
 	redef list_index += 1
 ENDM
