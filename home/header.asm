74b0ccdb51d1648f93b51b2c21908cbaecd01f8a
Rangi42 <sylvie.oukaour+rangi42@gmail.com>
2025-06-30 08:49:29 -0400

    Use `rgbfix -L` for Analogue Pocket logo

--- home/header.asm
+++ home/header.asm
@@ -166,14 +166,6 @@ Start::
 	nop ; no-optimize nops
 	jr _Start
 
-if DEF(ANALOGUE_POCKET)
-	; Use specialized logo for Analogue Pocket compatibility.
-	ds $0104 - @, $00
-	db $01, $10, $ce, $ef, $00, $00, $44, $aa, $00, $74, $00, $18, $11, $95, $00, $34
-	db $00, $1a, $00, $d5, $00, $22, $00, $69, $6f, $f6, $f7, $73, $09, $90, $e1, $10
-	db $44, $40, $9a, $90, $d5, $d0, $44, $30, $a9, $21, $5d, $48, $22, $e0, $f8, $60
-endc
-
 	; The rest of the header is handled by rgbfix.
 	ds $014e - @, $00
 
