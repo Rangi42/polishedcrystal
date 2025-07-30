0d237d3061253e5cbdf47a9b16943c101cd15dd5
Rangi42 <sylvie.oukaour+rangi42@gmail.com>
2025-07-30 15:40:11 -0400

    Fix "#MON" to "#mon"

--- maps/CherrygroveBay.asm
+++ maps/CherrygroveBay.asm
@@ -217,7 +217,7 @@ GenericTrainerSwimmerfTara:
 	cont "Cherrygrove."
 
 	para "Some say rare bird"
-	line "#MON come from"
+	line "#mon come from"
 
 	para "far away to nest"
 	line "there."
@@ -253,7 +253,7 @@ GenericTrainerHikerTony:
 
 	para "just to see the"
 	line "legendary bird"
-	cont "#MON!"
+	cont "#mon!"
 	done
 
 .BeatenText:
