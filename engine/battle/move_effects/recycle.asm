b4e21c03d4df938ec04ab42f6ca92563816e258a
aubs <candelaubrey@gmail.com>
2025-10-14 17:34:28 -0700

    Fixed a Recycle bug which caused stack corruptuion when used by a Pokemon that a) had no item and b) had used no item. This was caused by pushing hl to the stack, and then returning before popping it.

--- engine/battle/move_effects/recycle.asm
+++ engine/battle/move_effects/recycle.asm
@@ -1,25 +1,24 @@
 BattleCommand_recycle:
+	push de
 	; Don't do anything if we have an item already
 	farcall GetUserItem
 	ld a, [hl]
 	and a
 	jr nz, .failed
-	push hl
+	ld d, h
+	ld e, l
 
 	call GetUsedItemAddr
 	ld a, [hl]
 	and a
 	jr z, .failed
+	; Pick up the item
+	ld [de], a
+	ld [wNamedObjectIndex], a
 	ld b, a
 	xor a
 	ld [hl], a
 
-	; Pick up the item
-	pop hl
-	ld [hl], b
-
-	ld a, b
-	ld [wNamedObjectIndex], a
 	push bc
 	call GetItemName
 	ld hl, RecycledItemText
@@ -39,4 +38,5 @@ BattleCommand_recycle:
 	ret
 
 .failed
+	pop de
 	farjp ButItFailed
