6b114d813fee4a7561dedc7c9cbcc68cf7af6cfb
Rangi <sylvie.oukaour+rangi42@gmail.com>
2026-01-30 15:50:30 -0500

    Change player sprite while running

--- constants/sprite_constants.asm
+++ constants/sprite_constants.asm
@@ -204,6 +204,9 @@
 	const SPRITE_CRYS_BIKE ; c8
 	const SPRITE_CRYS_SURF ; c9
 	const SPRITE_THORTON ; ca
+	const SPRITE_CHRIS_RUN ; cb
+	const SPRITE_KRIS_RUN ; cc
+	const SPRITE_CRYS_RUN ; cd
 DEF NUM_OVERWORLD_SPRITES EQU const_value - 1
 
 ; special GetMonSprite values (see engine/overworld/overworld.asm)
