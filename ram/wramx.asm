cd72b66ff3ee01396d8797dbd0926c5eea70880b
Rangi <sylvie.oukaour+rangi42@gmail.com>
2026-03-09 00:32:31 -0400

    Remove a single-piece `UNION`

--- ram/wramx.asm
+++ ram/wramx.asm
@@ -282,7 +282,6 @@ wBattleKeyItemsPocketScrollPosition:: db
 
 wTMHMMoveNameBackup:: ds MOVE_NAME_LENGTH
 
-UNION
 wStringBuffer1:: ds STRING_BUFFER_LENGTH + 5
 wStringBuffer2:: ds STRING_BUFFER_LENGTH
 wStringBuffer3:: ds STRING_BUFFER_LENGTH
@@ -317,7 +316,6 @@ wTempPocketCursor:: ds NUM_POCKETS
 NEXTU
 wCandyMaxLevelExp:: ds 3
 wCandyPrevLevel:: db
-ENDU
 ENDU
 
 wBattleMenuCursorBuffer:: dw
