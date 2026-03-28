e57a98e1e931dc292b6817fb3e93efca318b546c
Rangi <sylvie.oukaour+rangi42@gmail.com>
2026-03-28 14:49:11 -0400

    Add shading to Daisy's hair, and color her green like FRLG + HGSS

--- data/sprites/sprites.asm
+++ data/sprites/sprites.asm
@@ -51,7 +51,7 @@ SpriteHeaders:
 	overworld_sprite EusineSpriteGFX, WALKING_SPRITE, PAL_OW_PURPLE
 	overworld_sprite KurtSpriteGFX, WALKING_SPRITE, PAL_OW_BROWN
 	overworld_sprite RedsMomSpriteGFX, WALKING_SPRITE, PAL_OW_RED
-	overworld_sprite DaisySpriteGFX, WALKING_SPRITE, PAL_OW_BLUE
+	overworld_sprite DaisySpriteGFX, WALKING_SPRITE, PAL_OW_GREEN
 	overworld_sprite LoreleiSpriteGFX, WALKING_SPRITE, PAL_OW_RED
 	overworld_sprite AgathaSpriteGFX, WALKING_SPRITE, PAL_OW_BROWN
 	overworld_sprite PalmerSpriteGFX, WALKING_SPRITE, PAL_OW_GREEN
