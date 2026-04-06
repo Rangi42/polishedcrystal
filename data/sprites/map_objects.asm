f9fe4298de5b04abc2993c12e25c6830c634d002
Rangi <sylvie.oukaour+rangi42@gmail.com>
2026-04-06 14:59:56 -0400

    Use flag constants for map objects' `SpriteMovementData`

--- data/sprites/map_objects.asm
+++ data/sprites/map_objects.asm
@@ -1,61 +1,413 @@
-MACRO sprite_movement_data
-	db \1, \2, \3, \4, \5
-	dn \6, 0
-ENDM
-
 SpriteMovementData::
 ; entries correspond to SPRITEMOVEDATA_* constants
 	table_width NUM_SPRITEMOVEDATA_FIELDS
-	; function,                                              facing, action,                         flags1, flags2, palette flags
-	sprite_movement_data SPRITEMOVEFN_00,                    DOWN,   OBJECT_ACTION_STAND,            $02,    $00,    %0000 ; SPRITEMOVEDATA_00
-	sprite_movement_data SPRITEMOVEFN_STANDING,              DOWN,   OBJECT_ACTION_STAND,            $0c,    $00,    %0000 ; SPRITEMOVEDATA_STILL
-	sprite_movement_data SPRITEMOVEFN_RANDOM_WALK_XY,        DOWN,   OBJECT_ACTION_STAND,            $00,    $00,    %0000 ; SPRITEMOVEDATA_WANDER
-	sprite_movement_data SPRITEMOVEFN_SLOW_RANDOM_SPIN,      DOWN,   OBJECT_ACTION_STAND,            $00,    $00,    %0000 ; SPRITEMOVEDATA_SPINRANDOM_SLOW
-	sprite_movement_data SPRITEMOVEFN_RANDOM_WALK_Y,         DOWN,   OBJECT_ACTION_STAND,            $00,    $00,    %0000 ; SPRITEMOVEDATA_WALK_UP_DOWN
-	sprite_movement_data SPRITEMOVEFN_RANDOM_WALK_X,         DOWN,   OBJECT_ACTION_STAND,            $00,    $00,    %0000 ; SPRITEMOVEDATA_WALK_LEFT_RIGHT
-	sprite_movement_data SPRITEMOVEFN_STANDING,              DOWN,   OBJECT_ACTION_STAND,            $00,    $00,    %0000 ; SPRITEMOVEDATA_STANDING_DOWN
-	sprite_movement_data SPRITEMOVEFN_STANDING,              UP,     OBJECT_ACTION_STAND,            $00,    $00,    %0000 ; SPRITEMOVEDATA_STANDING_UP
-	sprite_movement_data SPRITEMOVEFN_STANDING,              LEFT,   OBJECT_ACTION_STAND,            $00,    $00,    %0000 ; SPRITEMOVEDATA_STANDING_LEFT
-	sprite_movement_data SPRITEMOVEFN_STANDING,              RIGHT,  OBJECT_ACTION_STAND,            $00,    $00,    %0000 ; SPRITEMOVEDATA_STANDING_RIGHT
-	sprite_movement_data SPRITEMOVEFN_FAST_RANDOM_SPIN,      DOWN,   OBJECT_ACTION_STAND,            $00,    $00,    %0000 ; SPRITEMOVEDATA_SPINRANDOM_FAST
-	sprite_movement_data SPRITEMOVEFN_OBEY_DPAD,             DOWN,   OBJECT_ACTION_STAND,            $02,    $00,    %0000 ; SPRITEMOVEDATA_PLAYER
-	sprite_movement_data SPRITEMOVEFN_CUT_TREE,              DOWN,   OBJECT_ACTION_CUT_TREE,         $0c,    LOW_PRIORITY,    %0000 ; SPRITEMOVEDATA_CUTTABLE_TREE
-	sprite_movement_data SPRITEMOVEFN_FOLLOW,                DOWN,   OBJECT_ACTION_STAND,            $02,    $00,    %0000 ; SPRITEMOVEDATA_FOLLOWING
-	sprite_movement_data SPRITEMOVEFN_SCRIPTED,              DOWN,   OBJECT_ACTION_STAND,            $02,    $00,    %0000 ; SPRITEMOVEDATA_SCRIPTED
-	sprite_movement_data SPRITEMOVEFN_BIG_SNORLAX,           DOWN,   OBJECT_ACTION_BIG_SNORLAX,      $2e,    $01,    %1100 ; SPRITEMOVEDATA_SNORLAX
-	sprite_movement_data SPRITEMOVEFN_BOUNCE,                DOWN,   OBJECT_ACTION_BOUNCE,           $2e,    $00,    %0000 ; SPRITEMOVEDATA_POKEMON
-	sprite_movement_data SPRITEMOVEFN_STANDING,              DOWN,   OBJECT_ACTION_STAND,            $0c,    $00,    %0000 ; SPRITEMOVEDATA_SUDOWOODO
-	sprite_movement_data SPRITEMOVEFN_STANDING,              UP,     OBJECT_ACTION_STAND,            $2e,    $10,    %0000 ; SPRITEMOVEDATA_SMASHABLE_ROCK
-	sprite_movement_data SPRITEMOVEFN_STRENGTH,              DOWN,   OBJECT_ACTION_STAND,            $2e,    $00,    %0100 ; SPRITEMOVEDATA_STRENGTH_BOULDER
-	sprite_movement_data SPRITEMOVEFN_FOLLOWNOTEXACT,        DOWN,   OBJECT_ACTION_STAND,            $02,    $00,    %0000 ; SPRITEMOVEDATA_FOLLOWNOTEXACT
-	sprite_movement_data SPRITEMOVEFN_SHADOW,                DOWN,   OBJECT_ACTION_00,               $8e,    $01,    %0000 ; SPRITEMOVEDATA_SHADOW
-	sprite_movement_data SPRITEMOVEFN_EMOTE,                 DOWN,   OBJECT_ACTION_EMOTE,            $8e,    $02,    %0000 ; SPRITEMOVEDATA_EMOTE
-	sprite_movement_data SPRITEMOVEFN_SCREENSHAKE,           DOWN,   OBJECT_ACTION_00,               $82,    $00,    %0000 ; SPRITEMOVEDATA_SCREENSHAKE
-	sprite_movement_data SPRITEMOVEFN_SPIN_COUNTERCLOCKWISE, LEFT,   OBJECT_ACTION_STAND,            $00,    $00,    %0000 ; SPRITEMOVEDATA_SPINCOUNTERCLOCKWISE
-	sprite_movement_data SPRITEMOVEFN_SPIN_CLOCKWISE,        RIGHT,  OBJECT_ACTION_STAND,            $00,    $00,    %0000 ; SPRITEMOVEDATA_SPINCLOCKWISE
-	sprite_movement_data SPRITEMOVEFN_STRENGTH,              DOWN,   OBJECT_ACTION_BIG_DOLL,         $2e,    $01,    %1100 ; SPRITEMOVEDATA_BIGDOLL
-	sprite_movement_data SPRITEMOVEFN_BOULDERDUST,           DOWN,   OBJECT_ACTION_BOULDER_DUST,     $8e,    $01,    %0000 ; SPRITEMOVEDATA_BOULDERDUST
-	sprite_movement_data SPRITEMOVEFN_GRASS,                 DOWN,   OBJECT_ACTION_GRASS_SHAKE,      $8e,    $02,    %0000 ; SPRITEMOVEDATA_GRASS
-	sprite_movement_data SPRITEMOVEFN_PUDDLE,                DOWN,   OBJECT_ACTION_PUDDLE_SPLASH,    $8e,    $02,    %0000 ; SPRITEMOVEDATA_PUDDLE
-	sprite_movement_data SPRITEMOVEFN_RANDOM_WALK_XY,        DOWN,   OBJECT_ACTION_STAND,            $00,    $00,    %0010 ; SPRITEMOVEDATA_SWIM_AROUND
-	sprite_movement_data SPRITEMOVEFN_RANDOM_WALK_Y,         DOWN,   OBJECT_ACTION_STAND,            $00,    $00,    %0010 ; SPRITEMOVEDATA_SWIM_UP_DOWN
-	sprite_movement_data SPRITEMOVEFN_RANDOM_WALK_X,         DOWN,   OBJECT_ACTION_STAND,            $00,    $00,    %0010 ; SPRITEMOVEDATA_SWIM_LEFT_RIGHT
-	sprite_movement_data SPRITEMOVEFN_FRUIT,                 DOWN,   OBJECT_ACTION_FRUIT,            $00,    $00,    %0000 ; SPRITEMOVEDATA_FRUIT
-	sprite_movement_data SPRITEMOVEFN_BIG_GYARADOS,          DOWN,   OBJECT_ACTION_BIG_GYARADOS,     $2e,    $01,    %1100 ; SPRITEMOVEDATA_BIG_GYARADOS
-	sprite_movement_data SPRITEMOVEFN_STANDING_FLIP,         DOWN,   OBJECT_ACTION_STAND_FLIP,       $00,    $00,    %0000 ; SPRITEMOVEDATA_STANDING_DOWN_FLIP
-	sprite_movement_data SPRITEMOVEFN_STANDING_FLIP,         UP,     OBJECT_ACTION_STAND_FLIP,       $00,    $00,    %0000 ; SPRITEMOVEDATA_STANDING_UP_FLIP
-	sprite_movement_data SPRITEMOVEFN_POKECOM_NEWS,          DOWN,   OBJECT_ACTION_POKECOM_NEWS,     $8e,    $02,    %0000 ; SPRITEMOVEDATA_POKECOM_NEWS
-	sprite_movement_data SPRITEMOVEFN_MUSEUM_DRILL,          DOWN,   OBJECT_ACTION_MUSEUM_DRILL,     $8e,    $02,    %0000 ; SPRITEMOVEDATA_MUSEUM_DRILL_DOWN
-	sprite_movement_data SPRITEMOVEFN_MUSEUM_DRILL,          UP,     OBJECT_ACTION_MUSEUM_DRILL,     $8e,    $02,    %0000 ; SPRITEMOVEDATA_MUSEUM_DRILL_UP
-	sprite_movement_data SPRITEMOVEFN_MUSEUM_DRILL,          LEFT,   OBJECT_ACTION_MUSEUM_DRILL,     $8e,    $02,    %0000 ; SPRITEMOVEDATA_ARCH_TREE_LEFT
-	sprite_movement_data SPRITEMOVEFN_MUSEUM_DRILL,          RIGHT,  OBJECT_ACTION_MUSEUM_DRILL,     $8e,    $02,    %0000 ; SPRITEMOVEDATA_ARCH_TREE_RIGHT
-	sprite_movement_data SPRITEMOVEFN_SAILBOAT_TOP,          DOWN,   OBJECT_ACTION_SAILBOAT_TOP,     $2e,    $02,    %1100 ; SPRITEMOVEDATA_SAILBOAT_TOP
-	sprite_movement_data SPRITEMOVEFN_SAILBOAT_BOTTOM,       DOWN,   OBJECT_ACTION_SAILBOAT_BOTTOM,  $2e,    $01,    %1100 ; SPRITEMOVEDATA_SAILBOAT_BOTTOM
-	sprite_movement_data SPRITEMOVEFN_ALOLAN_EXEGGUTOR,      DOWN,   OBJECT_ACTION_ALOLAN_EXEGGUTOR, $0c,    $00,    %0000 ; SPRITEMOVEDATA_ALOLAN_EXEGGUTOR
-	sprite_movement_data SPRITEMOVEFN_TINY_WINDOWS,          DOWN,   OBJECT_ACTION_TINY_WINDOWS,     $8e,    $02,    %0000 ; SPRITEMOVEDATA_TINY_WINDOWS
-	sprite_movement_data SPRITEMOVEFN_STANDING,              UP,     OBJECT_ACTION_STAND,            $8e,    $02,    %0000 ; SPRITEMOVEDATA_PLACEHOLDER_UP
-	sprite_movement_data SPRITEMOVEFN_MICROPHONE,            LEFT,   OBJECT_ACTION_MICROPHONE,       $0c,    LOW_PRIORITY,    %0000 ; SPRITEMOVEDATA_MICROPHONE
-	sprite_movement_data SPRITEMOVEFN_BIG_HO_OH,             DOWN,   OBJECT_ACTION_BIG_HO_OH,        $2e,    $01,    %1100 ; SPRITEMOVEDATA_BIG_HO_OH
-	sprite_movement_data SPRITEMOVEFN_BIG_LUGIA,             DOWN,   OBJECT_ACTION_BIG_LUGIA,        $2e,    $01,    %1100 ; SPRITEMOVEDATA_BIG_LUGIA
-	sprite_movement_data SPRITEMOVEFN_ADMIN_MEOWTH,          DOWN,   OBJECT_ACTION_ADMIN_MEOWTH,     $2e,    $00,    %0000 ; SPRITEMOVEDATA_ADMIN_MEOWTH
+
+; SPRITEMOVEDATA_00
+	db SPRITEMOVEFN_00 ; movement function
+	db DOWN ; facing
+	db OBJECT_ACTION_STAND ; action
+	db WONT_DELETE ; flags1
+	db 0 ; flags2
+	db 0 ; palette flags
+
+; SPRITEMOVEDATA_STILL
+	db SPRITEMOVEFN_STANDING ; movement function
+	db DOWN ; facing
+	db OBJECT_ACTION_STAND ; action
+	db FIXED_FACING | SLIDING ; flags1
+	db 0 ; flags2
+	db 0 ; palette flags
+
+; SPRITEMOVEDATA_WANDER
+	db SPRITEMOVEFN_RANDOM_WALK_XY ; movement function
+	db DOWN ; facing
+	db OBJECT_ACTION_STAND ; action
+	db 0 ; flags1
+	db 0 ; flags2
+	db 0 ; palette flags
+
+; SPRITEMOVEDATA_SPINRANDOM_SLOW
+	db SPRITEMOVEFN_SLOW_RANDOM_SPIN ; movement function
+	db DOWN ; facing
+	db OBJECT_ACTION_STAND ; action
+	db 0 ; flags1
+	db 0 ; flags2
+	db 0 ; palette flags
+
+; SPRITEMOVEDATA_WALK_UP_DOWN
+	db SPRITEMOVEFN_RANDOM_WALK_Y ; movement function
+	db DOWN ; facing
+	db OBJECT_ACTION_STAND ; action
+	db 0 ; flags1
+	db 0 ; flags2
+	db 0 ; palette flags
+
+; SPRITEMOVEDATA_WALK_LEFT_RIGHT
+	db SPRITEMOVEFN_RANDOM_WALK_X ; movement function
+	db DOWN ; facing
+	db OBJECT_ACTION_STAND ; action
+	db 0 ; flags1
+	db 0 ; flags2
+	db 0 ; palette flags
+
+; SPRITEMOVEDATA_STANDING_DOWN
+	db SPRITEMOVEFN_STANDING ; movement function
+	db DOWN ; facing
+	db OBJECT_ACTION_STAND ; action
+	db 0 ; flags1
+	db 0 ; flags2
+	db 0 ; palette flags
+
+; SPRITEMOVEDATA_STANDING_UP
+	db SPRITEMOVEFN_STANDING ; movement function
+	db UP ; facing
+	db OBJECT_ACTION_STAND ; action
+	db 0 ; flags1
+	db 0 ; flags2
+	db 0 ; palette flags
+
+; SPRITEMOVEDATA_STANDING_LEFT
+	db SPRITEMOVEFN_STANDING ; movement function
+	db LEFT ; facing
+	db OBJECT_ACTION_STAND ; action
+	db 0 ; flags1
+	db 0 ; flags2
+	db 0 ; palette flags
+
+; SPRITEMOVEDATA_STANDING_RIGHT
+	db SPRITEMOVEFN_STANDING ; movement function
+	db RIGHT ; facing
+	db OBJECT_ACTION_STAND ; action
+	db 0 ; flags1
+	db 0 ; flags2
+	db 0 ; palette flags
+
+; SPRITEMOVEDATA_SPINRANDOM_FAST
+	db SPRITEMOVEFN_FAST_RANDOM_SPIN ; movement function
+	db DOWN ; facing
+	db OBJECT_ACTION_STAND ; action
+	db 0 ; flags1
+	db 0 ; flags2
+	db 0 ; palette flags
+
+; SPRITEMOVEDATA_PLAYER
+	db SPRITEMOVEFN_OBEY_DPAD ; movement function
+	db DOWN ; facing
+	db OBJECT_ACTION_STAND ; action
+	db WONT_DELETE ; flags1
+	db 0 ; flags2
+	db 0 ; palette flags
+
+; SPRITEMOVEDATA_CUTTABLE_TREE
+	db SPRITEMOVEFN_CUT_TREE ; movement function
+	db DOWN ; facing
+	db OBJECT_ACTION_CUT_TREE ; action
+	db FIXED_FACING | SLIDING ; flags1
+	db LOW_PRIORITY ; flags2
+	db 0 ; palette flags
+
+; SPRITEMOVEDATA_FOLLOWING
+	db SPRITEMOVEFN_FOLLOW ; movement function
+	db DOWN ; facing
+	db OBJECT_ACTION_STAND ; action
+	db WONT_DELETE ; flags1
+	db 0 ; flags2
+	db 0 ; palette flags
+
+; SPRITEMOVEDATA_SCRIPTED
+	db SPRITEMOVEFN_SCRIPTED ; movement function
+	db DOWN ; facing
+	db OBJECT_ACTION_STAND ; action
+	db WONT_DELETE ; flags1
+	db 0 ; flags2
+	db 0 ; palette flags
+
+; SPRITEMOVEDATA_SNORLAX
+	db SPRITEMOVEFN_BIG_SNORLAX ; movement function
+	db DOWN ; facing
+	db OBJECT_ACTION_BIG_SNORLAX ; action
+	db WONT_DELETE | FIXED_FACING | SLIDING | MOVE_ANYWHERE ; flags1
+	db LOW_PRIORITY ; flags2
+	db STRENGTH_BOULDER | BIG_OBJECT ; palette flags
+
+; SPRITEMOVEDATA_POKEMON
+	db SPRITEMOVEFN_BOUNCE ; movement function
+	db DOWN ; facing
+	db OBJECT_ACTION_BOUNCE ; action
+	db WONT_DELETE | FIXED_FACING | SLIDING | MOVE_ANYWHERE ; flags1
+	db 0 ; flags2
+	db 0 ; palette flags
+
+; SPRITEMOVEDATA_SUDOWOODO
+	db SPRITEMOVEFN_STANDING ; movement function
+	db DOWN ; facing
+	db OBJECT_ACTION_STAND ; action
+	db FIXED_FACING | SLIDING ; flags1
+	db 0 ; flags2
+	db 0 ; palette flags
+
+; SPRITEMOVEDATA_SMASHABLE_ROCK
+	db SPRITEMOVEFN_STANDING ; movement function
+	db UP ; facing
+	db OBJECT_ACTION_STAND ; action
+	db WONT_DELETE | FIXED_FACING | SLIDING | MOVE_ANYWHERE ; flags1
+	db USE_OBP1 ; flags2
+	db 0 ; palette flags
+
+; SPRITEMOVEDATA_STRENGTH_BOULDER
+	db SPRITEMOVEFN_STRENGTH ; movement function
+	db DOWN ; facing
+	db OBJECT_ACTION_STAND ; action
+	db WONT_DELETE | FIXED_FACING | SLIDING | MOVE_ANYWHERE ; flags1
+	db 0 ; flags2
+	db STRENGTH_BOULDER ; palette flags
+
+; SPRITEMOVEDATA_FOLLOWNOTEXACT
+	db SPRITEMOVEFN_FOLLOWNOTEXACT ; movement function
+	db DOWN ; facing
+	db OBJECT_ACTION_STAND ; action
+	db WONT_DELETE ; flags1
+	db 0 ; flags2
+	db 0 ; palette flags
+
+; SPRITEMOVEDATA_SHADOW
+	db SPRITEMOVEFN_SHADOW ; movement function
+	db DOWN ; facing
+	db OBJECT_ACTION_00 ; action
+	db WONT_DELETE | FIXED_FACING | SLIDING | EMOTE_OBJECT ; flags1
+	db LOW_PRIORITY ; flags2
+	db 0 ; palette flags
+
+; SPRITEMOVEDATA_EMOTE
+	db SPRITEMOVEFN_EMOTE ; movement function
+	db DOWN ; facing
+	db OBJECT_ACTION_EMOTE ; action
+	db WONT_DELETE | FIXED_FACING | SLIDING | EMOTE_OBJECT ; flags1
+	db HIGH_PRIORITY ; flags2
+	db 0 ; palette flags
+
+; SPRITEMOVEDATA_SCREENSHAKE
+	db SPRITEMOVEFN_SCREENSHAKE ; movement function
+	db DOWN ; facing
+	db OBJECT_ACTION_00 ; action
+	db WONT_DELETE | EMOTE_OBJECT ; flags1
+	db 0 ; flags2
+	db 0 ; palette flags
+
+; SPRITEMOVEDATA_SPINCOUNTERCLOCKWISE
+	db SPRITEMOVEFN_SPIN_COUNTERCLOCKWISE ; movement function
+	db LEFT ; facing
+	db OBJECT_ACTION_STAND ; action
+	db 0 ; flags1
+	db 0 ; flags2
+	db 0 ; palette flags
+
+; SPRITEMOVEDATA_SPINCLOCKWISE
+	db SPRITEMOVEFN_SPIN_CLOCKWISE ; movement function
+	db RIGHT ; facing
+	db OBJECT_ACTION_STAND ; action
+	db 0 ; flags1
+	db 0 ; flags2
+	db 0 ; palette flags
+
+; SPRITEMOVEDATA_BIGDOLL
+	db SPRITEMOVEFN_STRENGTH ; movement function
+	db DOWN ; facing
+	db OBJECT_ACTION_BIG_DOLL ; action
+	db WONT_DELETE | FIXED_FACING | SLIDING | MOVE_ANYWHERE ; flags1
+	db LOW_PRIORITY ; flags2
+	db STRENGTH_BOULDER | BIG_OBJECT ; palette flags
+
+; SPRITEMOVEDATA_BOULDERDUST
+	db SPRITEMOVEFN_BOULDERDUST ; movement function
+	db DOWN ; facing
+	db OBJECT_ACTION_BOULDER_DUST ; action
+	db WONT_DELETE | FIXED_FACING | SLIDING | EMOTE_OBJECT ; flags1
+	db LOW_PRIORITY ; flags2
+	db 0 ; palette flags
+
+; SPRITEMOVEDATA_GRASS
+	db SPRITEMOVEFN_GRASS ; movement function
+	db DOWN ; facing
+	db OBJECT_ACTION_GRASS_SHAKE ; action
+	db WONT_DELETE | FIXED_FACING | SLIDING | EMOTE_OBJECT ; flags1
+	db HIGH_PRIORITY ; flags2
+	db 0 ; palette flags
+
+; SPRITEMOVEDATA_PUDDLE
+	db SPRITEMOVEFN_PUDDLE ; movement function
+	db DOWN ; facing
+	db OBJECT_ACTION_PUDDLE_SPLASH ; action
+	db WONT_DELETE | FIXED_FACING | SLIDING | EMOTE_OBJECT ; flags1
+	db HIGH_PRIORITY ; flags2
+	db 0 ; palette flags
+
+; SPRITEMOVEDATA_SWIM_AROUND
+	db SPRITEMOVEFN_RANDOM_WALK_XY ; movement function
+	db DOWN ; facing
+	db OBJECT_ACTION_STAND ; action
+	db 0 ; flags1
+	db 0 ; flags2
+	db SWIMMING ; palette flags
+
+; SPRITEMOVEDATA_SWIM_UP_DOWN
+	db SPRITEMOVEFN_RANDOM_WALK_Y ; movement function
+	db DOWN ; facing
+	db OBJECT_ACTION_STAND ; action
+	db 0 ; flags1
+	db 0 ; flags2
+	db SWIMMING ; palette flags
+
+; SPRITEMOVEDATA_SWIM_LEFT_RIGHT
+	db SPRITEMOVEFN_RANDOM_WALK_X ; movement function
+	db DOWN ; facing
+	db OBJECT_ACTION_STAND ; action
+	db 0 ; flags1
+	db 0 ; flags2
+	db SWIMMING ; palette flags
+
+; SPRITEMOVEDATA_FRUIT
+	db SPRITEMOVEFN_FRUIT ; movement function
+	db DOWN ; facing
+	db OBJECT_ACTION_FRUIT ; action
+	db 0 ; flags1
+	db 0 ; flags2
+	db 0 ; palette flags
+
+; SPRITEMOVEDATA_BIG_GYARADOS
+	db SPRITEMOVEFN_BIG_GYARADOS ; movement function
+	db DOWN ; facing
+	db OBJECT_ACTION_BIG_GYARADOS ; action
+	db WONT_DELETE | FIXED_FACING | SLIDING | MOVE_ANYWHERE ; flags1
+	db LOW_PRIORITY ; flags2
+	db STRENGTH_BOULDER | BIG_OBJECT ; palette flags
+
+; SPRITEMOVEDATA_STANDING_DOWN_FLIP
+	db SPRITEMOVEFN_STANDING_FLIP ; movement function
+	db DOWN ; facing
+	db OBJECT_ACTION_STAND_FLIP ; action
+	db 0 ; flags1
+	db 0 ; flags2
+	db 0 ; palette flags
+
+; SPRITEMOVEDATA_STANDING_UP_FLIP
+	db SPRITEMOVEFN_STANDING_FLIP ; movement function
+	db UP ; facing
+	db OBJECT_ACTION_STAND_FLIP ; action
+	db 0 ; flags1
+	db 0 ; flags2
+	db 0 ; palette flags
+
+; SPRITEMOVEDATA_POKECOM_NEWS
+	db SPRITEMOVEFN_POKECOM_NEWS ; movement function
+	db DOWN ; facing
+	db OBJECT_ACTION_POKECOM_NEWS ; action
+	db WONT_DELETE | FIXED_FACING | SLIDING | EMOTE_OBJECT ; flags1
+	db HIGH_PRIORITY ; flags2
+	db 0 ; palette flags
+
+; SPRITEMOVEDATA_MUSEUM_DRILL_DOWN
+	db SPRITEMOVEFN_MUSEUM_DRILL ; movement function
+	db DOWN ; facing
+	db OBJECT_ACTION_MUSEUM_DRILL ; action
+	db WONT_DELETE | FIXED_FACING | SLIDING | EMOTE_OBJECT ; flags1
+	db HIGH_PRIORITY ; flags2
+	db 0 ; palette flags
+
+; SPRITEMOVEDATA_MUSEUM_DRILL_UP
+	db SPRITEMOVEFN_MUSEUM_DRILL ; movement function
+	db UP ; facing
+	db OBJECT_ACTION_MUSEUM_DRILL ; action
+	db WONT_DELETE | FIXED_FACING | SLIDING | EMOTE_OBJECT ; flags1
+	db HIGH_PRIORITY ; flags2
+	db 0 ; palette flags
+
+; SPRITEMOVEDATA_ARCH_TREE_LEFT
+	db SPRITEMOVEFN_MUSEUM_DRILL ; movement function
+	db LEFT ; facing
+	db OBJECT_ACTION_MUSEUM_DRILL ; action
+	db WONT_DELETE | FIXED_FACING | SLIDING | EMOTE_OBJECT ; flags1
+	db HIGH_PRIORITY ; flags2
+	db 0 ; palette flags
+
+; SPRITEMOVEDATA_ARCH_TREE_RIGHT
+	db SPRITEMOVEFN_MUSEUM_DRILL ; movement function
+	db RIGHT ; facing
+	db OBJECT_ACTION_MUSEUM_DRILL ; action
+	db WONT_DELETE | FIXED_FACING | SLIDING | EMOTE_OBJECT ; flags1
+	db HIGH_PRIORITY ; flags2
+	db 0 ; palette flags
+
+; SPRITEMOVEDATA_SAILBOAT_TOP
+	db SPRITEMOVEFN_SAILBOAT_TOP ; movement function
+	db DOWN ; facing
+	db OBJECT_ACTION_SAILBOAT_TOP ; action
+	db WONT_DELETE | FIXED_FACING | SLIDING | MOVE_ANYWHERE ; flags1
+	db HIGH_PRIORITY ; flags2
+	db STRENGTH_BOULDER | BIG_OBJECT ; palette flags
+
+; SPRITEMOVEDATA_SAILBOAT_BOTTOM
+	db SPRITEMOVEFN_SAILBOAT_BOTTOM ; movement function
+	db DOWN ; facing
+	db OBJECT_ACTION_SAILBOAT_BOTTOM ; action
+	db WONT_DELETE | FIXED_FACING | SLIDING | MOVE_ANYWHERE ; flags1
+	db LOW_PRIORITY ; flags2
+	db STRENGTH_BOULDER | BIG_OBJECT ; palette flags
+
+; SPRITEMOVEDATA_ALOLAN_EXEGGUTOR
+	db SPRITEMOVEFN_ALOLAN_EXEGGUTOR ; movement function
+	db DOWN ; facing
+	db OBJECT_ACTION_ALOLAN_EXEGGUTOR ; action
+	db FIXED_FACING | SLIDING ; flags1
+	db 0 ; flags2
+	db 0 ; palette flags
+
+; SPRITEMOVEDATA_TINY_WINDOWS
+	db SPRITEMOVEFN_TINY_WINDOWS ; movement function
+	db DOWN ; facing
+	db OBJECT_ACTION_TINY_WINDOWS ; action
+	db WONT_DELETE | FIXED_FACING | SLIDING | EMOTE_OBJECT ; flags1
+	db HIGH_PRIORITY ; flags2
+	db 0 ; palette flags
+
+; SPRITEMOVEDATA_PLACEHOLDER_UP
+	db SPRITEMOVEFN_STANDING ; movement function
+	db UP ; facing
+	db OBJECT_ACTION_STAND ; action
+	db WONT_DELETE | FIXED_FACING | SLIDING | EMOTE_OBJECT ; flags1
+	db HIGH_PRIORITY ; flags2
+	db 0 ; palette flags
+
+; SPRITEMOVEDATA_MICROPHONE
+	db SPRITEMOVEFN_MICROPHONE ; movement function
+	db LEFT ; facing
+	db OBJECT_ACTION_MICROPHONE ; action
+	db FIXED_FACING | SLIDING ; flags1
+	db LOW_PRIORITY ; flags2
+	db 0 ; palette flags
+
+; SPRITEMOVEDATA_BIG_HO_OH
+	db SPRITEMOVEFN_BIG_HO_OH ; movement function
+	db DOWN ; facing
+	db OBJECT_ACTION_BIG_HO_OH ; action
+	db WONT_DELETE | FIXED_FACING | SLIDING | MOVE_ANYWHERE ; flags1
+	db LOW_PRIORITY ; flags2
+	db STRENGTH_BOULDER | BIG_OBJECT ; palette flags
+
+; SPRITEMOVEDATA_BIG_LUGIA
+	db SPRITEMOVEFN_BIG_LUGIA ; movement function
+	db DOWN ; facing
+	db OBJECT_ACTION_BIG_LUGIA ; action
+	db WONT_DELETE | FIXED_FACING | SLIDING | MOVE_ANYWHERE ; flags1
+	db LOW_PRIORITY ; flags2
+	db STRENGTH_BOULDER | BIG_OBJECT ; palette flags
+
+; SPRITEMOVEDATA_ADMIN_MEOWTH
+	db SPRITEMOVEFN_ADMIN_MEOWTH ; movement function
+	db DOWN ; facing
+	db OBJECT_ACTION_ADMIN_MEOWTH ; action
+	db WONT_DELETE | FIXED_FACING | SLIDING | MOVE_ANYWHERE ; flags1
+	db 0 ; flags2
+	db 0 ; palette flags
+
 	assert_table_length NUM_SPRITEMOVEDATA
