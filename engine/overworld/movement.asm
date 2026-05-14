425539cfcccd4d0bc3f0dab3407620bb402d319a
Rangi <sylvie.oukaour+rangi42@gmail.com>
2026-05-14 15:42:59 -0400

    Parameterize more movement functions

--- engine/overworld/movement.asm
+++ engine/overworld/movement.asm
@@ -1,5 +1,9 @@
 MACRO movement
+if _NARG == 2
 	db \2
+else
+	db 0
+endc
 	dw \1
 ENDM
 
@@ -24,110 +28,110 @@ DoMovementFunction:
 .MovementPointers:
 ; entries correspond to movement_* constants (see macros/scripts/movement.asm)
 	table_width 3
-	movement TurnHead,           OW_DOWN                 ; 00
-	movement TurnHead,           OW_UP                   ; 01
-	movement TurnHead,           OW_LEFT                 ; 02
-	movement TurnHead,           OW_RIGHT                ; 03
-	movement TurnStep,           OW_DOWN                 ; 04
-	movement TurnStep,           OW_UP                   ; 05
-	movement TurnStep,           OW_LEFT                 ; 06
-	movement TurnStep,           OW_RIGHT                ; 07
-	movement NormalStep,         STEP_SLOW << 2 | DOWN   ; 08
-	movement NormalStep,         STEP_SLOW << 2 | UP     ; 09
-	movement NormalStep,         STEP_SLOW << 2 | LEFT   ; 0a
-	movement NormalStep,         STEP_SLOW << 2 | RIGHT  ; 0b
-	movement NormalStep,         STEP_WALK << 2 | DOWN   ; 0c
-	movement NormalStep,         STEP_WALK << 2 | UP     ; 0d
-	movement NormalStep,         STEP_WALK << 2 | LEFT   ; 0e
-	movement NormalStep,         STEP_WALK << 2 | RIGHT  ; 0f
-	movement NormalStep,         STEP_BIKE << 2 | DOWN   ; 10
-	movement NormalStep,         STEP_BIKE << 2 | UP     ; 11
-	movement NormalStep,         STEP_BIKE << 2 | LEFT   ; 12
-	movement NormalStep,         STEP_BIKE << 2 | RIGHT  ; 13
-	movement SlideStep,          STEP_SLOW << 2 | DOWN   ; 14
-	movement SlideStep,          STEP_SLOW << 2 | UP     ; 15
-	movement SlideStep,          STEP_SLOW << 2 | LEFT   ; 16
-	movement SlideStep,          STEP_SLOW << 2 | RIGHT  ; 17
-	movement SlideStep,          STEP_WALK << 2 | DOWN   ; 18
-	movement SlideStep,          STEP_WALK << 2 | UP     ; 19
-	movement SlideStep,          STEP_WALK << 2 | LEFT   ; 1a
-	movement SlideStep,          STEP_WALK << 2 | RIGHT  ; 1b
-	movement SlideStep,          STEP_RUN << 2 | DOWN    ; 1c
-	movement SlideStep,          STEP_RUN << 2 | UP      ; 1d
-	movement SlideStep,          STEP_RUN << 2 | LEFT    ; 1e
-	movement SlideStep,          STEP_RUN << 2 | RIGHT   ; 1f
-	movement TurningStep,        STEP_SLOW << 2 | DOWN   ; 20
-	movement TurningStep,        STEP_SLOW << 2 | UP     ; 21
-	movement TurningStep,        STEP_SLOW << 2 | LEFT   ; 22
-	movement TurningStep,        STEP_SLOW << 2 | RIGHT  ; 23
-	movement TurningStep,        STEP_WALK << 2 | DOWN   ; 24
-	movement TurningStep,        STEP_WALK << 2 | UP     ; 25
-	movement TurningStep,        STEP_WALK << 2 | LEFT   ; 26
-	movement TurningStep,        STEP_WALK << 2 | RIGHT  ; 27
-	movement TurningStep,        STEP_WALK << 2 | DOWN   ; 28
-	movement TurningStep,        STEP_WALK << 2 | UP     ; 29
-	movement TurningStep,        STEP_WALK << 2 | LEFT   ; 2a
-	movement TurningStep,        STEP_WALK << 2 | RIGHT  ; 2b
-	movement JumpStep,           STEP_SLOW << 2 | DOWN   ; 2c
-	movement JumpStep,           STEP_SLOW << 2 | UP     ; 2d
-	movement JumpStep,           STEP_SLOW << 2 | LEFT   ; 2e
-	movement JumpStep,           STEP_SLOW << 2 | RIGHT  ; 2f
-	movement JumpStep,           STEP_WALK << 2 | DOWN   ; 30
-	movement JumpStep,           STEP_WALK << 2 | UP     ; 31
-	movement JumpStep,           STEP_WALK << 2 | LEFT   ; 32
-	movement JumpStep,           STEP_WALK << 2 | RIGHT  ; 33
-	movement JumpStep,           STEP_BIKE << 2 | DOWN   ; 34
-	movement JumpStep,           STEP_BIKE << 2 | UP     ; 35
-	movement JumpStep,           STEP_BIKE << 2 | LEFT   ; 36
-	movement JumpStep,           STEP_BIKE << 2 | RIGHT  ; 37
-	movement RemoveSliding,      0                       ; 38
-	movement SetSliding,         0                       ; 39
-	movement RemoveFixedFacing,  0                       ; 3a
-	movement FixFacing,          0                       ; 3b
-	movement ShowObject,         0                       ; 3c
-	movement HideObject,         0                       ; 3d
-	movement SleepStep,          1                       ; 3e
-	movement SleepStep,          2                       ; 3f
-	movement SleepStep,          3                       ; 40
-	movement SleepStep,          4                       ; 41
-	movement SleepStep,          5                       ; 42
-	movement SleepStep,          6                       ; 43
-	movement SleepStep,          7                       ; 44
-	movement SleepStep,          8                       ; 45
-	movement LongSleepStep,      0                       ; 46
-	movement EndStep,            0                       ; 47
-	movement ResumeStep,         0                       ; 48
-	movement RemoveObject,       0                       ; 49
-	movement LoopStep,           0                       ; 4a
-	movement Step4B,             0                       ; 4b
-	movement SetStepType,        STEP_TYPE_TELEPORT_FROM ; 4c
-	movement SetStepType,        STEP_TYPE_TELEPORT_TO   ; 4d
-	movement SetStepType,        STEP_TYPE_SKYFALL       ; 4e
-	movement DigStep,            0                       ; 4f
-	movement BumpStep,           1                       ; 50
-	movement FishingStep,        STEP_TYPE_GOT_BITE      ; 51
-	movement FishingStep,        STEP_TYPE_FROM_MOVEMENT ; 52
-	movement HideEmote,          0                       ; 53
-	movement ShowEmote,          0                       ; 54
-	movement ShakeScreenStep,    0                       ; 55
-	movement ShakeTree,          24                      ; 56
-	movement SmashRock,          0                       ; 57
-	movement ReturnDig,          0                       ; 58
-	movement SetStepType,        STEP_TYPE_SKYFALL_TOP   ; 59
-	movement RunStep,            STEP_RUN << 2 | DOWN    ; 5a
-	movement RunStep,            STEP_RUN << 2 | UP      ; 5b
-	movement RunStep,            STEP_RUN << 2 | LEFT    ; 5c
-	movement RunStep,            STEP_RUN << 2 | RIGHT   ; 5d
-	movement NormalStep,         STEP_RUN << 2 | DOWN    ; 5e
-	movement NormalStep,         STEP_RUN << 2 | UP      ; 5f
-	movement NormalStep,         STEP_RUN << 2 | LEFT    ; 60
-	movement NormalStep,         STEP_RUN << 2 | RIGHT   ; 61
-	movement DiagonalStairsStep, STEP_WALK << 2 | DOWN   ; 62
-	movement DiagonalStairsStep, STEP_WALK << 2 | UP     ; 63
-	movement DiagonalStairsStep, STEP_WALK << 2 | LEFT   ; 64
-	movement DiagonalStairsStep, STEP_WALK << 2 | RIGHT  ; 65
-	movement ShakeExeggutor,     64                      ; 66
-	movement NormalStep,         STEP_WALK << 2 | RIGHT  ; 67
+	movement TurnHead,           OW_DOWN                  ; 00
+	movement TurnHead,           OW_UP                    ; 01
+	movement TurnHead,           OW_LEFT                  ; 02
+	movement TurnHead,           OW_RIGHT                 ; 03
+	movement TurnStep,           OW_DOWN                  ; 04
+	movement TurnStep,           OW_UP                    ; 05
+	movement TurnStep,           OW_LEFT                  ; 06
+	movement TurnStep,           OW_RIGHT                 ; 07
+	movement NormalStep,         STEP_SLOW << 2 | DOWN    ; 08
+	movement NormalStep,         STEP_SLOW << 2 | UP      ; 09
+	movement NormalStep,         STEP_SLOW << 2 | LEFT    ; 0a
+	movement NormalStep,         STEP_SLOW << 2 | RIGHT   ; 0b
+	movement NormalStep,         STEP_WALK << 2 | DOWN    ; 0c
+	movement NormalStep,         STEP_WALK << 2 | UP      ; 0d
+	movement NormalStep,         STEP_WALK << 2 | LEFT    ; 0e
+	movement NormalStep,         STEP_WALK << 2 | RIGHT   ; 0f
+	movement NormalStep,         STEP_BIKE << 2 | DOWN    ; 10
+	movement NormalStep,         STEP_BIKE << 2 | UP      ; 11
+	movement NormalStep,         STEP_BIKE << 2 | LEFT    ; 12
+	movement NormalStep,         STEP_BIKE << 2 | RIGHT   ; 13
+	movement SlideStep,          STEP_SLOW << 2 | DOWN    ; 14
+	movement SlideStep,          STEP_SLOW << 2 | UP      ; 15
+	movement SlideStep,          STEP_SLOW << 2 | LEFT    ; 16
+	movement SlideStep,          STEP_SLOW << 2 | RIGHT   ; 17
+	movement SlideStep,          STEP_WALK << 2 | DOWN    ; 18
+	movement SlideStep,          STEP_WALK << 2 | UP      ; 19
+	movement SlideStep,          STEP_WALK << 2 | LEFT    ; 1a
+	movement SlideStep,          STEP_WALK << 2 | RIGHT   ; 1b
+	movement SlideStep,          STEP_RUN << 2 | DOWN     ; 1c
+	movement SlideStep,          STEP_RUN << 2 | UP       ; 1d
+	movement SlideStep,          STEP_RUN << 2 | LEFT     ; 1e
+	movement SlideStep,          STEP_RUN << 2 | RIGHT    ; 1f
+	movement TurningStep,        STEP_SLOW << 2 | DOWN    ; 20
+	movement TurningStep,        STEP_SLOW << 2 | UP      ; 21
+	movement TurningStep,        STEP_SLOW << 2 | LEFT    ; 22
+	movement TurningStep,        STEP_SLOW << 2 | RIGHT   ; 23
+	movement TurningStep,        STEP_WALK << 2 | DOWN    ; 24
+	movement TurningStep,        STEP_WALK << 2 | UP      ; 25
+	movement TurningStep,        STEP_WALK << 2 | LEFT    ; 26
+	movement TurningStep,        STEP_WALK << 2 | RIGHT   ; 27
+	movement TurningStep,        STEP_WALK << 2 | DOWN    ; 28
+	movement TurningStep,        STEP_WALK << 2 | UP      ; 29
+	movement TurningStep,        STEP_WALK << 2 | LEFT    ; 2a
+	movement TurningStep,        STEP_WALK << 2 | RIGHT   ; 2b
+	movement JumpStep,           STEP_SLOW << 2 | DOWN    ; 2c
+	movement JumpStep,           STEP_SLOW << 2 | UP      ; 2d
+	movement JumpStep,           STEP_SLOW << 2 | LEFT    ; 2e
+	movement JumpStep,           STEP_SLOW << 2 | RIGHT   ; 2f
+	movement JumpStep,           STEP_WALK << 2 | DOWN    ; 30
+	movement JumpStep,           STEP_WALK << 2 | UP      ; 31
+	movement JumpStep,           STEP_WALK << 2 | LEFT    ; 32
+	movement JumpStep,           STEP_WALK << 2 | RIGHT   ; 33
+	movement JumpStep,           STEP_BIKE << 2 | DOWN    ; 34
+	movement JumpStep,           STEP_BIKE << 2 | UP      ; 35
+	movement JumpStep,           STEP_BIKE << 2 | LEFT    ; 36
+	movement JumpStep,           STEP_BIKE << 2 | RIGHT   ; 37
+	movement ObjectFlags1Step,   $86 + 8 * SLIDING_F      ; 38
+	movement ObjectFlags1Step,   $c6 + 8 * SLIDING_F      ; 39
+	movement ObjectFlags1Step,   $86 + 8 * FIXED_FACING_F ; 3a
+	movement ObjectFlags1Step,   $c6 + 8 * FIXED_FACING_F ; 3b
+	movement ObjectFlags1Step,   $86 + 8 * INVISIBLE_F    ; 3c
+	movement ObjectFlags1Step,   $c6 + 8 * INVISIBLE_F    ; 3d
+	movement SleepStep,          1                        ; 3e
+	movement SleepStep,          2                        ; 3f
+	movement SleepStep,          3                        ; 40
+	movement SleepStep,          4                        ; 41
+	movement SleepStep,          5                        ; 42
+	movement SleepStep,          6                        ; 43
+	movement SleepStep,          7                        ; 44
+	movement SleepStep,          8                        ; 45
+	movement LongSleepStep                                ; 46
+	movement EndStep                                      ; 47
+	movement ResumeStep                                   ; 48
+	movement RemoveObject                                 ; 49
+	movement LoopStep                                     ; 4a
+	movement Step4B                                       ; 4b
+	movement SetStepType,        STEP_TYPE_TELEPORT_FROM  ; 4c
+	movement SetStepType,        STEP_TYPE_TELEPORT_TO    ; 4d
+	movement SetStepType,        STEP_TYPE_SKYFALL        ; 4e
+	movement DigStep                                      ; 4f
+	movement BumpStep,           1                        ; 50
+	movement FishingStep,        STEP_TYPE_GOT_BITE       ; 51
+	movement FishingStep,        STEP_TYPE_FROM_MOVEMENT  ; 52
+	movement HideEmote                                    ; 53
+	movement ShowEmote                                    ; 54
+	movement ShakeScreenStep                              ; 55
+	movement ShakeTree,          24                       ; 56
+	movement SmashRock                                    ; 57
+	movement ReturnDig                                    ; 58
+	movement SetStepType,        STEP_TYPE_SKYFALL_TOP    ; 59
+	movement RunStep,            STEP_RUN << 2 | DOWN     ; 5a
+	movement RunStep,            STEP_RUN << 2 | UP       ; 5b
+	movement RunStep,            STEP_RUN << 2 | LEFT     ; 5c
+	movement RunStep,            STEP_RUN << 2 | RIGHT    ; 5d
+	movement NormalStep,         STEP_RUN << 2 | DOWN     ; 5e
+	movement NormalStep,         STEP_RUN << 2 | UP       ; 5f
+	movement NormalStep,         STEP_RUN << 2 | LEFT     ; 60
+	movement NormalStep,         STEP_RUN << 2 | RIGHT    ; 61
+	movement DiagonalStairsStep, STEP_WALK << 2 | DOWN    ; 62
+	movement DiagonalStairsStep, STEP_WALK << 2 | UP      ; 63
+	movement DiagonalStairsStep, STEP_WALK << 2 | LEFT    ; 64
+	movement DiagonalStairsStep, STEP_WALK << 2 | RIGHT   ; 65
+	movement ShakeExeggutor,     64                       ; 66
+	movement NormalStep,         STEP_WALK << 2 | RIGHT   ; 67
 	assert_table_length NUM_MOVEMENT_CMDS
 
 SetStepType:
@@ -313,40 +317,13 @@ LoopStep:
 	ld [hl], $0
 	jmp ContinueReadingMovement
 
-RemoveSliding:
+ObjectFlags1Step:
+; $86 + 8 * <FLAG>_F = res <FLAG>_F, [hl]
+; $c6 + 8 * <FLAG>_F = set <FLAG>_F, [hl]
 	ld hl, OBJECT_FLAGS1
 	add hl, bc
-	res SLIDING_F, [hl]
-	jmp ContinueReadingMovement
-
-SetSliding:
-	ld hl, OBJECT_FLAGS1
-	add hl, bc
-	set SLIDING_F, [hl]
-	jmp ContinueReadingMovement
-
-RemoveFixedFacing:
-	ld hl, OBJECT_FLAGS1
-	add hl, bc
-	res FIXED_FACING_F, [hl]
-	jmp ContinueReadingMovement
-
-FixFacing:
-	ld hl, OBJECT_FLAGS1
-	add hl, bc
-	set FIXED_FACING_F, [hl]
-	jmp ContinueReadingMovement
-
-ShowObject:
-	ld hl, OBJECT_FLAGS1
-	add hl, bc
-	res INVISIBLE_F, [hl]
-	jmp ContinueReadingMovement
-
-HideObject:
-	ld hl, OBJECT_FLAGS1
-	add hl, bc
-	set INVISIBLE_F, [hl]
+	ldh [hBitwiseFunctionOpcode], a
+	call hBitwiseFunction
 	jmp ContinueReadingMovement
 
 HideEmote:
