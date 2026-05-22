6712cf7dffc318fe38d806e6ddbb7a33681ea5c9
Rangi <sylvie.oukaour+rangi42@gmail.com>
2026-05-22 15:56:48 -0400

    Fix encounter music for Firebreather Dick
    Fixes #1565

--- maps/BurnedTower1F.asm
+++ maps/BurnedTower1F.asm
@@ -64,7 +64,8 @@ BurnedTower1FEusineTriggerScript:
 	end
 
 BurnedTowerFirebreatherDickBattleScript:
-	playmusic MUSIC_HIKER_ENCOUNTER
+	loadtrainer FIREBREATHER, DICK
+	encountermusic
 	showemote EMOTE_SHOCK, BURNEDTOWER1F_FIREBREATHER_DICK, 30
 	applyonemovement BURNEDTOWER1F_FIREBREATHER_DICK, step_up
 	turnobject PLAYER, DOWN
@@ -83,7 +84,6 @@ BurnedTowerFirebreatherDickBattleScript:
 
 	winlosstext FirebreatherDickBeatenText, 0
 	setlasttalked BURNEDTOWER1F_FIREBREATHER_DICK
-	loadtrainer FIREBREATHER, DICK
 	startbattle
 	iftruefwd .lost
 	disappear BURNEDTOWER1F_FIREBREATHER_DICK
