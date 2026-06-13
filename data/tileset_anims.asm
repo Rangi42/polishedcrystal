c0981f8a68daac07de00ee68233e8b4124b67084
Rangi <sylvie.oukaour+rangi42@gmail.com>
2026-06-13 12:38:45 -0400

    Add flashing Game Corner sign

--- data/tileset_anims.asm
+++ data/tileset_anims.asm
@@ -23,8 +23,6 @@ MACRO? tileframe
 ENDM
 
 TilesetJohtoTraditionalAnim::
-TilesetJohtoModernAnim::
-TilesetJohtoCoastAnim::
 TilesetJohtoOutlandsAnim::
 TilesetJohtoAncientAnim::
 TilesetBattleTowerOutsideAnim::
@@ -34,6 +32,19 @@ TilesetEcruteakShrineAnim::
 	tileframe AnimateWhirlpoolTiles,    $0:30
 	tileframe AnimateBuoyTiles,         $0:60
 	tileframe DoNothing
+	tileframe AnimateFlowerTile,        $0:03
+	tileframe AnimateRainTiles,         $0:1c
+	tileframe AnimateWaterfallTiles,    $0:34
+	tileframe StandingTileFrame8
+	tileframe DoneTileAnimation
+
+TilesetJohtoModernAnim::
+TilesetJohtoCoastAnim::
+	tileframe AnimateWaterTile,         $0:14
+	tileframe AnimateRainTiles,         $0:1c
+	tileframe AnimateWhirlpoolTiles,    $0:30
+	tileframe AnimateBuoyTiles,         $0:60
+	tileframe AnimateGameCornerTiles,   $1:94
 	tileframe AnimateFlowerTile,        $0:03
 	tileframe AnimateRainTiles,         $0:1c
 	tileframe AnimateWaterfallTiles,    $0:34
@@ -55,6 +66,7 @@ TilesetIndigoPlateauAnim::
 	tileframe ScrollTileRight,          $0:13
 	tileframe AnimateWaterfallTiles,    $0:6b
 	tileframe AnimateFountainTile,      $0:40
+	tileframe AnimateGameCornerTiles,   $0:7a
 	tileframe StandingTileFrame8
 	tileframe DoneTileAnimation
 
--- engine/tilesets/tileset_anims.asm
+++ engine/tilesets/tileset_anims.asm
@@ -765,6 +765,28 @@ AnimateWaterfallTiles:
 .WaterfallTileFrames:
 INCBIN "gfx/tilesets/animations/waterfall.2bpp"
 
+AnimateGameCornerTiles:
+	ld hl, sp + 0
+	ld b, h
+	ld c, l
+
+	; period 2, every 2 frames, offset to 4 tiles (64 bytes)
+	ld a, [wTileAnimationTimer]
+	maskbits 2, 1
+	swap a
+	add a
+
+	add LOW(.GameCornerTileFrames)
+	ld l, a
+	adc HIGH(.GameCornerTileFrames)
+	sub l
+	ld h, a
+
+	jmp WriteFourTilesHLToDE
+
+.GameCornerTileFrames:
+INCBIN "gfx/tilesets/animations/game_corner.2bpp"
+
 AnimateFireTiles:
 	ld hl, sp + 0
 	ld b, h
