2a4ad6a7c5ed61bb05c372c75c389564ffa1f8e2
Rangi <sylvie.oukaour+rangi42@gmail.com>
2026-08-30 00:29:00 -0400

    Move `CheckVBA` (home/vba.asm) out of ROM0

--- engine/movie/bsod.asm
+++ engine/movie/bsod.asm
@@ -151,3 +151,20 @@ BSODErrorStrings:
 .SRAMMismatch:     text "SRAM/WRAM mismatch@"
 .PEBKAC:           text "PEBKAC@"
 .UnknownError:     text "Unknown@"
+
+CheckVBA:
+	xor a
+	ldh [rSC], a ; no-optimize redundant loads (VBA loads this wrong)
+	ldh a, [rSC]
+	or ~%01111100
+	inc a
+	ret z
+	assert STARTOF(WRAM0) <= wEchoRAMTest && wEchoRAMTest < STARTOF(WRAM0) + SIZEOF(WRAM0), \
+		"wEchoRAMTest is not in WRAM0"
+	ld hl, wEchoRAMTest
+	ld [hl], %11100100
+	ld hl, wEchoRAMTest + $2000 ; echo RAM
+	ld [hl], %00100111
+	ld a, [wEchoRAMTest]
+	cp %00100111
+	ret
--- home.asm
+++ home.asm
@@ -58,7 +58,6 @@ INCLUDE "home/battle.asm"
 INCLUDE "home/sprite_anims.asm"
 INCLUDE "home/audio.asm"
 INCLUDE "home/restore_music.asm"
-INCLUDE "home/vba.asm"
 INCLUDE "home/vwf.asm"
 
 ; data used within home
