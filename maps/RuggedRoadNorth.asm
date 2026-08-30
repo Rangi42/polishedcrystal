ae70f877b72625b2a5c1f757de7b21c1c1ac53c5
Rangi <sylvie.oukaour+rangi42@gmail.com>
2026-08-30 13:07:42 -0400

    Add NPCs around the Rugged Road campfire

--- maps/RuggedRoadNorth.asm
+++ maps/RuggedRoadNorth.asm
@@ -16,6 +16,9 @@ RuggedRoadNorth_MapScriptHeader:
 
 	def_object_events
 	object_event  6, 11, SPRITE_BATTLE_GIRL, SPRITEMOVEDATA_SPINRANDOM_FAST, 0, 0, -1, 0, OBJECTTYPE_GENERICTRAINER, 2, GenericTrainerBattleGirlMei, -1
+	object_event 23, 9, SPRITE_HIKER, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, (1 << DAY) | (1 << NITE), 0, OBJECTTYPE_SCRIPT, 0, RuggedRoadNorthHikerScript, -1
+	object_event 22, 10, SPRITE_HIKER, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, (1 << MORN) | (1 << EVE), 0, OBJECTTYPE_SCRIPT, 0, RuggedRoadNorthHikerScript, -1
+	object_event 25, 10, SPRITE_FIREBREATHER, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 0, 1, -1, 0, OBJECTTYPE_COMMAND, jumptextfaceplayer, RuggedRoadNorthFirebreatherText, -1
 	object_event 23, 10, SPRITE_CAMPFIRE, SPRITEMOVEDATA_POKEMON, 0, 0, -1, 0, OBJECTTYPE_COMMAND, jumptextfaceplayer, RuggedRoadNorthCampfireText, -1
 	itemball_event  4,  4, X_ATTACK, 1, EVENT_RUGGED_ROAD_NORTH_X_ATTACK
 	itemball_event 26,  4, HYPER_POTION, 1, EVENT_RUGGED_ROAD_NORTH_HYPER_POTION
@@ -83,6 +86,54 @@ GenericTrainerBattleGirlMei:
 .BeatenText:
 	text "I see you've done"
 	line "your training."
+	done
+
+RuggedRoadNorthHikerScript:
+	checkevent EVENT_GOT_OVAL_STONE_FROM_RUGGED_ROAD
+	iftrue_jumptextfaceplayer .Text2
+	faceplayer
+	opentext
+	writetext .Text1
+	promptbutton
+	verbosegiveitem OVAL_STONE
+	iffalse_endtext
+	setevent EVENT_GOT_OVAL_STONE_FROM_RUGGED_ROAD
+	jumpthisopenedtext
+
+.Text2:
+	text "When you reach the"
+	line "top, don't forget"
+
+	para "to thank your"
+	line "lucky stone!"
+	done
+
+.Text1:
+	text "It's not just your"
+	line "skills that get"
+
+	para "you to the top of"
+	line "a tough mountain."
+
+	para "Luck must be on"
+	line "your side to reach"
+	cont "the top!"
+
+	para "This Oval Stone I"
+	line "found feels lucky."
+
+	para "Here, take it!"
+	done
+
+RuggedRoadNorthFirebreatherText:
+	text "Huff… puff…"
+
+	para "Keeping this fire"
+	line "lit is hard work,"
+
+	para "but without it,"
+	line "I'd turn into a"
+	cont "NeverMeltIce."
 	done
 
 RuggedRoadNorthCampfireText:
