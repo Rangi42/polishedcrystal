ab4bc7995fd05c31026ac7ff93c10fd5bd1a1871
Rangi <sylvie.oukaour+rangi42@gmail.com>
2026-09-01 09:55:46 -0400

    Use `LADDER_GLOW` collision in Cinnabar Volcano

--- maps/CinnabarVolcanoB1F.asm
+++ maps/CinnabarVolcanoB1F.asm
@@ -10,14 +10,14 @@ CinnabarVolcanoB1F_MapScriptHeader:
 	warp_event 19, 25, CINNABAR_VOLCANO_1F, 3
 	warp_event 29, 25, CINNABAR_VOLCANO_1F, 4
 	warp_event 27, 13, CINNABAR_VOLCANO_1F, 5
-	warp_event 18,  3, CINNABAR_VOLCANO_B2F, 1
+	warp_event 19,  3, CINNABAR_VOLCANO_B2F, 1
 	warp_event 33, 21, CINNABAR_VOLCANO_B2F, 2
 	warp_event  8,  9, CINNABAR_VOLCANO_B2F, 3
 	warp_event 12, 24, CINNABAR_VOLCANO_1F, 6 ; hole
 	warp_event 24, 24, CINNABAR_VOLCANO_1F, 7 ; hole
 	warp_event 24, 13, CINNABAR_VOLCANO_1F, 8 ; hole
 	warp_event 15, 11, CINNABAR_VOLCANO_1F, 9 ; hole
-	warp_event 20,  8, CINNABAR_VOLCANO_1F, 10
+	warp_event 20,  9, CINNABAR_VOLCANO_1F, 10
 
 	def_coord_events
 
