f2113cecd53233432e745b6f15399cca5d427ad0
ISSOtm <me@eldred.fr>
2026-09-02 17:53:22 +0200

    Merge the two gfx.asm files

--- home/gfx.asm
+++ home/gfx.asm
@@ -357,7 +357,7 @@ WriteVCopyRegistersToHRAM:
 	ldh [hNbTilesToCopy], a
 	ret
 
-VRAMToVRAMCopy::
+VRAMToVRAMCopy:
 	lb bc, STAT_MODE, LOW(rSTAT) ; predefine for speed and size
 	jr .waitNoHBlank2
 .outerLoop2
@@ -387,3 +387,61 @@ endr
 	ldh [hNbTilesToCopy], a
 	jr nz, .outerLoop2
 	jr DoneHBlankCopy
+
+HBlankCopy2bpp:
+	di
+	ld [wSPBuffer], sp
+	ld hl, hRequestedVTileDest
+	ld a, [hli]
+	ld e, a
+	ld a, [hli]
+	ld d, a ; destination
+
+	ld sp, hl ; source
+	pop hl
+	ld sp, hl ; set source to sp
+	ld a, h ; save source high byte for later
+	ld h, d ; exchange hl and de
+	ld l, e
+; vram to vram copy check:
+	cp HIGH(vTiles0) ; is source in RAM?
+	jr c, .innerLoop
+	cp HIGH(STARTOF(VRAM) + SIZEOF(VRAM)) ; is source past VRAM
+	jr nc, .innerLoop
+	jr VRAMToVRAMCopy
+.outerLoop
+	ldh a, [rLY]
+	cp $88
+	jmp nc, ContinueHBlankCopy
+.innerLoop
+	pop bc
+	pop de
+.waitNoHBlank
+	ldh a, [rSTAT]
+	and STAT_MODE
+	jr z, .waitNoHBlank
+.waitHBlank
+	ldh a, [rSTAT]
+	and STAT_MODE
+	jr nz, .waitHBlank
+; preloads r us
+	ld a, c
+	ld [hli], a
+	ld a, b
+	ld [hli], a
+	ld a, e
+	ld [hli], a
+	ld a, d
+	ld [hli], a
+rept 6
+	pop de
+	ld a, e
+	ld [hli], a
+	ld a, d
+	ld [hli], a
+endr
+	ldh a, [hNbTilesToCopy]
+	dec a
+	ldh [hNbTilesToCopy], a
+	jr nz, .outerLoop
+	jmp DoneHBlankCopy
